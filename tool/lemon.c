b1edd01ec9eabcc1cb3dcf45476d5f3b76bc08f4
drh <drh@noemail.net>
2000-06-02 18:52:12 +0000

    :-) (CVS 42)
    FossilOrigin-Name: 11e3296319843356cf09832577f7dea4b33c78be

--- tool/lemon.c
+++ tool/lemon.c
@@ -2994,7 +2994,8 @@ int mhflag;     /* Output in makeheaders format if true */
   if( lemp->arg && lemp->arg[0] ){
     int i;
     i = strlen(lemp->arg);
-    while( i>=1 && isalnum(lemp->arg[i-1]) ) i--;
+    while( i>=1 && isspace(lemp->arg[i-1]) ) i--;
+    while( i>=1 && (isalnum(lemp->arg[i-1]) || lemp->arg[i-1]=='_') ) i--;
     fprintf(out,"#define %sARGDECL ,%s\n",name,&lemp->arg[i]);  lineno++;
     fprintf(out,"#define %sXARGDECL %s;\n",name,lemp->arg);  lineno++;
     fprintf(out,"#define %sANSIARGDECL ,%s\n",name,lemp->arg);  lineno++;
