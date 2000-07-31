dba9c7577fdcb3802da704cf2bc07dd983924574
drh <drh@noemail.net>
2000-07-31 19:16:31 +0000

    :-) (CVS 118)
    FossilOrigin-Name: 4132d4760f6a2cbcf853f82546987d932e76e3c1

--- www/crosscompile.tcl
+++ www/crosscompile.tcl
@@ -1,7 +1,7 @@
 #
 # Run this Tcl script to generate the crosscompile.html file.
 #
-set rcsid {$Id: crosscompile.tcl,v 1.1 2000/07/31 18:56:15 drh Exp $}
+set rcsid {$Id: crosscompile.tcl,v 1.2 2000/07/31 19:16:32 drh Exp $}
 
 puts {<html>
 <head>
@@ -18,7 +18,7 @@ puts "<p align=center>
 puts {
 <p><a href="http://www.mingw.org/">MinGW</a> or
 <a href="http://www.mingw.org/">Minimalist GNU For Windows</a>
-is a version of the popular GCC compiler that build Win95/Win98/WinNT
+is a version of the popular GCC compiler that builds Win95/Win98/WinNT
 binaries.  See the website for details.</p>
 
 <p>This page describes how you can use MinGW configured as a
--- www/index.tcl
+++ www/index.tcl
@@ -1,7 +1,7 @@
 #
 # Run this TCL script to generate HTML for the index.html file.
 #
-set rcsid {$Id: index.tcl,v 1.20 2000/07/31 18:56:16 drh Exp $}
+set rcsid {$Id: index.tcl,v 1.21 2000/07/31 19:16:32 drh Exp $}
 
 puts {<html>
 <head><title>SQLite: An SQL Database Engine Built Atop GDBM</title></head>
@@ -59,8 +59,8 @@ the <a href="http://www.google.com/search?q=gnu+readline+library">GNU
 Readline library</a></li>
 <li>A Tcl-based test suite provides near 100% code coverage</li>
 <li>7500+ lines of C code.  No external dependencies other than GDBM.</li>
-<li>Built and tested under Linux (RedHat 6.0).  Should work under any Unix and
-probably also under Windows95/98/NT/2000.</li>
+<li>Built and tested under Linux, HPUX, and WinNT.
+Should work under any Unix or Win95/98/2000.</li>
 </ul>
 </p>
 
