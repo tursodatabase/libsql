2758db280d2c955c8f66bfa99d194db91e252c2b
drh <drh@noemail.net>
2000-08-04 13:52:30 +0000

    :-) (CVS 127)
    FossilOrigin-Name: 695fd68eb6291bdcc04af0eec7c7cdd7ff10872b

--- www/changes.tcl
+++ www/changes.tcl
@@ -17,6 +17,11 @@ proc chng {date desc} {
   puts "<DD><P><UL>$desc</UL></P></DD>"
 }
 
+chng {2000 Aug 4} {
+<li>Documentation updates</li>
+<li>Added "busy" and "timeout" methods to the Tcl interface</li>
+}
+
 chng {2000 Aug 3} {
 <li>File format version number was being stored in sqlite_master.tcl
     multiple times. This was harmless, but unnecessary. It is now fixed.</li>
