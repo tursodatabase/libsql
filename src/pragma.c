b1639ffeb5fcc4cd8c47934fc96bdc4220105a8a
jplyon <jplyon@noemail.net>
2004-01-19 04:52:29 +0000

    comment (CVS 1188)
    FossilOrigin-Name: fa55c9861e1cc372a36958383a86895dc311bb4b

--- src/pragma.c
+++ src/pragma.c
@@ -11,7 +11,7 @@
 *************************************************************************
 ** This file contains code used to implement the PRAGMA command.
 **
-** $Id: pragma.c,v 1.12 2003/12/16 03:44:48 drh Exp $
+** $Id: pragma.c,v 1.13 2004/01/19 04:52:29 jplyon Exp $
 */
 #include "sqliteInt.h"
 #include <ctype.h>
@@ -34,7 +34,8 @@ static int getBoolean(char *z){
 
 /*
 ** Interpret the given string as a safety level.  Return 0 for OFF,
-** 1 for ON or NORMAL and 2 for FULL.
+** 1 for ON or NORMAL and 2 for FULL.  Return 1 for an empty or 
+** unrecognized string argument.
 **
 ** Note that the values returned are one less that the values that
 ** should be passed into sqliteBtreeSetSafetyLevel().  The is done
