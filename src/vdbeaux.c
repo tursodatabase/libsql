86f43308d6128a7d3d4b71ea17f8090e82577edb
drh <drh@noemail.net>
2004-10-05 17:37:36 +0000

    Rearrange memory allocation in sqlite3VdbeMakeReady to try to work around some byte-alignment problems on Sparc. (CVS 2002)
    FossilOrigin-Name: f623d6e74fb5f6f70a22c06278b2bb1b4b7f9b85

--- src/vdbeaux.c
+++ src/vdbeaux.c
@@ -604,21 +604,22 @@ void sqlite3VdbeMakeReady(
     assert( nVar>=0 );
     n = isExplain ? 10 : p->nOp;
     p->aStack = sqliteMalloc(
-      n*(sizeof(p->aStack[0])+sizeof(Mem*))          /* aStack, apArg */
-      + nVar*sizeof(Mem)                             /* aVar */
-      + nVar*sizeof(char*)                           /* azVar */
-      + nMem*sizeof(Mem)                             /* aMem */
-      + nCursor*sizeof(Cursor*)                      /* apCsr */
+        n*sizeof(p->aStack[0])         /* aStack */
+      + n*sizeof(Mem*)                 /* apArg */
+      + nVar*sizeof(Mem)               /* aVar */
+      + nVar*sizeof(char*)             /* azVar */
+      + nMem*sizeof(Mem)               /* aMem */
+      + nCursor*sizeof(Cursor*)        /* apCsr */
     );
     if( !sqlite3_malloc_failed ){
-      p->apArg = (Mem **)&p->aStack[n];
-      p->aVar = (Mem *)&p->apArg[n];
-      p->azVar = (char**)&p->aVar[nVar];
-      p->okVar = 0;
-      p->nVar = nVar;
-      p->aMem = (Mem*)&p->azVar[nVar];
+      p->aMem = &p->aStack[n];
       p->nMem = nMem;
-      p->apCsr = (Cursor**)&p->aMem[nMem];
+      p->aVar = &p->aMem[nMem];
+      p->nVar = nVar;
+      p->okVar = 0;
+      p->apArg = (Mem**)&p->aVar[nVar];
+      p->azVar = (char**)&p->apArg[n];
+      p->apCsr = (Cursor**)&p->azVar[nVar];
       p->nCursor = nCursor;
       for(n=0; n<nVar; n++){
         p->aVar[n].flags = MEM_Null;
