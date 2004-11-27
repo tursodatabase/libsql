4b2266ae4ca32b77ef361a7d49b79e932275e672
drh <drh@noemail.net>
2004-11-27 15:52:16 +0000

    Disable the cursors.  "make install" now builds and installs a TCL package for SQLite if TCL is installed on the system. (CVS 2153)
    FossilOrigin-Name: d75014c299496a02f6850545beb0ab669802bf87

---
+++ tclinstaller.tcl
@@ -0,0 +1,28 @@
+# This script attempts to install SQLite3 so that it can be used
+# by TCL.  Invoke this script with single argument which is the
+# version number of SQLite.  Example:
+#
+#    tclsh tclinstaller.tcl 3.0
+#
+set VERSION [lindex $argv 0]
+set LIBFILE .libs/libtclsqlite3[info sharedlibextension]
+set LIBDIR [lindex $auto_path 0]
+set LIBNAME [file tail $LIBFILE]
+set LIB $LIBDIR/sqlite3/$LIBNAME
+
+file delete -force $LIBDIR/sqlite3
+file mkdir $LIBDIR/sqlite3
+set fd [open $LIBDIR/sqlite3/pkgIndex.tcl w]
+puts $fd "package ifneeded sqlite3 $VERSION \[list load $LIB sqlite3\]"
+close $fd
+
+# We cannot use [file copy] because that will just make a copy of
+# a symbolic link.  We have to open and copy the file for ourselves.
+#
+set in [open $LIBFILE]
+fconfigure $in -translation binary
+set out [open $LIB w]
+fconfigure $out -translation binary
+puts -nonewline $out [read $in]
+close $in
+close $out
