490a73bcde8bd503517a8d72ba1c9f2be3e5d57d
drh <drh@noemail.net>
2005-02-01 03:20:00 +0000

    Lemon optimization:  When doing a shift following a reduce that pops one or more elements off the stack, no need to check for stack overflow. (CVS 2300)
    FossilOrigin-Name: adcd9a3fa2a86464abd613aa88ae110b4799a241

--- tool/lempar.c
+++ tool/lempar.c
@@ -478,7 +478,22 @@ static void yy_reduce(
   yypParser->yyidx -= yysize;
   yyact = yy_find_reduce_action(yypParser,yygoto);
   if( yyact < YYNSTATE ){
-    yy_shift(yypParser,yyact,yygoto,&yygotominor);
+#ifdef NDEBUG
+    /* If we are not debugging and the reduce action popped at least
+    ** one element off the stack, then we can push the new element back
+    ** onto the stack here, and skip the stack overflow test in yy_shift().
+    ** That gives a significant speed improvement. */
+    if( yysize ){
+      yypParser->yyidx++;
+      yymsp -= yysize-1;
+      yymsp->stateno = yyact;
+      yymsp->major = yygoto;
+      yymsp->minor = yygotominor;
+    }else
+#endif
+    {
+      yy_shift(yypParser,yyact,yygoto,&yygotominor);
+    }
   }else if( yyact == YYNSTATE + YYNRULE + 1 ){
     yy_accept(yypParser);
   }
