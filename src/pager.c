88b01a13c6e540f3269a6f6a8e082c6a737f7cde
drh <drh@noemail.net>
2005-03-28 18:04:27 +0000

    Fix another memory leak in the pager. (CVS 2425)
    FossilOrigin-Name: 4ca11ca015d798655170593c1b76d312185d6ed2

--- src/pager.c
+++ src/pager.c
@@ -18,7 +18,7 @@
 ** file simultaneously, or one process from reading the database while
 ** another is writing.
 **
-** @(#) $Id: pager.c,v 1.200 2005/03/28 08:44:07 danielk1977 Exp $
+** @(#) $Id: pager.c,v 1.201 2005/03/28 18:04:28 drh Exp $
 */
 #include "sqliteInt.h"
 #include "os.h"
@@ -913,6 +913,7 @@ static int pager_unwritelock(Pager *pPager){
     pPager->dirtyCache = 0;
     pPager->nRec = 0;
   }else{
+    assert( pPager->aInJournal==0 );
     assert( pPager->dirtyCache==0 || pPager->useJournal==0 );
   }
   rc = sqlite3OsUnlock(&pPager->fd, SHARED_LOCK);
@@ -1985,6 +1986,7 @@ int sqlite3pager_close(Pager *pPager){
   if( pPager->journalOpen ){
     sqlite3OsClose(&pPager->jfd);
   }
+  sqliteFree(pPager->aInJournal);
   if( pPager->stmtOpen ){
     sqlite3OsClose(&pPager->stfd);
   }
@@ -2618,6 +2620,7 @@ static int pager_open_journal(Pager *pPager){
   assert( pPager->state>=PAGER_RESERVED );
   assert( pPager->journalOpen==0 );
   assert( pPager->useJournal );
+  assert( pPager->aInJournal==0 );
   sqlite3pager_pagecount(pPager);
   pPager->aInJournal = sqliteMalloc( pPager->dbSize/8 + 1 );
   if( pPager->aInJournal==0 ){
