6e4fc2ca2bb348bbe0c30f1c26756c456c5168d4
drh <drh@noemail.net>
2005-09-15 21:24:51 +0000

    The table_info pragma now gives the order of columns in the primary key. (CVS 2694)
    FossilOrigin-Name: 9b60f48de7fbca96c6e26266a8fb9eed8bc378f2

--- src/build.c
+++ src/build.c
@@ -22,7 +22,7 @@
 **     COMMIT
 **     ROLLBACK
 **
-** $Id: build.c,v 1.348 2005/09/10 15:35:07 drh Exp $
+** $Id: build.c,v 1.349 2005/09/15 21:24:52 drh Exp $
 */
 #include "sqliteInt.h"
 #include <ctype.h>
@@ -1015,7 +1015,9 @@ void sqlite3AddPrimaryKey(
           break;
         }
       }
-      if( iCol<pTab->nCol ) pTab->aCol[iCol].isPrimKey = 1;
+      if( iCol<pTab->nCol ){
+        pTab->aCol[iCol].isPrimKey = i<255 ? i+1 : 255;
+      }
     }
     if( pList->nExpr>1 ) iCol = -1;
   }
