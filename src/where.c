570b935c2a5c732ecb6d821f995ad0cb1bbf7ff8
drh <drh@noemail.net>
2006-02-01 02:45:02 +0000

    In joins of the form: "A, B left C" make sure that the reordering optimization does not put table A after table C.  Ticket #1652. (CVS 3052)
    FossilOrigin-Name: 248b9be93d9532e31c640432b75c3310e180acb3

--- src/where.c
+++ src/where.c
@@ -16,7 +16,7 @@
 ** so is applicable.  Because this module is responsible for selecting
 ** indices, you might also think of this module as the "query optimizer".
 **
-** $Id: where.c,v 1.203 2006/01/24 12:09:20 danielk1977 Exp $
+** $Id: where.c,v 1.204 2006/02/01 02:45:02 drh Exp $
 */
 #include "sqliteInt.h"
 
@@ -1500,9 +1500,16 @@ WhereInfo *sqlite3WhereBegin(
     double lowestCost;          /* Cost of the pBest */
     int bestJ = 0;              /* The value of j */
     Bitmask m;                  /* Bitmask value for j or bestJ */
+    int once = 0;               /* True when first table is seen */
 
     lowestCost = SQLITE_BIG_DBL;
     for(j=iFrom, pTabItem=&pTabList->a[j]; j<pTabList->nSrc; j++, pTabItem++){
+      if( once && 
+          ((pTabItem->jointype & (JT_LEFT|JT_CROSS))!=0
+           || (j>0 && (pTabItem[-1].jointype & (JT_LEFT|JT_CROSS))!=0))
+      ){
+        break;
+      }
       m = getMask(&maskSet, pTabItem->iCursor);
       if( (m & notReady)==0 ){
         if( j==iFrom ) iFrom++;
@@ -1512,16 +1519,12 @@ WhereInfo *sqlite3WhereBegin(
                        (i==0 && ppOrderBy) ? *ppOrderBy : 0,
                        &pIdx, &flags, &nEq);
       if( cost<lowestCost ){
+        once = 1;
         lowestCost = cost;
         pBest = pIdx;
         bestFlags = flags;
         bestNEq = nEq;
         bestJ = j;
-      }
-      if( (pTabItem->jointype & (JT_LEFT|JT_CROSS))!=0
-         || (j>0 && (pTabItem[-1].jointype & (JT_LEFT|JT_CROSS))!=0)
-      ){
-        break;
       }
     }
     TRACE(("*** Optimizer choose table %d for loop %d\n", bestJ,
