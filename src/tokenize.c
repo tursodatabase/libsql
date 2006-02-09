0b2a5eeae9d560df467603777dfaac49ccccfe7f
drh <drh@noemail.net>
2006-02-09 22:24:41 +0000

    Allow '@' to introduce host parameter names for compatibility with MS SQL Server.  Ticket #1671. (CVS 3067)
    FossilOrigin-Name: 0738ef818d4023a5159b6bee0a65f0b83d01c1d5

--- src/tokenize.c
+++ src/tokenize.c
@@ -15,7 +15,7 @@
 ** individual tokens and sends those tokens one-by-one over to the
 ** parser for analysis.
 **
-** $Id: tokenize.c,v 1.116 2006/01/20 17:56:33 drh Exp $
+** $Id: tokenize.c,v 1.117 2006/02/09 22:24:41 drh Exp $
 */
 #include "sqliteInt.h"
 #include "os.h"
@@ -261,6 +261,7 @@ static int getToken(const unsigned char *z, int *tokenType){
 #ifndef SQLITE_OMIT_TCL_VARIABLE
     case '$':
 #endif
+    case '@':  /* For compatibility with MS SQL Server */
     case ':': {
       int n = 0;
       *tokenType = TK_VARIABLE;
