2670a173ed0c2ad508efca74a5f6884c739e09a2
shess <shess@noemail.net>
2006-10-10 17:37:14 +0000

    Copy fts1/ to fts2/, changing reference from fts1 to fts2.  For future reference, the source versions copied were:
    README.txt r1.1
    fts1.c r1.37
    fts1.h r1.2
    fts1_hash.c r1.1
    fts1_hash.h r1.1
    fts1_porter.c r1.1
    fts1_tokenizer.h r1.4
    fts1_tokenizer1.c r1.6 (CVS 3471)
    
    FossilOrigin-Name: d0d1e7cdcc1dd085f1e359ce35c441699d517b02

---
+++ ext/fts2/fts2.h
@@ -0,0 +1,11 @@
+#include "sqlite3.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  /* __cplusplus */
+
+int sqlite3Fts2Init(sqlite3 *db);
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif  /* __cplusplus */
