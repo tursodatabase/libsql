6b6ab133533de12eca702cbf891ae92a4b8fce1a
shess <shess@noemail.net>
2007-04-09 20:45:40 +0000

    Fix crash in delete when existing row has null fields.  Previous code assumed that the row had values in all columns, sigh.  Fixes bug http://www.sqlite.org/cvstrac/tktview?tn=2289 . (CVS 3833)
    FossilOrigin-Name: 81be7290a4db7b74a533aaf95c7389eb4bde6a88

--- ext/fts2/fts2.c
+++ ext/fts2/fts2.c
@@ -1924,13 +1924,14 @@ static void freeStringArray(int nString, const char **pString){
   int i;
 
   for (i=0 ; i < nString ; ++i) {
-    free((void *) pString[i]);
+    if( pString[i]!=NULL ) free((void *) pString[i]);
   }
   free((void *) pString);
 }
 
 /* select * from %_content where rowid = [iRow]
  * The caller must delete the returned array and all strings in it.
+ * null fields will be NULL in the returned array.
  *
  * TODO: Perhaps we should return pointer/length strings here for consistency
  * with other code which uses pointer/length. */
@@ -1954,7 +1955,11 @@ static int content_select(fulltext_vtab *v, sqlite_int64 iRow,
 
   values = (const char **) malloc(v->nColumn * sizeof(const char *));
   for(i=0; i<v->nColumn; ++i){
-    values[i] = string_dup((char*)sqlite3_column_text(s, i));
+    if( sqlite3_column_type(s, i)==SQLITE_NULL ){
+      values[i] = NULL;
+    }else{
+      values[i] = string_dup((char*)sqlite3_column_text(s, i));
+    }
   }
 
   /* We expect only one row.  We must execute another sqlite3_step()
