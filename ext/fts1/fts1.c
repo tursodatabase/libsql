443ecd036d786952de732d609ce94b328a07eb39
shess <shess@noemail.net>
2007-07-25 00:38:05 +0000

    Replicates http://www.sqlite.org/cvstrac/chngview?cn=4151 which modified fts2:
    Modify handling of SQLITE_SCHEMA in fts2 code. An SQLITE_SCHEMA error
    may cause SQLite to reload the internal schema, deleting and
    recreating v-table objects. So the sqlite3_vtab structure can be
    deleted out from under a v-table implementation. (CVS 4183)
    
    FossilOrigin-Name: f9020cffda02923ef45979bb447ec2e232086ad5

--- ext/fts1/fts1.c
+++ ext/fts1/fts1.c
@@ -1193,25 +1193,18 @@ static int sql_step_statement(fulltext_vtab *v, fulltext_statement iStmt,
   assert( s==v->pFulltextStatements[iStmt] );
 
   while( (rc=sqlite3_step(s))!=SQLITE_DONE && rc!=SQLITE_ROW ){
-    sqlite3_stmt *pNewStmt;
-
     if( rc==SQLITE_BUSY ) continue;
     if( rc!=SQLITE_ERROR ) return rc;
 
-    rc = sqlite3_reset(s);
-    if( rc!=SQLITE_SCHEMA ) return SQLITE_ERROR;
-
-    v->pFulltextStatements[iStmt] = NULL;   /* Still in s */
-    rc = sql_get_statement(v, iStmt, &pNewStmt);
-    if( rc!=SQLITE_OK ) goto err;
-    *ppStmt = pNewStmt;
-
-    rc = sqlite3_transfer_bindings(s, pNewStmt);
-    if( rc!=SQLITE_OK ) goto err;
-
+    /* If an SQLITE_SCHEMA error has occured, then finalizing this
+     * statement is going to delete the fulltext_vtab structure. If
+     * the statement just executed is in the pFulltextStatements[]
+     * array, it will be finalized twice. So remove it before
+     * calling sqlite3_finalize().
+     */
+    v->pFulltextStatements[iStmt] = NULL;
     rc = sqlite3_finalize(s);
-    if( rc!=SQLITE_OK ) return rc;
-    s = pNewStmt;
+    break;
   }
   return rc;
 
