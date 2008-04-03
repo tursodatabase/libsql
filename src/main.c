fb103a848827863798cc80db8f0b6b69a51d0d3a
danielk1977 <danielk1977@noemail.net>
2008-04-03 16:28:24 +0000

    Instead of calling sqlite3_exec() to evaluate "PRAGMA encoding = UTF16" in sqlite3_open16(), set the connection encoding flag directly. (CVS 4959)
    FossilOrigin-Name: 33a12e737c343dbc452a25321a63456c1a8f7548

--- src/main.c
+++ src/main.c
@@ -14,7 +14,7 @@
 ** other files are for internal use by SQLite and should not be
 ** accessed by users of the library.
 **
-** $Id: main.c,v 1.430 2008/04/01 15:06:34 drh Exp $
+** $Id: main.c,v 1.431 2008/04/03 16:28:25 danielk1977 Exp $
 */
 #include "sqliteInt.h"
 #include <ctype.h>
@@ -1249,7 +1249,7 @@ int sqlite3_open16(
                       SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, 0);
     assert( *ppDb || rc==SQLITE_NOMEM );
     if( rc==SQLITE_OK ){
-      rc = sqlite3_exec(*ppDb, "PRAGMA encoding = 'UTF-16'", 0, 0, 0);
+      ENC(*ppDb) = SQLITE_UTF16NATIVE;
       if( rc!=SQLITE_OK ){
         sqlite3_close(*ppDb);
         *ppDb = 0;
