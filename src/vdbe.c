bfdc754f6550ebd2f9e7991045b49c3b6981bc9e
drh <drh@noemail.net>
2008-05-29 03:12:54 +0000

    Fix an obsolete comment on the OP_Rowid opcode in the VDBE. (CVS 5174)
    FossilOrigin-Name: 0d55328e680e23aae36d8f32a05aa3815393ac1d

--- src/vdbe.c
+++ src/vdbe.c
@@ -43,7 +43,7 @@
 ** in this file for details.  If in doubt, do not deviate from existing
 ** commenting and indentation practices when changing or adding code.
 **
-** $Id: vdbe.c,v 1.741 2008/05/15 17:48:20 danielk1977 Exp $
+** $Id: vdbe.c,v 1.742 2008/05/29 03:12:55 drh Exp $
 */
 #include "sqliteInt.h"
 #include <ctype.h>
@@ -3551,7 +3551,7 @@ case OP_RowData: {
 /* Opcode: Rowid P1 P2 * * *
 **
 ** Store in register P2 an integer which is the key of the table entry that
-** P1 is currently point to.  If p2==0 then push the integer.
+** P1 is currently point to.
 */
 case OP_Rowid: {                 /* out2-prerelease */
   int i = pOp->p1;
