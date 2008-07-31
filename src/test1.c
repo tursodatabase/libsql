8225f5ac550decffa83b88619b7ca49a3d0df37c
shane <shane@noemail.net>
2008-07-31 02:05:04 +0000

    Omit calls to test_get_table_printf() if SQLITE_OMIT_GET_TABLE defined. (CVS 5509)
    FossilOrigin-Name: 524a4075ddaecb07469da717e8afebc91240616e

--- src/test1.c
+++ src/test1.c
@@ -13,7 +13,7 @@
 ** is not included in the SQLite library.  It is used for automated
 ** testing of the SQLite library.
 **
-** $Id: test1.c,v 1.316 2008/07/30 15:27:54 drh Exp $
+** $Id: test1.c,v 1.317 2008/07/31 02:05:04 shane Exp $
 */
 #include "sqliteInt.h"
 #include "tcl.h"
@@ -497,6 +497,8 @@ static int test_snprintf_int(
   return TCL_OK;
 }
 
+#ifndef SQLITE_OMIT_GET_TABLE
+
 /*
 ** Usage:  sqlite3_get_table_printf  DB  FORMAT  STRING  ?--no-counts?
 **
@@ -558,6 +560,8 @@ static int test_get_table_printf(
   if( sqlite3TestErrCode(interp, db, rc) ) return TCL_ERROR;
   return TCL_OK;
 }
+
+#endif /* SQLITE_OMIT_GET_TABLE */
 
 
 /*
@@ -4529,7 +4533,9 @@ int Sqlitetest1_Init(Tcl_Interp *interp){
      { "sqlite3_exec_printf",           (Tcl_CmdProc*)test_exec_printf      },
      { "sqlite3_exec",                  (Tcl_CmdProc*)test_exec             },
      { "sqlite3_exec_nr",               (Tcl_CmdProc*)test_exec_nr          },
+#ifndef SQLITE_OMIT_GET_TABLE
      { "sqlite3_get_table_printf",      (Tcl_CmdProc*)test_get_table_printf },
+#endif
      { "sqlite3_close",                 (Tcl_CmdProc*)sqlite_test_close     },
      { "sqlite3_create_function",       (Tcl_CmdProc*)test_create_function  },
      { "sqlite3_create_aggregate",      (Tcl_CmdProc*)test_create_aggregate },
--- src/test_config.c
+++ src/test_config.c
@@ -16,7 +16,7 @@
 ** The focus of this file is providing the TCL testing layer
 ** access to compile-time constants.
 **
-** $Id: test_config.c,v 1.32 2008/07/25 15:39:04 drh Exp $
+** $Id: test_config.c,v 1.33 2008/07/31 02:05:05 shane Exp $
 */
 
 #include "sqliteLimit.h"
@@ -241,6 +241,12 @@ static void set_options(Tcl_Interp *interp){
   Tcl_SetVar2(interp, "sqlite_options", "fts3", "1", TCL_GLOBAL_ONLY);
 #else
   Tcl_SetVar2(interp, "sqlite_options", "fts3", "0", TCL_GLOBAL_ONLY);
+#endif
+
+#ifdef SQLITE_OMIT_GET_TABLE
+  Tcl_SetVar2(interp, "sqlite_options", "gettable", "0", TCL_GLOBAL_ONLY);
+#else
+  Tcl_SetVar2(interp, "sqlite_options", "gettable", "1", TCL_GLOBAL_ONLY);
 #endif
 
 #ifdef SQLITE_OMIT_GLOBALRECOVER
