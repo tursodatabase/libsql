ca8c4667a8f883e82165a369b1fedb428837d8a2
drh <drh@noemail.net>
2008-12-28 20:47:02 +0000

    Multi-index OR optimizer response to ORDER BY rowid. But fix in sqlite3_stmt_status(): report a full table scan when "ORDER BY rowid" is used without constraints. (CVS 6069)
    FossilOrigin-Name: 3464d369d3b6899ec726cf5b42b68b1dac2ba982

--- src/where.c
+++ src/where.c
@@ -16,7 +16,7 @@
 ** so is applicable.  Because this module is responsible for selecting
 ** indices, you might also think of this module as the "query optimizer".
 **
-** $Id: where.c,v 1.346 2008/12/28 18:35:09 drh Exp $
+** $Id: where.c,v 1.347 2008/12/28 20:47:03 drh Exp $
 */
 #include "sqliteInt.h"
 
@@ -1877,6 +1877,12 @@ static void bestIndex(
         pCost->nRow = nRow;
         pCost->plan.wsFlags = WHERE_MULTI_OR;
         pCost->plan.u.pTerm = pTerm;
+        if( pOrderBy!=0
+         && sortableByRowid(iCur, pOrderBy, pWC->pMaskSet, &rev)
+         && !rev
+        ){
+          pCost->plan.wsFlags = WHERE_ORDERBY|WHERE_MULTI_OR;
+        }
       }
     }
   }
@@ -2426,6 +2432,7 @@ static Bitmask codeOneLoopStart(
     pLevel->op = bRev ? OP_Prev : OP_Next;
     pLevel->p1 = iCur;
     pLevel->p2 = start;
+    pLevel->p5 = (pStart==0 && pEnd==0) ?1:0;
     codeRowSetEarly = regRowSet>=0 ? whereRowReadyForOutput(pWC) : 0;
     if( codeRowSetEarly || testOp!=OP_Noop ){
       int r1 = sqlite3GetTempReg(pParse);
