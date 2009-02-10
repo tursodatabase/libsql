2b754b4820fa1b438608e4e4ab551c5fe05cc723
danielk1977 <danielk1977@noemail.net>
2009-02-10 18:54:02 +0000

    For the "onefile" demo, pass SQLITE_OPEN_TEMP_DB instead of MAIN_DB to the OS layer when opening the single file. This is to work around the assert() in os_unix.c that tests that the locking region is not written to. (CVS 6278)
    FossilOrigin-Name: 2da076a2c1663e916dc50e5e1679be216922dfc5

--- src/test_onefile.c
+++ src/test_onefile.c
@@ -10,7 +10,7 @@
 **
 *************************************************************************
 **
-** $Id: test_onefile.c,v 1.10 2008/12/09 01:32:03 drh Exp $
+** $Id: test_onefile.c,v 1.11 2009/02/10 18:54:03 danielk1977 Exp $
 **
 ** OVERVIEW:
 **
@@ -604,6 +604,7 @@ static int fsOpen(
   for(; pReal && strncmp(pReal->zName, zName, nName); pReal=pReal->pNext);
 
   if( !pReal ){
+    int real_flags = (flags&~(SQLITE_OPEN_MAIN_DB))|SQLITE_OPEN_TEMP_DB;
     sqlite3_int64 size;
     sqlite3_file *pRealFile;
     sqlite3_vfs *pParent = pFsVfs->pParent;
@@ -618,7 +619,7 @@ static int fsOpen(
     pReal->zName = zName;
     pReal->pFile = (sqlite3_file *)(&pReal[1]);
 
-    rc = pParent->xOpen(pParent, zName, pReal->pFile, flags, pOutFlags);
+    rc = pParent->xOpen(pParent, zName, pReal->pFile, real_flags, pOutFlags);
     if( rc!=SQLITE_OK ){
       goto open_out;
     }
