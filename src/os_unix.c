f48f9ca65c574e522d68bd7473defcebbb8d5ea3
drh <drh@noemail.net>
2009-03-28 23:47:10 +0000

    Previous commit ((6401)) did not quite fix the problem.  This should work better. (CVS 6402)
    FossilOrigin-Name: 2e7d3cc9f04de1fe7ef95cd5736dbc409c209cef

--- src/os_unix.c
+++ src/os_unix.c
@@ -43,7 +43,7 @@
 **   *  Definitions of sqlite3_vfs objects for all locking methods
 **      plus implementations of sqlite3_os_init() and sqlite3_os_end().
 **
-** $Id: os_unix.c,v 1.246 2009/03/28 23:23:03 drh Exp $
+** $Id: os_unix.c,v 1.247 2009/03/28 23:47:11 drh Exp $
 */
 #include "sqliteInt.h"
 #if SQLITE_OS_UNIX              /* This file is used on unix only */
@@ -1481,7 +1481,8 @@ static int unixUnlock(sqlite3_file *id, int locktype){
           pFile->lastErrno = tErrno;
         }
         pLock->cnt = 1;
-        pLock->locktype = locktype;
+        pLock->locktype = NO_LOCK;
+        pFile->locktype = NO_LOCK;
         goto end_unlock;
       }
     }
