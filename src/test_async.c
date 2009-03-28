17561adc6100f2b02e48d327d8fc37e975641f81
drh <drh@noemail.net>
2009-03-28 18:56:14 +0000

    The test_async.c module must pass an unchanging filename to the underlying VFS. (CVS 6400)
    FossilOrigin-Name: d1eeee21677a3dffdb4ee1182322007bf24ef03a

--- src/test_async.c
+++ src/test_async.c
@@ -10,7 +10,7 @@
 **
 *************************************************************************
 **
-** $Id: test_async.c,v 1.55 2009/03/28 17:21:52 danielk1977 Exp $
+** $Id: test_async.c,v 1.56 2009/03/28 18:56:14 drh Exp $
 **
 ** This file contains an example implementation of an asynchronous IO 
 ** backend for SQLite.
@@ -1133,9 +1133,9 @@ static int asyncOpen(
 
   if( !isAsyncOpen ){
     int flagsout;
-    rc = pVfs->xOpen(pVfs, zName, pData->pBaseRead, flags, &flagsout);
+    rc = pVfs->xOpen(pVfs, pData->zName, pData->pBaseRead, flags, &flagsout);
     if( rc==SQLITE_OK && (flagsout&SQLITE_OPEN_READWRITE) ){
-      rc = pVfs->xOpen(pVfs, zName, pData->pBaseWrite, flags, 0);
+      rc = pVfs->xOpen(pVfs, pData->zName, pData->pBaseWrite, flags, 0);
     }
     if( pOutFlags ){
       *pOutFlags = flagsout;
@@ -1154,7 +1154,7 @@ static int asyncOpen(
 #ifdef ENABLE_FILE_LOCKING
         if( flags&SQLITE_OPEN_MAIN_DB ){
           pLock->pFile = (sqlite3_file *)&pLock[1];
-          rc = pVfs->xOpen(pVfs, zName, pLock->pFile, flags, 0);
+          rc = pVfs->xOpen(pVfs, pData->zName, pLock->pFile, flags, 0);
           if( rc!=SQLITE_OK ){
             sqlite3_free(pLock);
             pLock = 0;
