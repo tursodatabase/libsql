52e2c35fb2035ba2a10d07eb0c4e2f2545e0fc41
drh <drh@noemail.net>
2009-04-17 15:18:47 +0000

    Get the behavior of SQLITE_HAVE_ISNAN right.  It was reversed in check-in (6517).  Ticket #3809. (CVS 6518)
    FossilOrigin-Name: 97c6ea2368556b2a9a914ba2322085e312598b15

--- src/util.c
+++ src/util.c
@@ -14,7 +14,7 @@
 ** This file contains functions for allocating memory, comparing
 ** strings, and stuff like that.
 **
-** $Id: util.c,v 1.250 2009/04/17 11:57:22 drh Exp $
+** $Id: util.c,v 1.251 2009/04/17 15:18:48 drh Exp $
 */
 #include "sqliteInt.h"
 #include <stdarg.h>
@@ -58,7 +58,7 @@ int sqlite3Assert(void){
 ** Otherwise, we have our own implementation that works on most systems.
 */
 int sqlite3IsNaN(double x){
-#if defined(SQLITE_HAVE_ISNAN)
+#if !defined(SQLITE_HAVE_ISNAN)
   /*
   ** Systems that support the isnan() library function should probably
   ** make use of it by compiling with -DSQLITE_HAVE_ISNAN.  But we have
