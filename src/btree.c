1bc9ee9835f5d5690e45a9004c4787b938dae09f
danielk1977 <danielk1977@noemail.net>
2009-07-04 15:41:02 +0000

    Remove a redundant branch from btree.c. (CVS 6847)
    FossilOrigin-Name: 133357d2f070ba303deddff59beead1ec8d10521

--- src/btree.c
+++ src/btree.c
@@ -9,7 +9,7 @@
 **    May you share freely, never taking more than you give.
 **
 *************************************************************************
-** $Id: btree.c,v 1.650 2009/07/03 16:25:07 danielk1977 Exp $
+** $Id: btree.c,v 1.651 2009/07/04 15:41:03 danielk1977 Exp $
 **
 ** This file implements a external (disk-based) database using BTrees.
 ** See the header comment on "btreeInt.h" for additional information.
@@ -2236,13 +2236,12 @@ static int lockBtreeWithRetry(Btree *pRef){
 ** this routine unrefs the first page of the database file which 
 ** has the effect of releasing the read lock.
 **
-** If there are any outstanding cursors, this routine is a no-op.
-**
 ** If there is a transaction in progress, this routine is a no-op.
 */
 static void unlockBtreeIfUnused(BtShared *pBt){
   assert( sqlite3_mutex_held(pBt->mutex) );
-  if( pBt->inTransaction==TRANS_NONE && pBt->pCursor==0 && pBt->pPage1!=0 ){
+  assert( pBt->pCursor==0 || pBt->inTransaction>TRANS_NONE );
+  if( pBt->inTransaction==TRANS_NONE && pBt->pPage1!=0 ){
     assert( pBt->pPage1->aData );
     assert( sqlite3PagerRefcount(pBt->pPager)==1 );
     assert( pBt->pPage1->aData );
