257d9dc74855d8ba4e58dbb0d088299d518788bb
danielk1977 <danielk1977@noemail.net>
2009-07-22 07:27:56 +0000

    Changes to test code (only) to allow veryquick.test to run with OMIT_UTF16 builsd. Ticket #3985. (CVS 6919)
    FossilOrigin-Name: 7f3fc7f20f6dc205eedbe445e35f5071f1c01185

--- src/test_func.c
+++ src/test_func.c
@@ -12,7 +12,7 @@
 ** Code for testing all sorts of SQLite interfaces.  This code
 ** implements new SQL functions used by the test scripts.
 **
-** $Id: test_func.c,v 1.15 2009/05/07 13:43:49 drh Exp $
+** $Id: test_func.c,v 1.16 2009/07/22 07:27:57 danielk1977 Exp $
 */
 #include "sqlite3.h"
 #include "tcl.h"
@@ -156,6 +156,7 @@ void sqlite3EndBenignMalloc(void);
 static void test_agg_errmsg16_step(sqlite3_context *a, int b,sqlite3_value **c){
 }
 static void test_agg_errmsg16_final(sqlite3_context *ctx){
+#ifndef SQLITE_OMIT_UTF16
   const void *z;
   sqlite3 * db = sqlite3_context_db_handle(ctx);
   sqlite3_aggregate_context(ctx, 2048);
@@ -163,6 +164,7 @@ static void test_agg_errmsg16_final(sqlite3_context *ctx){
   z = sqlite3_errmsg16(db);
   sqlite3EndBenignMalloc();
   sqlite3_result_text16(ctx, z, -1, SQLITE_TRANSIENT);
+#endif
 }
 
 /*
