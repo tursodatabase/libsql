e1a2a990b8541f9032ac9d0c8c75eee11501950f
dan <dan@noemail.net>
2010-07-06 10:55:44 +0000

    Before calling Tcl_ExitThread() in a multi-threaded test, call Tcl_DoOneEvent() as many times as necessary to handle any queued events.
    FossilOrigin-Name: b3399b40785d13754979203f15d71c95fec50282

--- src/test_thread.c
+++ src/test_thread.c
@@ -150,6 +150,7 @@ static Tcl_ThreadCreateType tclScriptThread(ClientData pSqlThread){
   Tcl_DecrRefCount(pList);
   Tcl_DecrRefCount(pRes);
   Tcl_DeleteInterp(interp);
+  while( Tcl_DoOneEvent(TCL_ALL_EVENTS|TCL_DONT_WAIT) );
   Tcl_ExitThread(0);
   TCL_THREAD_CREATE_RETURN;
 }
