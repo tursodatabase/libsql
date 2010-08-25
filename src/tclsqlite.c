4c0f1649aca8be3a3b1c0311d35cd146008f7efe
drh <drh@noemail.net>
2010-08-25 19:39:19 +0000

    Disable the legacy "sqlite" command in the TCL interface.  Provide only the "sqlite3" command.
    FossilOrigin-Name: 909b3d8862aeae04611969f5fc27d32a82026767

--- src/tclsqlite.c
+++ src/tclsqlite.c
@@ -3019,8 +3019,10 @@ int Sqlite3_Init(Tcl_Interp *interp){
   Tcl_InitStubs(interp, "8.4", 0);
   Tcl_CreateObjCommand(interp, "sqlite3", (Tcl_ObjCmdProc*)DbMain, 0, 0);
   Tcl_PkgProvide(interp, "sqlite3", PACKAGE_VERSION);
+#if 0
   Tcl_CreateObjCommand(interp, "sqlite", (Tcl_ObjCmdProc*)DbMain, 0, 0);
   Tcl_PkgProvide(interp, "sqlite", PACKAGE_VERSION);
+#endif
   return TCL_OK;
 }
 int Tclsqlite3_Init(Tcl_Interp *interp){ return Sqlite3_Init(interp); }
