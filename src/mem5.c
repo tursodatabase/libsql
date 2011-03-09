a6ec892b5becd14478f41f617dcc764341e9e59f
shaneh <shaneh@noemail.net>
2011-03-09 21:36:17 +0000

    Fix issue with mem5 allocator when min request size is larger thatn 2^30.
    FossilOrigin-Name: d7dae06fb2d57ed6b9555b774712f42077ae4155

--- src/mem5.c
+++ src/mem5.c
@@ -442,7 +442,7 @@ static int memsys5Roundup(int n){
 */
 static int memsys5Log(int iValue){
   int iLog;
-  for(iLog=0; (1<<iLog)<iValue; iLog++);
+  for(iLog=0; (iLog<((sizeof(int)*8)-2)) && (1<<iLog)<iValue; iLog++);
   return iLog;
 }
 
@@ -473,7 +473,10 @@ static int memsys5Init(void *NotUsed){
   zByte = (u8*)sqlite3GlobalConfig.pHeap;
   assert( zByte!=0 );  /* sqlite3_config() does not allow otherwise */
 
-  nMinLog = memsys5Log(sqlite3GlobalConfig.mnReq);
+  /* boundaries on sqlite3GlobalConfig.mnReq are enforced in sqlite3_config() */
+  nMinLog = sqlite3GlobalConfig.mnReq;
+  assert( nMinLog>0 && nMinLog<=(1<<12) );
+  nMinLog = memsys5Log(nMinLog);
   mem5.szAtom = (1<<nMinLog);
   while( (int)sizeof(Mem5Link)>mem5.szAtom ){
     mem5.szAtom = mem5.szAtom << 1;
