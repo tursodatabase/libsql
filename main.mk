70586beb3dbd344d4b67b9d8d2d268ac7d50149c
drh <drh@noemail.net>
2011-04-01 23:49:44 +0000

    Add the "wholenumber" virtual table module to the test suite - useful in populating tables with many rows of content prior to a test.
    FossilOrigin-Name: 975dff155bd9db42f2b6855b24c861852bb7a201

--- main.mk
+++ main.mk
@@ -258,6 +258,7 @@ TESTSRC = \
   $(TOP)/src/test_tclvar.c \
   $(TOP)/src/test_thread.c \
   $(TOP)/src/test_vfs.c \
+  $(TOP)/src/test_wholenumber.c \
   $(TOP)/src/test_wsd.c
 
 #TESTSRC += $(TOP)/ext/fts2/fts2_tokenizer.c
--- src/tclsqlite.c
+++ src/tclsqlite.c
@@ -3583,6 +3583,7 @@ static void init_all(Tcl_Interp *interp){
     extern int SqliteSuperlock_Init(Tcl_Interp*);
     extern int SqlitetestSyscall_Init(Tcl_Interp*);
     extern int Sqlitetestfuzzer_Init(Tcl_Interp*);
+    extern int Sqlitetestwholenumber_Init(Tcl_Interp*);
 
 #ifdef SQLITE_ENABLE_ZIPVFS
     extern int Zipvfs_Init(Tcl_Interp*);
@@ -3622,6 +3623,7 @@ static void init_all(Tcl_Interp *interp){
     SqliteSuperlock_Init(interp);
     SqlitetestSyscall_Init(interp);
     Sqlitetestfuzzer_Init(interp);
+    Sqlitetestwholenumber_Init(interp);
 
     Tcl_CreateObjCommand(interp,"load_testfixture_extensions",init_all_cmd,0,0);
 
