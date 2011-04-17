1c4e6037276dfe6ce16da892f64908c58e515d41
drh <drh@noemail.net>
2011-04-17 17:09:58 +0000

    Change the prototype for the open() system call to agree with Posix.  Though a faulty function prototype in a pointer cast is a seemingly innocuous error, the correct prototype is necessary for pthreads to work correctly on NetBSD.
    FossilOrigin-Name: 3e135748f1efacb52b414b3ac3f4ae2c08bcd8fb

--- src/os_unix.c
+++ src/os_unix.c
@@ -293,7 +293,7 @@ static struct unix_syscall {
   sqlite3_syscall_ptr pDefault; /* Default value */
 } aSyscall[] = {
   { "open",         (sqlite3_syscall_ptr)open,       0  },
-#define osOpen      ((int(*)(const char*,int,int))aSyscall[0].pCurrent)
+#define osOpen      ((int(*)(const char*,int,...))aSyscall[0].pCurrent)
 
   { "close",        (sqlite3_syscall_ptr)close,      0  },
 #define osClose     ((int(*)(int))aSyscall[1].pCurrent)
