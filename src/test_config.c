f4ba1093568851a1320a84ce195b1dc08e8964c1
dan <dan@noemail.net>
2011-10-08 14:57:07 +0000

    Add the SQLITE_DIRECT_OVERFLOW_READ compile time option.
    FossilOrigin-Name: 2ab14a846727857175eac7961b7338c4d3b417ca

--- src/test_config.c
+++ src/test_config.c
@@ -55,6 +55,12 @@ static void set_options(Tcl_Interp *interp){
   Tcl_SetVar2(interp, "sqlite_options", "debug", "0", TCL_GLOBAL_ONLY);
 #endif
 
+#ifdef SQLITE_DIRECT_OVERFLOW_READ
+  Tcl_SetVar2(interp, "sqlite_options", "direct_read", "1", TCL_GLOBAL_ONLY);
+#else
+  Tcl_SetVar2(interp, "sqlite_options", "direct_read", "0", TCL_GLOBAL_ONLY);
+#endif
+
 #ifdef SQLITE_DISABLE_DIRSYNC
   Tcl_SetVar2(interp, "sqlite_options", "dirsync", "0", TCL_GLOBAL_ONLY);
 #else
