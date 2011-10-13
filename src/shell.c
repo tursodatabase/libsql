58c803b7f06f74ad7f7303fb36fe480d64ed86b7
drh <drh@noemail.net>
2011-10-13 13:34:04 +0000

    Do the ".dump" command inside of a transaction to prevent other processes from modifying the database while the dump is underway. Ticket [2466653295e65]
    FossilOrigin-Name: 1c00d5454c85dfddb6c628c3742b4ddeaad6423a

--- src/shell.c
+++ src/shell.c
@@ -1580,7 +1580,7 @@ static int do_meta_command(char *zLine, struct callback_data *p){
     fprintf(p->out, "PRAGMA foreign_keys=OFF;\n");
     fprintf(p->out, "BEGIN TRANSACTION;\n");
     p->writableSchema = 0;
-    sqlite3_exec(p->db, "PRAGMA writable_schema=ON", 0, 0, 0);
+    sqlite3_exec(p->db, "BEGIN; PRAGMA writable_schema=ON", 0, 0, 0);
     p->nErr = 0;
     if( nArg==1 ){
       run_schema_dump_query(p, 
@@ -1613,7 +1613,7 @@ static int do_meta_command(char *zLine, struct callback_data *p){
       }
     }
     if( p->writableSchema ){
-      fprintf(p->out, "PRAGMA writable_schema=OFF;\n");
+      fprintf(p->out, "PRAGMA writable_schema=OFF; COMMIT;\n");
       p->writableSchema = 0;
     }
     sqlite3_exec(p->db, "PRAGMA writable_schema=OFF", 0, 0, 0);
