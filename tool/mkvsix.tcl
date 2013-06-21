463f348f2ca660446da64b9abe842da22f59e432
mistachkin <mistachkin@noemail.net>
2013-06-21 07:54:39 +0000

    Update comments and package flavor error message.
    FossilOrigin-Name: 9d1b96182af08a73de9dd0327965b96a6d48ac11

--- tool/mkvsix.tcl
+++ tool/mkvsix.tcl
@@ -65,12 +65,12 @@
 # argument is optional and if present must contain the name of the directory
 # containing the root of the source tree for SQLite.  The third argument is
 # optional and if present must contain the flavor the VSIX package to build.
-# Currently, the only supported package flavors are "WinRT" and "WP80".  The
-# fourth argument is optional and if present must be a string containing a list
-# of platforms to include in the VSIX package.  The format of the platform list
-# string is "platform1,platform2,platform3".  Typically, when on Windows, this
-# script is executed using commands similar to the following from a normal
-# Windows command prompt:
+# Currently, the only supported package flavors are "WinRT", "WinRT81", and
+# "WP80".  The fourth argument is optional and if present must be a string
+# containing a list of platforms to include in the VSIX package.  The format
+# of the platform list string is "platform1,platform2,platform3".  Typically,
+# when on Windows, this script is executed using commands similar to the
+# following from a normal Windows command prompt:
 #
 #                         CD /D C:\dev\sqlite\core
 #                         tclsh85 tool\mkvsix.tcl C:\Temp
@@ -280,7 +280,7 @@ if {[string equal -nocase $packageFlavor WinRT]} then {
   set extraSdkPath "\\..\\$targetPlatformIdentifier"
   set extraFileListAttributes ""
 } else {
-  fail "unsupported package flavor, must be \"WinRT\" or \"WP80\""
+  fail "unsupported package flavor, must be \"WinRT\", \"WinRT81\", or \"WP80\""
 }
 
 if {$argc >= 4} then {
