049d487e2e1f8d68503cc6e16a68f78493f8257e
mistachkin <mistachkin@noemail.net>
2013-07-04 23:53:56 +0000

    Modify several extensions to use the new exported function naming.  Fix some shared library compilation issues.
    FossilOrigin-Name: f2ab8747825ab5131ffab174aa0ffe5e474f6811

--- ext/fts1/fts1.c
+++ ext/fts1/fts1.c
@@ -3335,8 +3335,11 @@ int sqlite3Fts1Init(sqlite3 *db){
 }
 
 #if !SQLITE_CORE
-int sqlite3_extension_init(sqlite3 *db, char **pzErrMsg,
-                           const sqlite3_api_routines *pApi){
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+int sqlite3_fts1_init(sqlite3 *db, char **pzErrMsg,
+                      const sqlite3_api_routines *pApi){
   SQLITE_EXTENSION_INIT2(pApi)
   return sqlite3Fts1Init(db);
 }
