3535ec3e10555c1b75b4854dd6c7a52b99cde816
drh <drh@noemail.net>
2013-08-06 16:56:44 +0000

    Remove unreachable branches in expr.c, replacing them with assert() and testcase() statements.
    FossilOrigin-Name: 9103c27ceb3f4023ea3a41b679a10717d3f80210

--- src/expr.c
+++ src/expr.c
@@ -1739,10 +1739,11 @@ int sqlite3CodeSubselect(
           return 0;
         }
         pEList = pExpr->x.pSelect->pEList;
-        if( pKeyInfo && ALWAYS(pEList!=0 && pEList->nExpr>0) ){ 
-          pKeyInfo->aColl[0] = sqlite3BinaryCompareCollSeq(pParse, pExpr->pLeft,
-              pEList->a[0].pExpr);
-        }
+        assert( pKeyInfo!=0 ); /* OOM will exit after sqlite3Select() */
+        assert( pEList!=0 );
+        assert( pEList->nExpr>0 );
+        pKeyInfo->aColl[0] = sqlite3BinaryCompareCollSeq(pParse, pExpr->pLeft,
+                                                         pEList->a[0].pExpr);
       }else if( ALWAYS(pExpr->x.pList!=0) ){
         /* Case 2:     expr IN (exprlist)
         **
