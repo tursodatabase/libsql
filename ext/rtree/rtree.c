14ec33f73be78acb450dfeb66bda59c1993bc7a5
dan <dan@noemail.net>
2013-11-11 19:56:35 +0000

    Fix typos in compile and run-time tests of the sqlite library version number in  rtree.c.
    FossilOrigin-Name: f58d57017199421167dae8ebc67db2f19be45082

--- ext/rtree/rtree.c
+++ ext/rtree/rtree.c
@@ -1360,8 +1360,8 @@ static int rtreeFilter(
 ** support estimatedRows. In that case this function is a no-op.
 */
 static void setEstimatedRows(sqlite3_index_info *pIdxInfo, i64 nRow){
-#if SQLITE_VERSION_NUMBER>=308002
-  if( sqlite3_libversion_number()>=300802 ){
+#if SQLITE_VERSION_NUMBER>=3008002
+  if( sqlite3_libversion_number()>=3008002 ){
     pIdxInfo->estimatedRows = nRow;
   }
 #endif
