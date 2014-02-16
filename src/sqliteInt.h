65acf373c953f387d4a5166e7becee9135d88292
mistachkin <mistachkin@noemail.net>
2014-02-16 19:20:00 +0000

    Use the modern MinGW major version macro, not the deprecated one.
    FossilOrigin-Name: 057c16ee9786efcad6372a29b02d743ff3d628ff

--- src/sqliteInt.h
+++ src/sqliteInt.h
@@ -75,7 +75,7 @@
 */
 #if !defined(_USE_32BIT_TIME_T) && !defined(_USE_64BIT_TIME_T) && \
     defined(_WIN32) && !defined(_WIN64) && \
-    defined(__MINGW32_MAJOR_VERSION) && __MINGW32_MAJOR_VERSION >= 4 && \
+    defined(__MINGW_MAJOR_VERSION) && __MINGW_MAJOR_VERSION >= 4 && \
     defined(__MSVCRT__)
 # define _USE_32BIT_TIME_T
 #endif
