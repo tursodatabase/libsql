6d36ffe8f3e45fa497c72c3c36061500932af84b
drh <drh@noemail.net>
2014-06-16 15:01:37 +0000

    Add the ".eqp" dot-command to the ".help" output in the command-line shell. This was accidently omitted when the ".eqp" command was originally implemented for SQLite 3.8.4 (check-in [e6ecf7337658624]).
    FossilOrigin-Name: b0b4c776c321ca4a792ef67efbc62351eeac247d

--- src/shell.c
+++ src/shell.c
@@ -1579,6 +1579,7 @@ static char zHelp[] =
   "                         If TABLE specified, only dump tables matching\n"
   "                         LIKE pattern TABLE.\n"
   ".echo on|off           Turn command echo on or off\n"
+  ".eqp on|off            Enable or disable automatic EXPLAIN QUERY PLAN\n"
   ".exit                  Exit this program\n"
   ".explain ?on|off?      Turn output mode suitable for EXPLAIN on or off.\n"
   "                         With no args, it turns EXPLAIN on.\n"
