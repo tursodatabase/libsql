2c3ea0694feef861c13fb4e3ff818558d4655be8
drh <drh@noemail.net>
2014-08-04 21:26:58 +0000

    Part of the change in the previous check-in was incorrect and can result in an incorrect UPDATE for WITHOUT ROWID tables.  This check-in fixes the problem.
    FossilOrigin-Name: ee5f6eae57a656d09a4b3f7fbef664b2c696ddb4

--- src/update.c
+++ src/update.c
@@ -437,9 +437,9 @@ void sqlite3Update(
       VdbeCoverageNeverTaken(v);
     }
     labelContinue = labelBreak;
-    if( pPk==0 ){
-      sqlite3VdbeAddOp2(v, OP_IsNull, regOldRowid, labelBreak); VdbeCoverage(v);
-    }
+    sqlite3VdbeAddOp2(v, OP_IsNull, pPk ? regKey : regOldRowid, labelBreak);
+    VdbeCoverageIf(v, pPk==0);
+    VdbeCoverageIf(v, pPk!=0);
   }else if( pPk ){
     labelContinue = sqlite3VdbeMakeLabel(v);
     sqlite3VdbeAddOp2(v, OP_Rewind, iEph, labelBreak); VdbeCoverage(v);
