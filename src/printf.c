4e7a479597ea919eca9fa82e304bd424dfd9eccf
drh <drh@noemail.net>
2014-09-20 00:29:20 +0000

    Only enable HAVE_STRCHRNUL by default on linux, as that is the only place it appears to work by default.
    FossilOrigin-Name: 0fac2c045f47c7735af4eb68ced81d8b43622a1f

--- src/printf.c
+++ src/printf.c
@@ -21,7 +21,7 @@
 ** the glibc version so the glibc version is definitely preferred.
 */
 #if !defined(HAVE_STRCHRNUL)
-# if defined(_GNU_SOURCE)
+# if defined(linux)
 #  define HAVE_STRCHRNUL 1
 # else
 #  define HAVE_STRCHRNUL 0
