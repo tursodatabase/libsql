8237f6d180d0233a4abac22e12e5f52f6497fd8f
drh <drh@noemail.net>
2015-03-31 17:32:06 +0000

    Change the sqlite3_log() message generated on a retry after delay in os_win to be an SQLITE_NOTICE instead of SQLITE_IOERR.
    FossilOrigin-Name: 5d533eee049e63b3700f5ef7c28c588afccde57a

--- src/os_win.c
+++ src/os_win.c
@@ -1968,7 +1968,7 @@ static int winRetryIoerr(int *pnRetry, DWORD *pError){
 */
 static void winLogIoerr(int nRetry, int lineno){
   if( nRetry ){
-    sqlite3_log(SQLITE_IOERR,
+    sqlite3_log(SQLITE_NOTICE,
       "delayed %dms for lock/sharing conflict at line %d",
       winIoerrRetryDelay*nRetry*(nRetry+1)/2, lineno
     );
