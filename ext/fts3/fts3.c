3c9a0737232d1800155bc42c9b7158487069a42a
dan <dan@noemail.net>
2015-04-21 12:06:53 +0000

    Fix an fts4 problem to do with the handling of phrases that contain zero tokens.
    FossilOrigin-Name: caba5be0b2bdaf4eb4af9af267c0c22dfbd3dba5

--- ext/fts3/fts3.c
+++ ext/fts3/fts3.c
@@ -4617,12 +4617,14 @@ static void fts3EvalStartReaders(
 ){
   if( pExpr && SQLITE_OK==*pRc ){
     if( pExpr->eType==FTSQUERY_PHRASE ){
-      int i;
       int nToken = pExpr->pPhrase->nToken;
-      for(i=0; i<nToken; i++){
-        if( pExpr->pPhrase->aToken[i].pDeferred==0 ) break;
+      if( nToken ){
+        int i;
+        for(i=0; i<nToken; i++){
+          if( pExpr->pPhrase->aToken[i].pDeferred==0 ) break;
+        }
+        pExpr->bDeferred = (i==nToken);
       }
-      pExpr->bDeferred = (i==nToken);
       *pRc = fts3EvalPhraseStart(pCsr, 1, pExpr->pPhrase);
     }else{
       fts3EvalStartReaders(pCsr, pExpr->pLeft, pRc);
