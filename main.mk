e712acb64141425120e9913651658d97a02bf5d1
dan <dan@noemail.net>
2015-09-10 10:40:00 +0000

    Revert an accidentally committed makefile change.
    FossilOrigin-Name: 402704b13f1f246c0224f90862bed93a825575f1

--- main.mk
+++ main.mk
@@ -332,9 +332,8 @@ TESTSRC += \
   $(TOP)/ext/misc/vfslog.c \
   $(TOP)/ext/fts5/fts5_tcl.c \
   $(TOP)/ext/fts5/fts5_test_mi.c \
-  $(FTS5_SRC)
+  fts5.c
 
-#  fts5.c
 
 
 #TESTSRC += $(TOP)/ext/fts2/fts2_tokenizer.c
