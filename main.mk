3ed7029921c201a6dc9a0861497b782d392ac47c
dan <dan@noemail.net>
2016-01-16 21:06:41 +0000

    Add extra tests to cover untested branches in fts5.
    FossilOrigin-Name: 61deab043dcea860070dba6b02601a7de627fff1

--- main.mk
+++ main.mk
@@ -334,8 +334,7 @@ TESTSRC += \
   $(TOP)/ext/misc/vfslog.c \
   $(TOP)/ext/fts5/fts5_tcl.c \
   $(TOP)/ext/fts5/fts5_test_mi.c \
-  $(TOP)/ext/fts5/fts5_test_tok.c \
-  $(FTS5_SRC)
+  $(TOP)/ext/fts5/fts5_test_tok.c 
 
 
 #TESTSRC += $(TOP)/ext/fts2/fts2_tokenizer.c
