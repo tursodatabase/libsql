4786cf5a20ded9637e487a9dd93ea104c8fb46ef
dan <dan@noemail.net>
2016-02-04 19:45:19 +0000

    Further improve performance of unindexed fts5 prefix queries.
    FossilOrigin-Name: c9c6457d8ea911f6cc63967127e58da3146fd3ef

--- ext/fts5/fts5_index.c
+++ ext/fts5/fts5_index.c
@@ -4705,76 +4705,94 @@ static void fts5MergePrefixLists(
   Fts5Buffer *p2                  /* Second list to merge */
 ){
   if( p2->n ){
-    i64 iLastRowid = 0;
-    Fts5DoclistIter i1;
-    Fts5DoclistIter i2;
-    Fts5Buffer out;
-    Fts5Buffer tmp;
-    memset(&out, 0, sizeof(out));
-    memset(&tmp, 0, sizeof(tmp));
+    if( p1->n==0 ){
+      fts5BufferSwap(p1, p2);
+    }else{
+      i64 iLastRowid = 0;
+      Fts5DoclistIter i1;
+      Fts5DoclistIter i2;
+      Fts5Buffer out = {0, 0, 0};
+      Fts5Buffer tmp = {0, 0, 0};
 
-    sqlite3Fts5BufferSize(&p->rc, &out, p1->n + p2->n);
-    fts5DoclistIterInit(p1, &i1);
-    fts5DoclistIterInit(p2, &i2);
-    while( p->rc==SQLITE_OK && (i1.aPoslist!=0 || i2.aPoslist!=0) ){
-      if( i2.aPoslist==0 || (i1.aPoslist && i1.iRowid<i2.iRowid) ){
-        /* Copy entry from i1 */
-        fts5MergeAppendDocid(&out, iLastRowid, i1.iRowid);
-        fts5BufferSafeAppendBlob(&out, i1.aPoslist, i1.nPoslist+i1.nSize);
-        fts5DoclistIterNext(&i1);
-      }
-      else if( i1.aPoslist==0 || i2.iRowid!=i1.iRowid ){
-        /* Copy entry from i2 */
-        fts5MergeAppendDocid(&out, iLastRowid, i2.iRowid);
-        fts5BufferSafeAppendBlob(&out, i2.aPoslist, i2.nPoslist+i2.nSize);
-        fts5DoclistIterNext(&i2);
-      }
-      else{
-        i64 iPos1 = 0;
-        i64 iPos2 = 0;
-        int iOff1 = 0;
-        int iOff2 = 0;
-        u8 *a1 = &i1.aPoslist[i1.nSize];
-        u8 *a2 = &i2.aPoslist[i2.nSize];
+      if( sqlite3Fts5BufferSize(&p->rc, &out, p1->n + p2->n) ) return;
+      fts5DoclistIterInit(p1, &i1);
+      fts5DoclistIterInit(p2, &i2);
 
-        Fts5PoslistWriter writer;
-        memset(&writer, 0, sizeof(writer));
+      while( 1 ){
 
-        /* Merge the two position lists. */ 
-        fts5MergeAppendDocid(&out, iLastRowid, i2.iRowid);
-        fts5BufferZero(&tmp);
+        if( i1.iRowid<i2.iRowid ){
+          /* Copy entry from i1 */
+          fts5MergeAppendDocid(&out, iLastRowid, i1.iRowid);
+          fts5BufferSafeAppendBlob(&out, i1.aPoslist, i1.nPoslist+i1.nSize);
+          fts5DoclistIterNext(&i1);
+          if( i1.aPoslist==0 ) break;
+        }
+        else if( i2.iRowid!=i1.iRowid ){
+          /* Copy entry from i2 */
+          fts5MergeAppendDocid(&out, iLastRowid, i2.iRowid);
+          fts5BufferSafeAppendBlob(&out, i2.aPoslist, i2.nPoslist+i2.nSize);
+          fts5DoclistIterNext(&i2);
+          if( i2.aPoslist==0 ) break;
+        }
+        else{
+          i64 iPos1 = 0;
+          i64 iPos2 = 0;
+          int iOff1 = 0;
+          int iOff2 = 0;
+          u8 *a1 = &i1.aPoslist[i1.nSize];
+          u8 *a2 = &i2.aPoslist[i2.nSize];
 
-        sqlite3Fts5PoslistNext64(a1, i1.nPoslist, &iOff1, &iPos1);
-        sqlite3Fts5PoslistNext64(a2, i2.nPoslist, &iOff2, &iPos2);
+          Fts5PoslistWriter writer;
+          memset(&writer, 0, sizeof(writer));
 
-        while( p->rc==SQLITE_OK && (iPos1>=0 || iPos2>=0) ){
-          i64 iNew;
-          if( iPos2<0 || (iPos1>=0 && iPos1<iPos2) ){
-            iNew = iPos1;
-            sqlite3Fts5PoslistNext64(a1, i1.nPoslist, &iOff1, &iPos1);
-          }else{
-            iNew = iPos2;
-            sqlite3Fts5PoslistNext64(a2, i2.nPoslist, &iOff2, &iPos2);
-            if( iPos1==iPos2 ){
-              sqlite3Fts5PoslistNext64(a1, i1.nPoslist, &iOff1,&iPos1);
+          /* Merge the two position lists. */ 
+          fts5MergeAppendDocid(&out, iLastRowid, i2.iRowid);
+          fts5BufferZero(&tmp);
+
+          sqlite3Fts5PoslistNext64(a1, i1.nPoslist, &iOff1, &iPos1);
+          sqlite3Fts5PoslistNext64(a2, i2.nPoslist, &iOff2, &iPos2);
+
+          while( iPos1>=0 || iPos2>=0 ){
+            i64 iNew;
+            if( iPos2<0 || (iPos1>=0 && iPos1<iPos2) ){
+              iNew = iPos1;
+              sqlite3Fts5PoslistNext64(a1, i1.nPoslist, &iOff1, &iPos1);
+            }else{
+              iNew = iPos2;
+              sqlite3Fts5PoslistNext64(a2, i2.nPoslist, &iOff2, &iPos2);
+              if( iPos1==iPos2 ){
+                sqlite3Fts5PoslistNext64(a1, i1.nPoslist, &iOff1,&iPos1);
+              }
+            }
+            if( iNew!=writer.iPrev || tmp.n==0 ){
+              p->rc = sqlite3Fts5PoslistWriterAppend(&tmp, &writer, iNew);
+              if( p->rc ) goto error_out;
             }
           }
-          if( iNew!=writer.iPrev || tmp.n==0 ){
-            p->rc = sqlite3Fts5PoslistWriterAppend(&tmp, &writer, iNew);
-          }
+
+          /* WRITEPOSLISTSIZE */
+          fts5BufferSafeAppendVarint(&out, tmp.n * 2);
+          fts5BufferSafeAppendBlob(&out, tmp.p, tmp.n);
+          fts5DoclistIterNext(&i1);
+          fts5DoclistIterNext(&i2);
+          if( i1.aPoslist==0 || i2.aPoslist==0 ) break;
         }
-
-        /* WRITEPOSLISTSIZE */
-        fts5BufferSafeAppendVarint(&out, tmp.n * 2);
-        fts5BufferSafeAppendBlob(&out, tmp.p, tmp.n);
-        fts5DoclistIterNext(&i1);
-        fts5DoclistIterNext(&i2);
       }
-    }
 
-    fts5BufferSet(&p->rc, p1, out.n, out.p);
-    fts5BufferFree(&tmp);
-    fts5BufferFree(&out);
+      if( i1.aPoslist ){
+        fts5MergeAppendDocid(&out, iLastRowid, i1.iRowid);
+        fts5BufferSafeAppendBlob(&out, i1.aPoslist, i1.aEof - i1.aPoslist);
+      }
+      else if( i2.aPoslist ){
+        fts5MergeAppendDocid(&out, iLastRowid, i2.iRowid);
+        fts5BufferSafeAppendBlob(&out, i2.aPoslist, i2.aEof - i2.aPoslist);
+      }
+
+     error_out:
+      fts5BufferSet(&p->rc, p1, out.n, out.p);
+      fts5BufferFree(&tmp);
+      fts5BufferFree(&out);
+    }
   }
 }
 
