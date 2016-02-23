0e0089679fa2a5f191aedde660512d4b5c95d0d1
drh <drh@noemail.net>
2016-02-23 16:28:34 +0000

    Add the SQLITE_BITMASK_TYPE compile-time option.
    FossilOrigin-Name: 0064a8c77b2b048c71277d1cfd1ba3975b513b70

--- src/sqliteInt.h
+++ src/sqliteInt.h
@@ -2338,7 +2338,11 @@ struct IdList {
 ** tables in a join to 32 instead of 64.  But it also reduces the size
 ** of the library by 738 bytes on ix86.
 */
-typedef u64 Bitmask;
+#ifdef SQLITE_BITMASK_TYPE
+  typedef SQLITE_BITMASK_TYPE Bitmask;
+#else
+  typedef u64 Bitmask;
+#endif
 
 /*
 ** The number of bits in a Bitmask.  "BMS" means "BitMask Size".
