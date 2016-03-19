1e8dae0e438a20211f619def9c5ce908a1787252
dan <dan@noemail.net>
2016-03-19 14:53:36 +0000

    Fix test scripts sqldiff.test and rbudiff.test so that they work with the --testdir option.
    FossilOrigin-Name: 1ffe3cde03f924bb8405a8729c8e1bc01f5b6d3b

--- test/tester.tcl
+++ test/tester.tcl
@@ -2136,15 +2136,11 @@ proc test_restore_config_pagecache {} {
   sqlite3 db test.db
 }
 
-# Find the name of the 'shell' executable (e.g. "sqlite3.exe") to use for
-# the tests in shell[1-5].test. If no such executable can be found, invoke
-# [finish_test ; return] in the callers context.
-#
-proc test_find_cli {} {
+proc test_find_binary {nm} {
   if {$::tcl_platform(platform)=="windows"} {
-    set ret "sqlite3.exe"
+    set ret "$nm.exe"
   } else {
-    set ret "sqlite3"
+    set ret $nm
   }
   set ret [file normalize [file join $::cmdlinearg(TESTFIXTURE_HOME) $ret]]
   if {![file executable $ret]} {
@@ -2153,6 +2149,23 @@ proc test_find_cli {} {
   }
   return $ret
 }
+
+# Find the name of the 'shell' executable (e.g. "sqlite3.exe") to use for
+# the tests in shell[1-5].test. If no such executable can be found, invoke
+# [finish_test ; return] in the callers context.
+#
+proc test_find_cli {} {
+  uplevel test_find_binary sqlite3
+}
+
+# Find the name of the 'sqldiff' executable (e.g. "sqlite3.exe") to use for
+# the tests in sqldiff tests. If no such executable can be found, invoke
+# [finish_test ; return] in the callers context.
+#
+proc test_find_sqldiff {} {
+  uplevel test_find_binary sqldiff
+}
+
 
 # If the library is compiled with the SQLITE_DEFAULT_AUTOVACUUM macro set
 # to non-zero, then set the global variable $AUTOVACUUM to 1.
