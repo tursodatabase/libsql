75ab50ce8ff0821bae0989a86f037024fd6a8711
drh <drh@noemail.net>
2016-04-28 14:15:12 +0000

    Use comments to mark several branches as optimizations.  No changes to code.
    FossilOrigin-Name: 33e627472780b872716c504f2d585cc057c390a5

--- src/util.c
+++ src/util.c
@@ -1419,7 +1419,7 @@ LogEst sqlite3LogEst(u64 x){
     if( x<2 ) return 0;
     while( x<8 ){  y -= 10; x <<= 1; }
   }else{
-    while( x>255 ){ y += 40; x >>= 4; }
+    while( x>255 ){ y += 40; x >>= 4; }  /*OPTIMIZATION-IF-TRUE*/
     while( x>15 ){  y += 10; x >>= 1; }
   }
   return a[x&7] + y - 10;
