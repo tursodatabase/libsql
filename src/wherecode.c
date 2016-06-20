2b693d63e43b747169be6dcd4541a678eeb9b860
dan <dan@noemail.net>
2016-06-20 17:22:06 +0000

    Allow LIKE operators that appear in a WHERE clause to be included in the cursor-hint for a cursor on the rhs of a LEFT JOIN.
    FossilOrigin-Name: 7455d932f5079ffe40462a8c119fc22b8a9bcbcc

--- src/wherecode.c
+++ src/wherecode.c
@@ -642,12 +642,19 @@ static int codeCursorHintCheckExpr(Walker *pWalker, Expr *pExpr){
 **   CASE WHEN col THEN 0 ELSE 1 END
 */
 static int codeCursorHintIsOrFunction(Walker *pWalker, Expr *pExpr){
-  if( pExpr->op==TK_IS || pExpr->op==TK_FUNCTION 
+  if( pExpr->op==TK_IS 
    || pExpr->op==TK_ISNULL || pExpr->op==TK_ISNOT 
    || pExpr->op==TK_NOTNULL || pExpr->op==TK_CASE 
   ){
     pWalker->eCode = 1;
+  }else if( pExpr->op==TK_FUNCTION ){
+    int d1;
+    char d2[3];
+    if( 0==sqlite3IsLikeFunction(pWalker->pParse->db, pExpr, &d1, d2) ){
+      pWalker->eCode = 1;
+    }
   }
+
   return WRC_Continue;
 }
 
