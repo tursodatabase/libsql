64a2a7582ef2bf63cd91bf89b14109815720efb2
drh <drh@noemail.net>
2016-07-29 14:23:23 +0000

    Use __sync_synchronous(), when available, instead of a mutex when a memory barrier is needed.
    FossilOrigin-Name: 7d4562e9d577e21406e05300b9a1ad56fded2309

--- src/os_unix.c
+++ src/os_unix.c
@@ -4629,8 +4629,12 @@ static void unixShmBarrier(
   sqlite3_file *fd                /* Database file holding the shared memory */
 ){
   UNUSED_PARAMETER(fd);
+#if defined(__GNUC__) && GCC_VERSION>=4001000
+  __sync_synchronize();
+#else
   unixEnterMutex();
   unixLeaveMutex();
+#endif
 }
 
 /*
