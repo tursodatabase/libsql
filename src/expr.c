9f24b53dcccf022ee9290f7c38872e288b1eaaa3
drh <drh@noemail.net>
2016-09-05 22:50:48 +0000

    Simplified logic to extract a column from a row value.
    FossilOrigin-Name: e8f105c3009e9b667db2afc0088b020a2b94d370

--- src/expr.c
+++ src/expr.c
@@ -371,9 +371,8 @@ int sqlite3ExprVectorSize(Expr *pExpr){
 Expr *sqlite3VectorFieldSubexpr(Expr *pVector, int i){
   assert( i<sqlite3ExprVectorSize(pVector) );
   if( sqlite3ExprIsVector(pVector) ){
-    if( pVector->op==TK_SELECT
-     || (pVector->op==TK_REGISTER && pVector->op2==TK_SELECT)
-    ){
+    assert( pVector->op2==0 || pVector->op==TK_REGISTER );
+    if( pVector->op==TK_SELECT || pVector->op2==TK_SELECT ){
       return pVector->x.pSelect->pEList->a[i].pExpr;
     }else{
       return pVector->x.pList->a[i].pExpr;
