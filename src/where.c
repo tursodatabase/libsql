c436a03d027f27d33f6ba5a95724b0a9c4e814e9
drh <drh@noemail.net>
2016-10-12 18:55:53 +0000

    Avoid reading the -1-th element of an array in the query planner.  Fix to a bug introduced by check-in [8e2b25f9b8a7] from earlier today.  Curiously, the problem only appeared on 32-bit systems.
    FossilOrigin-Name: 443913d582bcd953d85159047541592e2f68ade3

--- src/where.c
+++ src/where.c
@@ -4123,19 +4123,21 @@ static int wherePathSolver(WhereInfo *pWInfo, LogEst nRowEst){
       pWInfo->nOBSat = pFrom->isOrdered;
       pWInfo->revMask = pFrom->revLoop;
       if( pWInfo->nOBSat<=0 ){
-        u32 wsFlags = pFrom->aLoop[nLoop-1]->wsFlags;
         pWInfo->nOBSat = 0;
-        if( nLoop>0 && (wsFlags & WHERE_ONEROW)==0 
-         && (wsFlags & (WHERE_IPK|WHERE_COLUMN_IN))!=(WHERE_IPK|WHERE_COLUMN_IN)
-        ){
-          Bitmask m = 0;
-          int rc = wherePathSatisfiesOrderBy(pWInfo, pWInfo->pOrderBy, pFrom,
+        if( nLoop>0 ){
+          u32 wsFlags = pFrom->aLoop[nLoop-1]->wsFlags;
+          if( (wsFlags & WHERE_ONEROW)==0 
+           && (wsFlags&(WHERE_IPK|WHERE_COLUMN_IN))!=(WHERE_IPK|WHERE_COLUMN_IN)
+          ){
+            Bitmask m = 0;
+            int rc = wherePathSatisfiesOrderBy(pWInfo, pWInfo->pOrderBy, pFrom,
                       WHERE_ORDERBY_LIMIT, nLoop-1, pFrom->aLoop[nLoop-1], &m);
-          testcase( wsFlags & WHERE_IPK );
-          testcase( wsFlags & WHERE_COLUMN_IN );
-          if( rc==pWInfo->pOrderBy->nExpr ){
-            pWInfo->bOrderedInnerLoop = 1;
-            pWInfo->revMask = m;
+            testcase( wsFlags & WHERE_IPK );
+            testcase( wsFlags & WHERE_COLUMN_IN );
+            if( rc==pWInfo->pOrderBy->nExpr ){
+              pWInfo->bOrderedInnerLoop = 1;
+              pWInfo->revMask = m;
+            }
           }
         }
       }
