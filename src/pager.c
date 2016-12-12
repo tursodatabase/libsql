6606586d818a4547cf77358f4ea1b05aca636b16
drh <drh@noemail.net>
2016-12-12 11:05:15 +0000

    Performance optimization in sqlite3PagerWrite().
    FossilOrigin-Name: d4dff10a3d4edf8e1cc77c0a4f185d33f2c2e953

--- src/pager.c
+++ src/pager.c
@@ -5961,11 +5961,11 @@ int sqlite3PagerWrite(PgHdr *pPg){
   assert( (pPg->flags & PGHDR_MMAP)==0 );
   assert( pPager->eState>=PAGER_WRITER_LOCKED );
   assert( assert_pager_state(pPager) );
-  if( pPager->errCode ){
-    return pPager->errCode;
-  }else if( (pPg->flags & PGHDR_WRITEABLE)!=0 && pPager->dbSize>=pPg->pgno ){
+  if( (pPg->flags & PGHDR_WRITEABLE)!=0 && pPager->dbSize>=pPg->pgno ){
     if( pPager->nSavepoint ) return subjournalPageIfRequired(pPg);
     return SQLITE_OK;
+  }else if( pPager->errCode ){
+    return pPager->errCode;
   }else if( pPager->sectorSize > (u32)pPager->pageSize ){
     assert( pPager->tempFile==0 );
     return pagerWriteLargeSector(pPg);
