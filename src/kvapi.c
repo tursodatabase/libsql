42345c1e4af0c3c1e56699e6d06073fb8a131cc2
drh <drh@noemail.net>
2017-01-18 19:54:07 +0000

    Begin fleshing out the key/value accessor implementation.  This is an incremental check-in.
    FossilOrigin-Name: 02d60e196fe2a07ef90a5cfad0daa46020bfd06e

--- src/kvapi.c
+++ src/kvapi.c
@@ -32,17 +32,75 @@ struct sqlite3_kv {
   sqlite3_int64 iRowid;   /* Current rowid */
 };
 
+/*
+** Create a new sqlite3_kv object open on zDb.zTable and return
+** a pointer to that object.
+*/
 int sqlite3_kv_open(
-  sqlite3 *db,
-  const char *zDb,
-  const char *zTable,
+  sqlite3 *db,          /* The database connection */
+  const char *zDb,      /* Schema containing zTable.  NULL for "main" */
+  const char *zTable,   /* Name of table the key/value table */
   unsigned int flags,   /* Must be zero.  Reserved for future expansion. */
-  sqlite3_kv *pKvOut
+  sqlite3_kv **ppKvOut  /* Store the new sqlite3_kv object here */
 ){
-  return SQLITE_MISUSE;
+  sqlite3_kv *pKv;
+  Table *pTab;
+  int rc = SQLITE_ERROR;
+
+#ifdef SQLITE_ENABLE_API_ARMOR
+  if( ppKvOut==0 ){
+    return SQLITE_MISUSE_BKPT;
+  }
+#endif
+  *ppKvOut = 0;
+#ifdef SQLITE_ENABLE_API_ARMOR
+  if( !sqlite3SafetyCheckOk(db) || zTable==0 ){
+    return SQLITE_MISUSE_BKPT;
+  }
+#endif
+  sqlite3_mutex_enter(db->mutex);
+  sqlite3BtreeEnterAll(db);
+
+  pTab = sqlite3FindTable(db, zTable, zDb);
+  if( pTab==0 ){
+    goto kv_open_done;
+  }
+  if( !((pTab->nCol==1 && pTab->iPKey<0)
+        || (pTab->nCol==2 && pTab->iPKey==0)) 
+  ){
+    /* Must be an single-column table without an INTEGER PRIMARY KEY,
+    ** or a two-column table where the first column is the INTEGER PRIMARY KEY
+    */
+    goto kv_open_done;
+  }
+  if( pTab->pIndex!=0 || pTab->pFKey!=0 || pTab->pCheck!=0 ){
+    /* Do not allow secondary indexes, foreign keys, or CHECK constraints */
+    goto kv_open_done;
+  }
+  if( pTab->tabFlags & (TF_Autoincrement|TF_Virtual|TF_WithoutRowid) ){
+    /* Must not have autoincrement.  Must not be a virtual table or a
+    ** without rowid table */
+    goto kv_open_done;
+  }
+  *ppKvOut = pKv = sqlite3_malloc(sizeof(*pKv));
+  if( pKv==0 ){
+    rc = SQLITE_NOMEM;
+    goto kv_open_done;
+  }
+  pKv->db = db;
+  rc = SQLITE_OK;
+
+kv_open_done:
+  sqlite3BtreeLeaveAll(db);
+  sqlite3_mutex_leave(db->mutex);
+  return rc;
 }
 
+/*
+** Free the key/value accessor at pKv
+*/
 int sqlite3_kv_close(sqlite3_kv *pKv){
+  sqlite3_free(pKv);
   return SQLITE_OK;
 }
 
