c8b8c37478af060849b6a2123065240d26a71f5c
John Gallagher <jgallagher@bignerdranch.com>
2017-01-23 20:17:14 -0500

    Don't ask to link to /usr/lib directly if pkg-config fails to find SQLite.

--- rusqlite/libsqlite3-sys/build.rs
+++ rusqlite/libsqlite3-sys/build.rs
@@ -4,27 +4,24 @@ extern crate pkg_config;
 #[cfg(not(feature = "bundled"))]
 fn main() {
     use std::env;
-    use std::fs;
 
     // Allow users to specify where to find SQLite.
-    let lib_dir = match env::var("SQLITE3_LIB_DIR") {
-        Ok(dir) => dir,
+    match env::var("SQLITE3_LIB_DIR") {
+        Ok(dir) => {
+            println!("cargo:rustc-link-lib=sqlite3");
+            println!("cargo:rustc-link-search={}", dir);
+        }
         Err(_) => {
             // See if pkg-config can do everything for us.
-            if pkg_config::find_library("sqlite3").is_ok() {
-                return
+            if !pkg_config::find_library("sqlite3").is_ok() {
+                // No env var set and pkg-config couldn't help; just output the link-lib
+                // request and hope that the library exists on the system paths. We used to
+                // output /usr/lib explicitly, but that can introduce other linking problems; see
+                // https://github.com/jgallagher/rusqlite/issues/207.
+                println!("cargo:rustc-link-lib=sqlite3");
             }
-
-            // Try to fall back to /usr/lib if pkg-config failed.
-            match fs::metadata("/usr/lib") {
-                Ok(ref attr) if attr.is_dir() => "/usr/lib".to_owned(),
-                _ => panic!("Could not find sqlite3. Try setting SQLITE3_LIB_DIR."),
-            }
-        },
+        }
     };
-
-    println!("cargo:rustc-link-lib=sqlite3");
-    println!("cargo:rustc-link-search={}", lib_dir);
 }
 
 #[cfg(feature = "bundled")]
