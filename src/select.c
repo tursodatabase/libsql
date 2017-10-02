878fcf9d4ef72b8cf2eebd79566ac6ccf9f191f6
drh <drh@noemail.net>
2017-10-02 23:50:08 +0000

    Mark a single branch within sqlite3SelectExpand() as an optimization.
    FossilOrigin-Name: 3a4ffb21d0ee2326c31b9ddc512d79d4ebf3a249a2840e88e99165978eec31fd

--- src/select.c
+++ src/select.c
@@ -4645,7 +4645,7 @@ static void sqlite3SelectExpand(Parse *pParse, Select *pSelect){
   Walker w;
   w.xExprCallback = sqlite3ExprWalkNoop;
   w.pParse = pParse;
-  if( pParse->hasCompound ){
+  if( OK_IF_ALWAYS_TRUE(pParse->hasCompound) ){
     w.xSelectCallback = convertCompoundSelectToSubquery;
     w.xSelectCallback2 = 0;
     sqlite3WalkSelect(&w, pSelect);
