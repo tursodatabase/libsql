e2463398fd1154581c078347b5000201b54f037e
drh <drh@noemail.net>
2017-10-03 14:24:24 +0000

    The simplification to sqlite3SelectExpand() in last night's [3a4ffb21] check-in was not completely correct.  This adjustment is needed for correct OOM handling in some configurations.
    FossilOrigin-Name: c5ad5e1675f6cb5a54df21606dfec2198a7cf88acc529460ebe27bdab128a6e5

--- src/select.c
+++ src/select.c
@@ -4729,7 +4729,8 @@ void sqlite3SelectPrep(
   Select *p,             /* The SELECT statement being coded. */
   NameContext *pOuterNC  /* Name context for container */
 ){
-  assert( p!=0 );
+  assert( p!=0 || pParse->db->mallocFailed );
+  if( pParse->db->mallocFailed ) return;
   if( p->selFlags & SF_HasTypeInfo ) return;
   sqlite3SelectExpand(pParse, p);
   if( pParse->nErr || pParse->db->mallocFailed ) return;
