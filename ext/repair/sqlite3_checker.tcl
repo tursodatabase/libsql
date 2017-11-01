4e8ad3bc1435e5202d1a59a9dd3e7eabfaff9469
drh <drh@noemail.net>
2017-11-01 00:10:34 +0000

    Begin putting functionality into the sqlite3_checker binary.
    FossilOrigin-Name: e82e883b93128e4d1105a82abe8d1860c0a15505b6ca421e187b9bbbc2fdc659

--- ext/repair/sqlite3_checker.tcl
+++ ext/repair/sqlite3_checker.tcl
@@ -4,6 +4,21 @@
 # program.
 #
 
+# Special case:
+#
+#      sqlite3_checker --test FILENAME ARGS
+#
+# uses FILENAME in place of this script.
+#
+if {[lindex $argv 0]=="--test" && [llength $argv]>2} {
+  set file [lindex $argv 1]
+  set argv [lrange $argv 2 end]
+  source $file
+  exit 0
+}
+
+# Emulate a TCL shell
+#
 proc tclsh {} {
   set line {}
   while {![eof stdin]} {
@@ -27,6 +42,8 @@ proc tclsh {} {
   }
 }
 
+# Print a usage message on standard error, then quit.
+#
 proc usage {} {
   set argv0 [file rootname [file tail [info nameofexecutable]]]
   puts stderr "Usage: $argv0 OPTIONS database-filename"
@@ -35,6 +52,8 @@ Do sanity checking on a live SQLite3 database file specified by the
 "database-filename" argument.
 
 Options:
+
+   --freelist   Perform a freelist check
 
    --tclsh      Run the built-in TCL interpreter interactively (for debugging)
 
@@ -45,6 +64,8 @@ Options:
 
 set file_to_analyze {}
 append argv {}
+set bFreelistCheck 0
+set bSummary 1
 foreach arg $argv {
   if {[regexp {^-+tclsh$} $arg]} {
     tclsh
@@ -55,6 +76,11 @@ foreach arg $argv {
     puts [mem one {SELECT sqlite_version()||' '||sqlite_source_id()}]
     mem close
     exit 0
+  }
+  if {[regexp {^-+freelist$} $arg]} {
+    set bFreelistCheck 1
+    set bSummary 0
+    continue
   }
   if {[regexp {^-} $arg]} {
     puts stderr "Unknown option: $arg"
@@ -85,4 +111,35 @@ if {![file exists $root_filename]} {
 if {![file readable $root_filename]} {
   puts stderr "File is not readable: $root_filename"
   exit 1
+}
+
+if {[catch {sqlite3 db $file_to_analyze} res]} {
+  puts stderr "Cannot open datababase $root_filename: $res"
+  exit 1
+}
+
+if {$bFreelistCheck} {
+  puts "freelist-check:"
+  flush stdout
+  puts [db one {SELECT checkfreelist('main')}]
+}
+if {$bSummary} {
+  set scale 0
+  set pgsz [db one {PRAGMA page_size}]
+  db eval {SELECT nPage*$pgsz AS sz, name, tbl_name
+             FROM sqlite_btreeinfo
+            WHERE type='index'
+            ORDER BY 1 DESC, name} {
+    if {$scale==0} {
+      if {$sz>10000000} {
+        set scale 1000000.0
+        set unit MB
+      } else {
+        set scale 1000.0
+        set unit KB
+      }
+    }
+    puts [format {%7.1f %s index %s of table %s} \
+            [expr {$sz/$scale}] $unit $name $tbl_name]
+  }
 }
