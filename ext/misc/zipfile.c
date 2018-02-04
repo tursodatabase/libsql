001f52ed7d2ecd3b9c28a662234193b121318652
mistachkin <mistachkin@noemail.net>
2018-02-04 01:29:01 +0000

    Move variable declaration in zipfile extension to fix MSVC compiler error.
    FossilOrigin-Name: ac6a32482e675871c3ce5ecc727e50e4bba649e80e3c55bf99025fed5667da94

--- ext/misc/zipfile.c
+++ ext/misc/zipfile.c
@@ -701,6 +701,7 @@ static u32 zipfileMtime(ZipfileCDS *pCDS){
   int sec = (pCDS->mTime & 0x1F)*2;
   int min = (pCDS->mTime >> 5) & 0x3F;
   int hr = (pCDS->mTime >> 11) & 0x1F;
+  i64 JD;
 
   /* JD = INT(365.25 * (Y+4716)) + INT(30.6001 * (M+1)) + D + B - 1524.5 */
 
@@ -709,7 +710,7 @@ static u32 zipfileMtime(ZipfileCDS *pCDS){
     Y = Y-1;
     M = M+12;
   }
-  i64 JD = (i64)(24*60*60) * (
+  JD = (i64)(24*60*60) * (
       (int)(365.25 * (Y + 4716))
     + (int)(30.6001 * (M + 1))
     + D + B - 1524
