f2379a22bcb8ce249b0e62da3fae23c22fe586e0
gwenn <gtreguier@gmail.com>
2018-02-10 11:11:51 +0100

    Fix upgrade script
    Avoid cleaning the whole target dir

--- rusqlite/libsqlite3-sys/upgrade.sh
+++ rusqlite/libsqlite3-sys/upgrade.sh
@@ -16,7 +16,7 @@ rm -f $SQLITE3_LIB_DIR/bindgen_bundled_version.rs
 SQLITE3_INCLUDE_DIR=$SQLITE3_LIB_DIR
 cargo update
 # Just to make sure there is only one bingen.rs file in target dir
-cargo clean
+find $SCRIPT_DIR/target -type f -name bindgen.rs -exec rm {} \;
 cargo build --features "buildtime_bindgen" --no-default-features
 find $SCRIPT_DIR/target -type f -name bindgen.rs -exec cp {} $SQLITE3_LIB_DIR/bindgen_bundled_version.rs \;
 # Sanity check
