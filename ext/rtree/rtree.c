338e311acb213a772927bac361c7a80b0bf51632
drh <drh@noemail.net>
2018-05-24 23:51:57 +0000

    When doing a one-pass UPDATE or DELETE on virtual tables, close the cursor prior to running VUpdate.  This allows one-pass to work on virtual tables that do not allow concurrent reads and writes.  Enhance rtree to take advantage of this new capability.
    FossilOrigin-Name: b816023ce07d01024d5769e16db924374a49bf909edd12dc1344a0a1ef693db5

--- ext/rtree/rtree.c
+++ ext/rtree/rtree.c
@@ -1904,6 +1904,7 @@ static int rtreeBestIndex(sqlite3_vtab *tab, sqlite3_index_info *pIdxInfo){
       */ 
       pIdxInfo->estimatedCost = 30.0;
       pIdxInfo->estimatedRows = 1;
+      pIdxInfo->idxFlags = SQLITE_INDEX_SCAN_UNIQUE;
       return SQLITE_OK;
     }
 
