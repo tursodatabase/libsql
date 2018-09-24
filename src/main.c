41954375047bf4042e2f0add49ab4c906cce05b6
drh <drh@noemail.net>
2018-09-24 21:07:35 +0000

    Avoid incrementing the SQLITE_LOOKASIDE_MISS_SIZE stat before sqlite3_open() returns. Fix test script problem in lookaside.test.
    FossilOrigin-Name: 3bd94e431783085a22733102218c0fe24a26376033cf9fe4380f6890aba8c79a

--- src/main.c
+++ src/main.c
@@ -3021,6 +3021,7 @@ static int openDatabase(
   db->nDb = 2;
   db->magic = SQLITE_MAGIC_BUSY;
   db->aDb = db->aDbStatic;
+  db->lookaside.bDisable = 1;
 
   assert( sizeof(db->aLimit)==sizeof(aHardLimit) );
   memcpy(db->aLimit, aHardLimit, sizeof(db->aLimit));
