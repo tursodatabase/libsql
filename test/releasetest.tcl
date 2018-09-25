5bc4851d00d48aa4663ac4e19ded07edbf6b29ff
drh <drh@noemail.net>
2018-09-25 01:35:17 +0000

    Fix a compilation issue in the "userauth" extension. Also fix a few test script errors that occur with SQLITE_USER_AUTHENTICATION=1 builds.
    FossilOrigin-Name: e7db5f59ee62bb8bf61e17a75ecf89f7472fccf68d6d02b892e349142db1f47e

--- test/releasetest.tcl
+++ test/releasetest.tcl
@@ -136,6 +136,7 @@ array set ::Configs [strip_comments {
     -DSQLITE_ENABLE_STAT4
     -DSQLITE_ENABLE_RBU
     -DSQLITE_MAX_ATTACHED=125
+    -DSQLITE_USER_AUTHENTICATION=1
     -DLONGDOUBLE_TYPE=double
     --enable-session
   }
