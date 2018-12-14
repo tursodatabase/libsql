d6401e869baca759a169a58de148689785dc7907
drh <drh@noemail.net>
2018-12-14 13:35:48 +0000

    Expand the size of the temporary page used during btree rebalancing by a few bytes, to avoid problems with small buffer overreads that can occur on corrupted database files.
    FossilOrigin-Name: 32754ca6f86da8165e274f98f35fc3df3aebd273e9da08387e2d0c3c89abda0f

--- src/pcache1.c
+++ src/pcache1.c
@@ -477,7 +477,10 @@ static void pcache1FreePage(PgHdr1 *p){
 ** exists, this function falls back to sqlite3Malloc().
 */
 void *sqlite3PageMalloc(int sz){
-  return pcache1Alloc(sz);
+  /* During rebalance operations on a corrupt database file, it is sometimes
+  ** (rarely) possible to overread the temporary page buffer by a few bytes.
+  ** Enlarge the allocation slightly so that this does not cause problems. */
+  return pcache1Alloc(sz + 32);
 }
 
 /*
