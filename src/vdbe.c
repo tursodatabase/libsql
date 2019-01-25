0814acd98177fab44df29ebd24595a93c63eb68d
drh <drh@noemail.net>
2019-01-25 20:09:04 +0000

    Do not accidently truncate zeroblob values when doing an arithmetic operation.  Fix for ticket [bb4bdb9f7f654b0bb9f34cfba].
    FossilOrigin-Name: 13f6942eb0da2d92a0830f18640ce64208bd0cd6ff6d0c97e4a4c57ac3d65ba6

--- src/vdbe.c
+++ src/vdbe.c
@@ -385,6 +385,7 @@ void sqlite3ValueApplyAffinity(
 static u16 SQLITE_NOINLINE computeNumericType(Mem *pMem){
   assert( (pMem->flags & (MEM_Int|MEM_Real))==0 );
   assert( (pMem->flags & (MEM_Str|MEM_Blob))!=0 );
+  ExpandBlob(pMem);
   if( sqlite3AtoF(pMem->z, &pMem->u.r, pMem->n, pMem->enc)==0 ){
     return 0;
   }
