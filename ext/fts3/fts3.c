d756bbef46113bbc63c0f86084c0738637faf77e
dan <dan@noemail.net>
2019-01-26 19:09:15 +0000

    Fix a broken assert() in fts3.
    FossilOrigin-Name: b8dd2d67b5c5555d0fd8b8fc9b953d54efb2d154413a50dca795a57b44dbdec7

--- ext/fts3/fts3.c
+++ ext/fts3/fts3.c
@@ -3868,7 +3868,6 @@ static int fts3RollbackToMethod(sqlite3_vtab *pVtab, int iSavepoint){
   Fts3Table *p = (Fts3Table*)pVtab;
   UNUSED_PARAMETER(iSavepoint);
   assert( p->inTransaction );
-  assert( p->mxSavepoint >= iSavepoint );
   TESTONLY( p->mxSavepoint = iSavepoint );
   sqlite3Fts3PendingTermsClear(p);
   return SQLITE_OK;
