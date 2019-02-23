4bdd26dfab8408645b1e32e657355e332b098350
drh <drh@noemail.net>
2019-02-23 00:08:09 +0000

    Remove a testcase() macro which is now unreachable due to the contraction of the use of IN_INDEX_ROWID.
    FossilOrigin-Name: 90c5a17cd526b256753f876e575f3e731d5e8b9dd6e196cf8d8f7306e099b91c

--- src/wherecode.c
+++ src/wherecode.c
@@ -582,7 +582,6 @@ static int codeEqualityTerm(
         if( pLoop->aLTerm[i]->pExpr==pX ){
           int iOut = iReg + i - iEq;
           if( eType==IN_INDEX_ROWID ){
-            testcase( nEq>1 );  /* Happens with a UNIQUE index on ROWID */
             pIn->addrInTop = sqlite3VdbeAddOp2(v, OP_Rowid, iTab, iOut);
           }else{
             int iCol = aiMap ? aiMap[iMap++] : 0;
