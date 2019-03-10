49dbfec862117cbb308978467f1c3a933cf9451b
gwenn <gtreguier@gmail.com>
2019-03-10 13:49:21 +0100

    Upgrade bundled version to SQLite 3.27.2
    And upgrade to bindgen 0.48

--- rusqlite/libsqlite3-sys/upgrade.sh
+++ rusqlite/libsqlite3-sys/upgrade.sh
@@ -4,8 +4,8 @@ cd $SCRIPT_DIR
 export SQLITE3_LIB_DIR=$SCRIPT_DIR/sqlite3
 
 # Download and extract amalgamation
-SQLITE=sqlite-amalgamation-3260000
-curl -O http://sqlite.org/2018/$SQLITE.zip
+SQLITE=sqlite-amalgamation-3270200
+curl -O http://sqlite.org/2019/$SQLITE.zip
 unzip -p $SQLITE.zip $SQLITE/sqlite3.c > $SQLITE3_LIB_DIR/sqlite3.c
 unzip -p $SQLITE.zip $SQLITE/sqlite3.h > $SQLITE3_LIB_DIR/sqlite3.h
 unzip -p $SQLITE.zip $SQLITE/sqlite3ext.h > $SQLITE3_LIB_DIR/sqlite3ext.h
@@ -16,9 +16,9 @@ rm -f $SQLITE3_LIB_DIR/bindgen_bundled_version.rs
 export SQLITE3_INCLUDE_DIR=$SQLITE3_LIB_DIR
 cargo update
 # Just to make sure there is only one bindgen.rs file in target dir
-find $SCRIPT_DIR/target -type f -name bindgen.rs -exec rm {} \;
+find $SCRIPT_DIR/../target -type f -name bindgen.rs -exec rm {} \;
 cargo build --features "buildtime_bindgen" --no-default-features
-find $SCRIPT_DIR/target -type f -name bindgen.rs -exec cp {} $SQLITE3_LIB_DIR/bindgen_bundled_version.rs \;
+find $SCRIPT_DIR/../target -type f -name bindgen.rs -exec cp {} $SQLITE3_LIB_DIR/bindgen_bundled_version.rs \;
 # Sanity check
 cd $SCRIPT_DIR/..
 cargo update
