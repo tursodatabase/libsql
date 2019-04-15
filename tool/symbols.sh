660107f35eb9f8e74ea26f6a66c6c05d2cb1f83e
drh <drh@noemail.net>
2019-04-15 13:51:27 +0000

    Update tool/symbols.sh to ignore symbols from sessions and rebaser and to not try to compile with ICU which is not available on all platforms.
    FossilOrigin-Name: 0bab4ece6e81916d0f7255a8e553b8ca040cb8a96ab21e294212e232119f8ddc

--- tool/symbols.sh
+++ tool/symbols.sh
@@ -5,17 +5,19 @@
 #
 make sqlite3.c
 
-echo '****** Exported symbols from a build including RTREE, FTS4 & ICU ******'
+echo '****** Exported symbols from a build including RTREE, FTS4 & FTS5 ******'
 gcc -c -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_RTREE \
   -DSQLITE_ENABLE_MEMORY_MANAGEMENT -DSQLITE_ENABLE_STAT3 \
   -DSQLITE_ENABLE_MEMSYS5 -DSQLITE_ENABLE_UNLOCK_NOTIFY \
   -DSQLITE_ENABLE_COLUMN_METADATA -DSQLITE_ENABLE_ATOMIC_WRITE \
-  -DSQLITE_ENABLE_ICU -DSQLITE_ENABLE_PREUPDATE_HOOK -DSQLITE_ENABLE_SESSION \
+  -DSQLITE_ENABLE_PREUPDATE_HOOK -DSQLITE_ENABLE_SESSION \
+  -DSQLITE_ENABLE_FTS5 -DSQLITE_ENABLE_GEOPOLY \
   sqlite3.c
 nm sqlite3.o | grep ' [TD] ' | sort -k 3
 
-echo '****** Surplus symbols from a build including RTREE, FTS4 & ICU ******'
-nm sqlite3.o | grep ' [TD] ' | grep -v ' .*sqlite3_'
+echo '****** Surplus symbols from a build including RTREE, FTS4 & FTS5 ******'
+nm sqlite3.o | grep ' [TD] ' |
+   egrep -v ' .*sqlite3(session|rebaser|changeset|changegroup)?_'
 
 echo '****** Dependencies of the core. No extensions. No OS interface *******'
 gcc -c -DSQLITE_ENABLE_MEMORY_MANAGEMENT -DSQLITE_ENABLE_STAT3 \
