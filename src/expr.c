1d2d71a02cac0e4a13d54ae1bd3b3351abbf9828
drh <drh@noemail.net>
2019-04-19 23:05:56 +0000

    Mark a single branch as no longer reachable.
    FossilOrigin-Name: 4f35b3b78a461b0617a5cd04f60d2ab277bcc7c994dae463851a3cb5d041a723

--- src/expr.c
+++ src/expr.c
@@ -2079,7 +2079,7 @@ int sqlite3ExprContainsSubquery(Expr *p){
 */
 int sqlite3ExprIsInteger(Expr *p, int *pValue){
   int rc = 0;
-  if( p==0 ) return 0;  /* Can only happen following on OOM */
+  if( NEVER(p==0) ) return 0;  /* Used to only happen following on OOM */
 
   /* If an expression is an integer literal that fits in a signed 32-bit
   ** integer, then the EP_IntValue flag will have already been set */
