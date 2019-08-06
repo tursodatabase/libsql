2712b022abeb6c3d3ca0f1253bbbcca404e52b8a
dan <dan@noemail.net>
2019-08-06 21:16:28 +0000

    Ensure that when the col in an operator like "val IN(col)" is a column of a view, its affinity is not used to coerce val. Fix for [0a5e2c1d].
    FossilOrigin-Name: 17b3d2218c02a4005d4c96471c452105b54abb25901ae62990b01f0c955135fe

--- src/select.c
+++ src/select.c
@@ -3476,6 +3476,9 @@ static Expr *substExpr(
           pNew->iRightJoinTable = pExpr->iRightJoinTable;
           ExprSetProperty(pNew, EP_FromJoin);
         }
+        if( pNew && ExprHasProperty(pExpr,EP_Generic) ){
+          ExprSetProperty(pNew, EP_Generic);
+        }
         sqlite3ExprDelete(db, pExpr);
         pExpr = pNew;
       }
