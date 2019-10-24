ef2df8f3435edb6ad260ab6ebbf0f151bf0a2a48
dan <dan@noemail.net>
2019-10-24 20:35:27 +0000

    In fts5, fix a case of overreading a buffer by 1 byte when counting characters in malformed utf-8. Fix for [dd1f67bf].
    FossilOrigin-Name: 8d964e1c21d4cea699023e02b0616a75c5859dd083c9365cdcbc0676ebbdaae4

--- ext/fts5/fts5_index.c
+++ ext/fts5/fts5_index.c
@@ -5318,6 +5318,7 @@ int sqlite3Fts5IndexCharlenToBytelen(
   for(i=0; i<nChar; i++){
     if( n>=nByte ) return 0;      /* Input contains fewer than nChar chars */
     if( (unsigned char)p[n++]>=0xc0 ){
+      if( n>=nByte ) break;
       while( (p[n] & 0xc0)==0x80 ){
         n++;
         if( n>=nByte ) break;
