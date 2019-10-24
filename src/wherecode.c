4485ac1a25e9919b5911a0db2ad7bed58be143d6
drh <drh@noemail.net>
2019-10-24 21:02:06 +0000

    The previous fix was incomplete.  It is also necessary to disable the Expr.y.pTab field when making the translation.
    FossilOrigin-Name: b99d5701312f7472e6b606bd824f9273617e2655920485bf50aa96d408064721

--- src/wherecode.c
+++ src/wherecode.c
@@ -1139,6 +1139,7 @@ static int whereIndexExprTransColumn(Walker *p, Expr *pExpr){
     if( pExpr->iTable==pX->iTabCur && pExpr->iColumn==pX->iTabCol ){
       pExpr->iTable = pX->iIdxCur;
       pExpr->iColumn = pX->iIdxCol;
+      pExpr->y.pTab = 0;
     }
   }
   return WRC_Continue;
