9c9c70920bae0ccd6216ea7fa61f387fdeb0b6ae
dan <dan@noemail.net>
2019-11-21 17:14:55 +0000

    Fix a recently introduced memory leak in the test code in test_vfs.c.
    FossilOrigin-Name: 2d53a30cc23e53033af8e8666457654db5dcc453ed6cd176ce1d0bff4f528159

--- src/test_vfs.c
+++ src/test_vfs.c
@@ -1392,8 +1392,8 @@ static void SQLITE_TCLAPI testvfs_obj_del(ClientData cd){
   if( p->pScript ) Tcl_DecrRefCount(p->pScript);
   sqlite3_vfs_unregister(p->pVfs);
   memset(p->pVfs, 0, sizeof(sqlite3_vfs));
-  memset(p, 0, sizeof(Testvfs));
   ckfree((char *)p->pVfs);
+  memset(p, 0, sizeof(Testvfs));
   ckfree((char *)p);
 }
 
