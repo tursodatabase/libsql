4dad7ed532ef1a6b50e5ebf4dedf2a4611a82159
drh <drh@noemail.net>
2019-12-16 16:52:22 +0000

    When a table is part of a LEFT JOIN and should be a completely NULL row due to the semantics of a LEFT JOIN, make sure any generated columns on that row evaluate to NULL.  Ticket [3b84b42943644d6f]
    FossilOrigin-Name: 0271491438ad2a985aeff355173a8d0f1e5813954c82147bc68cb26cca5804c8

--- src/expr.c
+++ src/expr.c
@@ -3408,11 +3408,20 @@ void sqlite3ExprCodeGeneratedColumn(
   Column *pCol,
   int regOut
 ){
+  int iAddr;
+  Vdbe *v = pParse->pVdbe;
+  assert( v!=0 );
+  assert( pParse->iSelfTab!=0 );
+  if( pParse->iSelfTab>0 ){
+    iAddr = sqlite3VdbeAddOp3(v, OP_IfNullRow, pParse->iSelfTab-1, 0, regOut);
+  }else{
+    iAddr = 0;
+  }
   sqlite3ExprCode(pParse, pCol->pDflt, regOut);
   if( pCol->affinity>=SQLITE_AFF_TEXT ){
-    sqlite3VdbeAddOp4(pParse->pVdbe, OP_Affinity, regOut, 1, 0,
-                      &pCol->affinity, 1);
+    sqlite3VdbeAddOp4(v, OP_Affinity, regOut, 1, 0, &pCol->affinity, 1);
   }
+  if( iAddr ) sqlite3VdbeJumpHere(v, iAddr);
 }
 #endif /* SQLITE_OMIT_GENERATED_COLUMNS */
 
