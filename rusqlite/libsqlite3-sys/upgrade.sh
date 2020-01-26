3ee6bb052f5800542cfc33d1fbe032d32cc36e7c
Kamran Mackey <kamranm1200@gmail.com>
2020-01-26 12:44:03 -0700

    Upgrade bundled SQLite version to 3.31.0.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- rusqlite/libsqlite3-sys/upgrade.sh
+++ rusqlite/libsqlite3-sys/upgrade.sh
@@ -4,8 +4,8 @@ cd $SCRIPT_DIR
 export SQLITE3_LIB_DIR=$SCRIPT_DIR/sqlite3
 
 # Download and extract amalgamation
-SQLITE=sqlite-amalgamation-3300100
-curl -O https://sqlite.org/2019/$SQLITE.zip
+SQLITE=sqlite-amalgamation-3310000
+curl -O https://sqlite.org/2020/$SQLITE.zip
 unzip -p $SQLITE.zip $SQLITE/sqlite3.c > $SQLITE3_LIB_DIR/sqlite3.c
 unzip -p $SQLITE.zip $SQLITE/sqlite3.h > $SQLITE3_LIB_DIR/sqlite3.h
 unzip -p $SQLITE.zip $SQLITE/sqlite3ext.h > $SQLITE3_LIB_DIR/sqlite3ext.h
