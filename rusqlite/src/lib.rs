f31d9e7507d27e9120f906a493683479cd9ea75d
gwenn <gtreguier@gmail.com>
2020-03-04 20:26:31 +0100

    Fix error while executing ALTER statement
    `execute_bacth` should be used for DDL.
    `execute` should still work except when `extra-check` feature is
    activated.

--- rusqlite/src/lib.rs
+++ rusqlite/src/lib.rs
@@ -1745,5 +1745,14 @@ mod test {
             )
             .unwrap();
         }
+
+        #[test]
+        #[cfg(not(feature = "extra_check"))]
+        fn test_alter_table() {
+            let db = checked_memory_handle();
+            db.execute_batch("CREATE TABLE x(t);").unwrap();
+            // `execute_batch` should be used but `execute` should also work
+            db.execute("ALTER TABLE x RENAME TO y;", params![]).unwrap();
+        }
     }
 }
