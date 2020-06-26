ae90e62754f30978c2479a1ae330617fab49232b
gwenn <gtreguier@gmail.com>
2020-06-26 19:22:20 +0200

    Clippy

--- rusqlite/src/types/time.rs
+++ rusqlite/src/types/time.rs
@@ -23,10 +23,10 @@ impl FromSql for time::Timespec {
                 match s.len() {
                     19 => time::strptime(s, CURRENT_TIMESTAMP_FMT),
                     _ => time::strptime(s, SQLITE_DATETIME_FMT).or_else(|err| {
-                        time::strptime(s, SQLITE_DATETIME_FMT_LEGACY).or_else(|_| Err(err))
+                        time::strptime(s, SQLITE_DATETIME_FMT_LEGACY).map_err(|_| err)
                     }),
                 }
-                .or_else(|err| Err(FromSqlError::Other(Box::new(err))))
+                .map_err(|err| FromSqlError::Other(Box::new(err)))
             })
             .map(|tm| tm.to_timespec())
     }
