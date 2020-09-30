f287d0010880033a6b9745a351cac39c554fc591
drh <drh@noemail.net>
2020-09-30 00:10:22 +0000

    In the OP_SeekScan opcode, changes to pOp must occur after the call to VdbeBranchTaken() so that the VdbeBranchTaken() attributes the branch to the correct opcode.
    FossilOrigin-Name: 61fbe7aa7cc0e8fe3e6b1c5aec277183d8cd7c86b1e835c38e0b37a9b8063343

--- src/vdbe.c
+++ src/vdbe.c
@@ -4481,8 +4481,8 @@ case OP_SeekScan: {
         printf("... %d steps and then skip\n", pOp->p1 - n);
       }        
 #endif
-      pOp++;
       VdbeBranchTaken(1,3);
+      pOp++;
       goto jump_to_p2;
     }
     if( res==0 ){
@@ -4491,8 +4491,8 @@ case OP_SeekScan: {
         printf("... %d steps and then success\n", pOp->p1 - n);
       }        
 #endif
-      pOp += 2;
       VdbeBranchTaken(2,3);
+      pOp += 2;
       break;
     }
     if( n<=0 ){
