8e2b9c2a8903f3a408429d0b3749549973b83ca5
drh <drh@noemail.net>
2020-12-20 14:51:17 +0000

    Always declare the sqlite3WhereTrace variable, even for non-debug builds.
    FossilOrigin-Name: 88d93ee380b6fd87474545f20ade874ba05c784c787ce9c45ebfcffed3795308

--- src/sqliteInt.h
+++ src/sqliteInt.h
@@ -1014,9 +1014,9 @@ extern u32 sqlite3SelectTrace;
 /*
 ** Macros for "wheretrace"
 */
+extern u32 sqlite3WhereTrace;
 #if defined(SQLITE_DEBUG) \
     && (defined(SQLITE_TEST) || defined(SQLITE_ENABLE_WHERETRACE))
-extern u32 sqlite3WhereTrace;
 # define WHERETRACE(K,X)  if(sqlite3WhereTrace&(K)) sqlite3DebugPrintf X
 # define WHERETRACE_ENABLED 1
 #else
