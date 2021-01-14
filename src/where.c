5870dc80f906c5e841d330db095ec2dae6db4ce9
drh <>
2021-01-14 00:53:14 +0000

    The early-out of the inner loop on the min/max optimization was overly aggressive for the cases where there is a join and outer loops contain IN operators.  Fix this.  Test case in TH3.
    FossilOrigin-Name: ccd3bae14b6b47bb0f9622700c04db989f76ce65e10e0709964cfd0675eca762

--- src/where.c
+++ src/where.c
@@ -112,14 +112,17 @@ int sqlite3WhereOrderByLimitOptLabel(WhereInfo *pWInfo){
 */
 void sqlite3WhereMinMaxOptEarlyOut(Vdbe *v, WhereInfo *pWInfo){
   WhereLevel *pInner;
+  int i;
   if( !pWInfo->bOrderedInnerLoop ) return;
   if( pWInfo->nOBSat==0 ) return;
-  pInner = &pWInfo->a[pWInfo->nLevel-1];
-  if( (pInner->pWLoop->wsFlags & WHERE_COLUMN_IN)==0 ){
-    sqlite3VdbeGoto(v, pWInfo->iBreak);
-  }else if( pInner->addrNxt!=pWInfo->iContinue ){
-    sqlite3VdbeGoto(v, pInner->addrNxt);
+  for(i=pWInfo->nLevel-1; i>=0; i--){
+    pInner = &pWInfo->a[i];
+    if( (pInner->pWLoop->wsFlags & WHERE_COLUMN_IN)!=0 ){
+      sqlite3VdbeGoto(v, pInner->addrNxt);
+      return;
+    }
   }
+  sqlite3VdbeGoto(v, pWInfo->iBreak);
 }
 
 /*
