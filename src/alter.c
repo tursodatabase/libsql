736d11ed210a40bebe2a40bf9d3b74c856049d13
drh <>
2021-05-01 12:09:36 +0000

    Back out the NEVER() inserted by [c7309ed3c7588c7e] because it is reachable after all.  dbsqlfuzz bc17a306a09329bba0ecc61547077f6178bcf321
    FossilOrigin-Name: 20a4f3f12cdd8823d53d91025fa41e284c9f629b1d049eb486ff8dc4a1736e1f

--- src/alter.c
+++ src/alter.c
@@ -835,7 +835,7 @@ static int renameUnmapSelectCb(Walker *pWalker, Select *p){
   Parse *pParse = pWalker->pParse;
   int i;
   if( pParse->nErr ) return WRC_Abort;
-  if( NEVER(p->selFlags & SF_View) ) return WRC_Prune;
+  if( p->selFlags & SF_View ) return WRC_Prune;
   if( ALWAYS(p->pEList) ){
     ExprList *pList = p->pEList;
     for(i=0; i<pList->nExpr; i++){
