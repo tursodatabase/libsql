8b83b969b9a8931707c205360927a964a766c601
gwenn <gtreguier@gmail.com>
2021-06-12 18:29:01 +0200

    Fix FromSql impl for OffsetDateTime
    Use time 0.3.0-alpha-1

--- rusqlite/src/types/time.rs
+++ rusqlite/src/types/time.rs
@@ -1,7 +1,109 @@
 //! [`ToSql`] and [`FromSql`] implementation for [`time::OffsetDateTime`].
 use crate::types::{FromSql, FromSqlError, FromSqlResult, ToSql, ToSqlOutput, ValueRef};
-use crate::Result;
-use time::{Format, OffsetDateTime, PrimitiveDateTime, UtcOffset};
+use crate::{Error, Result};
+use time::format_description::well_known::Rfc3339;
+use time::format_description::{modifier, Component, FormatItem};
+use time::{OffsetDateTime, PrimitiveDateTime, UtcOffset};
+
+const DATE_FORMAT: &[FormatItem<'_>] = &[
+    FormatItem::Component(Component::Year(modifier::Year {
+        repr: modifier::YearRepr::Full,
+        iso_week_based: false,
+        sign_is_mandatory: false,
+        padding: modifier::Padding::Zero,
+    })),
+    FormatItem::Literal(b"-"),
+    FormatItem::Component(Component::Month(modifier::Month {
+        repr: modifier::MonthRepr::Numerical,
+        padding: modifier::Padding::Zero,
+    })),
+    FormatItem::Literal(b"-"),
+    FormatItem::Component(Component::Day(modifier::Day {
+        padding: modifier::Padding::Zero,
+    })),
+];
+
+const SHORT_TIME_FORMAT: &[FormatItem<'_>] = &[
+    FormatItem::Component(Component::Hour(modifier::Hour {
+        padding: modifier::Padding::Zero,
+        is_12_hour_clock: false,
+    })),
+    FormatItem::Literal(b":"),
+    FormatItem::Component(Component::Minute(modifier::Minute {
+        padding: modifier::Padding::Zero,
+    })),
+    FormatItem::Literal(b":"),
+    FormatItem::Component(Component::Second(modifier::Second {
+        padding: modifier::Padding::Zero,
+    })),
+];
+const TIME_FORMAT: &[FormatItem<'_>] = &[
+    FormatItem::Compound(SHORT_TIME_FORMAT),
+    FormatItem::Literal(b"."),
+    FormatItem::Component(Component::Subsecond(modifier::Subsecond {
+        digits: modifier::SubsecondDigits::OneOrMore, // TODO SQLite supports ZeroOrMore
+    })),
+];
+const LEGACY_TIME_FORMAT: &[FormatItem<'_>] = &[
+    FormatItem::Compound(SHORT_TIME_FORMAT),
+    FormatItem::Literal(b":"), // legacy
+    FormatItem::Component(Component::Subsecond(modifier::Subsecond {
+        digits: modifier::SubsecondDigits::OneOrMore,
+    })),
+];
+
+const OFFSET_FORMAT: &[FormatItem<'_>] = &[
+    FormatItem::Component(Component::OffsetHour(modifier::OffsetHour {
+        sign_is_mandatory: true,
+        padding: modifier::Padding::Zero,
+    })),
+    FormatItem::Literal(b":"),
+    FormatItem::Component(Component::OffsetMinute(modifier::OffsetMinute {
+        padding: modifier::Padding::Zero,
+    })),
+];
+
+const PRIMITIVE_SHORT_DATE_TIME_FORMAT: &[FormatItem<'_>] = &[
+    FormatItem::Compound(DATE_FORMAT),
+    FormatItem::Literal(b" "), // TODO "T"
+    FormatItem::Compound(SHORT_TIME_FORMAT),
+];
+
+const PRIMITIVE_DATE_TIME_FORMAT: &[FormatItem<'_>] = &[
+    FormatItem::Compound(DATE_FORMAT),
+    FormatItem::Literal(b" "), // TODO "T"
+    FormatItem::Compound(TIME_FORMAT),
+];
+const PRIMITIVE_DATE_TIME_Z_FORMAT: &[FormatItem<'_>] = &[
+    FormatItem::Compound(DATE_FORMAT),
+    FormatItem::Literal(b" "), // TODO "T"
+    FormatItem::Compound(TIME_FORMAT),
+    FormatItem::Literal(b"Z"), // TODO "T"
+];
+
+const OFFSET_SHORT_DATE_TIME_FORMAT: &[FormatItem<'_>] = &[
+    FormatItem::Compound(DATE_FORMAT),
+    FormatItem::Literal(b" "), // TODO "T"
+    FormatItem::Compound(SHORT_TIME_FORMAT),
+    //FormatItem::Literal(b" "), optional
+    FormatItem::Compound(OFFSET_FORMAT),
+];
+
+const OFFSET_DATE_TIME_FORMAT: &[FormatItem<'_>] = &[
+    FormatItem::Compound(DATE_FORMAT),
+    FormatItem::Literal(b" "), // TODO "T"
+    FormatItem::Compound(TIME_FORMAT),
+    // FormatItem::Literal(b" "), optional
+    FormatItem::Compound(OFFSET_FORMAT),
+];
+
+const LEGACY_DATE_TIME_FORMAT: &[FormatItem<'_>] = &[
+    FormatItem::Compound(DATE_FORMAT),
+    FormatItem::Literal(b" "), // TODO "T"
+    FormatItem::Compound(LEGACY_TIME_FORMAT),
+    FormatItem::Literal(b" "),
+    FormatItem::Compound(OFFSET_FORMAT),
+];
 
 impl ToSql for OffsetDateTime {
     #[inline]
@@ -9,7 +111,8 @@ impl ToSql for OffsetDateTime {
         // FIXME keep original offset
         let time_string = self
             .to_offset(UtcOffset::UTC)
-            .format("%Y-%m-%d %H:%M:%S.%NZ");
+            .format(&PRIMITIVE_DATE_TIME_Z_FORMAT)
+            .map_err(|err| Error::ToSqlConversionFailure(err.into()))?;
         Ok(ToSqlOutput::from(time_string))
     }
 }
@@ -17,35 +120,28 @@ impl ToSql for OffsetDateTime {
 impl FromSql for OffsetDateTime {
     fn column_result(value: ValueRef<'_>) -> FromSqlResult<Self> {
         value.as_str().and_then(|s| {
+            let s = s.strip_suffix('Z').unwrap_or(s);
             match s.len() {
-                len if len <= 10 => PrimitiveDateTime::parse(s, "%Y-%m-%d").map(|d| d.assume_utc()),
                 len if len <= 19 => {
                     // TODO YYYY-MM-DDTHH:MM:SS
-                    PrimitiveDateTime::parse(s, "%Y-%m-%d %H:%M:%S").map(|d| d.assume_utc())
-                }
-                _ if s.ends_with('Z') => {
-                    // TODO YYYY-MM-DDTHH:MM:SS.SSS
-                    // FIXME time bug: %N specifier doesn't parse millis correctly (https://github.com/time-rs/time/issues/329)
-                    PrimitiveDateTime::parse(s, "%Y-%m-%d %H:%M:%S.%NZ").map(|d| d.assume_utc())
+                    PrimitiveDateTime::parse(s, &PRIMITIVE_SHORT_DATE_TIME_FORMAT)
+                        .map(|d| d.assume_utc())
                 }
                 _ if s.as_bytes()[10] == b'T' => {
                     // YYYY-MM-DDTHH:MM:SS.SSS[+-]HH:MM
-                    OffsetDateTime::parse(s, Format::Rfc3339)
+                    OffsetDateTime::parse(s, &Rfc3339)
                 }
                 _ if s.as_bytes()[19] == b':' => {
                     // legacy
-                    // FIXME time bug: %N specifier doesn't parse millis correctly (https://github.com/time-rs/time/issues/329)
-                    OffsetDateTime::parse(s, "%Y-%m-%d %H:%M:%S:%N %z")
+                    OffsetDateTime::parse(s, &LEGACY_DATE_TIME_FORMAT)
                 }
-                _ => {
-                    // FIXME time bug: %N specifier doesn't parse millis correctly (https://github.com/time-rs/time/issues/329)
-                    // FIXME time bug: %z does not support ':' (https://github.com/time-rs/time/issues/241)
-                    OffsetDateTime::parse(s, "%Y-%m-%d %H:%M:%S.%N%z").or_else(|err| {
-                        PrimitiveDateTime::parse(s, "%Y-%m-%d %H:%M:%S.%N")
+                _ if s.as_bytes()[19] == b'.' => OffsetDateTime::parse(s, &OFFSET_DATE_TIME_FORMAT)
+                    .or_else(|err| {
+                        PrimitiveDateTime::parse(s, &PRIMITIVE_DATE_TIME_FORMAT)
                             .map(|d| d.assume_utc())
                             .map_err(|_| err)
-                    })
-                }
+                    }),
+                _ => OffsetDateTime::parse(s, &OFFSET_SHORT_DATE_TIME_FORMAT),
             }
             .map_err(|err| FromSqlError::Other(Box::new(err)))
         })
@@ -55,8 +151,7 @@ impl FromSql for OffsetDateTime {
 #[cfg(test)]
 mod test {
     use crate::{Connection, Result};
-    use std::time::Duration;
-    use time::{date, offset, OffsetDateTime, Time};
+    use time::{Date, Month, OffsetDateTime, Time, UtcOffset};
 
     fn checked_memory_handle() -> Result<Connection> {
         let db = Connection::open_in_memory()?;
@@ -70,8 +165,9 @@ mod test {
 
         let mut ts_vec = vec![];
 
-        let make_datetime =
-            |secs, nanos| OffsetDateTime::from_unix_timestamp(secs) + Duration::from_nanos(nanos);
+        let make_datetime = |secs: i128, nanos: i128| {
+            OffsetDateTime::from_unix_timestamp_nanos(1_000_000_000 * secs + nanos).unwrap()
+        };
 
         ts_vec.push(make_datetime(10_000, 0)); //January 1, 1970 2:46:40 AM
         ts_vec.push(make_datetime(10_000, 1000)); //January 1, 1970 2:46:40 AM (and one microsecond)
@@ -98,37 +194,25 @@ mod test {
         for (s, t) in vec![
             (
                 "2013-10-07 08:23:19.120",
-                Ok(date!(2013 - 10 - 07)
-                    .with_time(
-                        Time::/*FIXME time bug try_from_hms_milli*/try_from_hms_nano(
-                            8, 23, 19, 120,
-                        )
-                        .unwrap(),
-                    )
+                Ok(Date::from_calendar_date(2013, Month::October, 7)
+                    .unwrap()
+                    .with_time(Time::from_hms_milli(8, 23, 19, 120).unwrap())
                     .assume_utc()),
             ),
             (
                 "2013-10-07 08:23:19.120Z",
-                Ok(date!(2013 - 10 - 07)
-                    .with_time(
-                        Time::/*FIXME time bug try_from_hms_milli*/try_from_hms_nano(
-                            8, 23, 19, 120,
-                        )
-                        .unwrap(),
-                    )
+                Ok(Date::from_calendar_date(2013, Month::October, 7)
+                    .unwrap()
+                    .with_time(Time::from_hms_milli(8, 23, 19, 120).unwrap())
                     .assume_utc()),
             ),
             //"2013-10-07T08:23:19.120Z", // TODO
             (
                 "2013-10-07 04:23:19.120-04:00",
-                Ok(date!(2013 - 10 - 07)
-                    .with_time(
-                        Time::/*FIXME time bug try_from_hms_milli*/try_from_hms_nano(
-                            4, 23, 19, 120,
-                        )
-                        .unwrap(),
-                    )
-                    .assume_offset(offset!(-4))),
+                Ok(Date::from_calendar_date(2013, Month::October, 7)
+                    .unwrap()
+                    .with_time(Time::from_hms_milli(4, 23, 19, 120).unwrap())
+                    .assume_offset(UtcOffset::from_hms(-4, 0, 0).unwrap())),
             ),
         ] {
             let result: Result<OffsetDateTime> = db.query_row("SELECT ?", [s], |r| r.get(0));
