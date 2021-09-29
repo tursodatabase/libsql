a40f5af40368526954f0dca0c78b435b80e8d401
drh <>
2021-09-29 14:01:44 +0000

    Improved testability of changes from check-in [255b0eeed113d83b].
    FossilOrigin-Name: bbfd083c26086fb103fec88faa59a01e689e0b7fce38c09d8846bb472a0e6760

--- src/expr.c
+++ src/expr.c
@@ -518,7 +518,7 @@ Expr *sqlite3ExprForVectorField(
   }else{
     if( pVector->op==TK_VECTOR ) pVector = pVector->x.pList->a[iField].pExpr;
     pRet = sqlite3ExprDup(pParse->db, pVector, 0);
-    if( IN_RENAME_OBJECT && pRet ){
+    if( pRet && IN_RENAME_OBJECT ){
       SWAP(Expr, *pRet, *pVector);
       sqlite3RenameTokenRemap(pParse, pRet, pVector);
     }
