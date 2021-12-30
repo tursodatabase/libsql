b484ae62b847f0eafb50efe27d304a656c348ccc
drh <>
2021-12-30 11:27:53 +0000

    Do not attempt to reprepare a prepared statement that returns SQLITE_SCHEMA if there was an OOM.  This enhancement supercedes check-in [fee469925231d074].
    FossilOrigin-Name: 103f95db3e17d7d021b489c160b84e66c630b4f13516be11d10fffe2b42599fa

--- src/prepare.c
+++ src/prepare.c
@@ -808,6 +808,7 @@ static int sqlite3LockAndPrepare(
     ** reset is considered a permanent error. */
     rc = sqlite3Prepare(db, zSql, nBytes, prepFlags, pOld, ppStmt, pzTail);
     assert( rc==SQLITE_OK || *ppStmt==0 );
+    if( rc==SQLITE_OK || db->mallocFailed ) break;
   }while( rc==SQLITE_ERROR_RETRY
        || (rc==SQLITE_SCHEMA && (sqlite3ResetOneSchema(db,-1), cnt++)==0) );
   sqlite3BtreeLeaveAll(db);
