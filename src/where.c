c36de71968e1d6a9536992be4a3131b350d0fbb8
drh <>
2022-03-09 18:30:55 +0000

    Further refinements to the sqlite_offset() fix from [6029514b08b88e3f].
    FossilOrigin-Name: 98799d7bbb6d62d71862d5fdfe87856995dd179d66612e369c5dc440e7fac31a

--- src/where.c
+++ src/where.c
@@ -6150,6 +6150,10 @@ void sqlite3WhereEnd(WhereInfo *pWInfo){
             Index *pPk = sqlite3PrimaryKeyIndex(pTab);
             x = pPk->aiColumn[x];
             assert( x>=0 );
+#ifdef SQLITE_ENABLE_OFFSET_SQL_FUNC
+          }else if( pOp->opcode==OP_Offset ){
+            /* Do not need to translate the column number */
+#endif
           }else{
             testcase( x!=sqlite3StorageColumnToTable(pTab,x) );
             x = sqlite3StorageColumnToTable(pTab,x);
