4bea8c6b564bf84005df34e4b7f318af6ba37189
drh <>
2022-04-28 17:35:58 +0000

    Tweaks to the name resolution on parenthesized joins.  A small number of tests fail now due to extra columns appearing in the expansion of "*".
    FossilOrigin-Name: c86804917e3fffcf0c19bbf11875667f7968275210fc768e650826bd9c87a5d2

--- src/select.c
+++ src/select.c
@@ -5878,12 +5878,13 @@ static int selectExpander(Walker *pWalker, Select *p){
             */
             if( pTab->aCol[j].colFlags & (COLFLAG_HIDDEN|COLFLAG_NOEXPAND) ){
               if( IsHiddenColumn(&pTab->aCol[j])
-               && (selFlags & SF_IncludeHidden)==0
+               && (selFlags & (SF_IncludeHidden|SF_NestedFrom))==0
               ){
                 continue;
               }
               if( (pTab->aCol[j].colFlags & COLFLAG_NOEXPAND)!=0
                && zTName==0
+               && (selFlags & (SF_NestedFrom))==0
               ){
                 continue;
               }
