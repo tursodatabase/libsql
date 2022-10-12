74507dbd43f1667579ccb8e72085ff8a6b1b2f77
Grzegorz Nowakowski <kamikazelol@op.pl>
2022-10-12 15:46:47 +0200

    build.rs path changed

--- test/rust/src/build.rs
+++ test/rust/src/build.rs
@@ -1,4 +1,3 @@
 fn main() {
-    // <XXX> - paste output of `pwd` command executed in /test/rust directory
-    println!("cargo:rustc-link-search=<XXX>/c/");
+    println!("cargo:rustc-link-search=./src");
 }
