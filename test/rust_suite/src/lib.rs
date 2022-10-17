c8c8273f148b0fae95b63141d9af23c1ea1eab71
Piotr Sarna <sarna@chiselstrike.com>
2022-10-17 13:44:39 +0200

    test: add Rust test suite
    The suite currently only consists of a single smoke test case
    and it will be possible to invoke it with `make rusttest`.
    
    The test is based on rusqlite crate, which allows specifying
    a custom sqlite3 header and library. The test suite always uses
    the local header and library previously built with `make`.
    
    When the need arises, we can also provide custom C bindings,
    but using a popular wrapper crate is not only more ergonomic,
    but also makes sure we stay compatible with available SQLite tools.

---
+++ test/rust_suite/src/lib.rs
@@ -0,0 +1,48 @@
+#[cfg(test)]
+mod tests {
+    use rusqlite::Connection;
+
+    #[derive(Debug, PartialEq)]
+    struct Person {
+        name: String,
+        data: Option<Vec<u8>>,
+    }
+
+    #[test]
+    fn test_insert_steven() {
+        let conn = Connection::open_in_memory().unwrap();
+
+        conn.execute(
+            "CREATE TABLE person (
+                name  TEXT NOT NULL,
+                data  BLOB
+            )",
+            (),
+        )
+        .unwrap();
+        let steven = Person {
+            name: "Steven".to_string(),
+            data: Some(vec![4, 2]),
+        };
+        conn.execute(
+            "INSERT INTO person (name, data) VALUES (?1, ?2)",
+            (&steven.name, &steven.data),
+        )
+        .unwrap();
+
+        let mut stmt = conn.prepare("SELECT name, data FROM person").unwrap();
+        let mut person_iter = stmt
+            .query_map([], |row| {
+                Ok(Person {
+                    name: row.get(0).unwrap(),
+                    data: row.get(1).unwrap(),
+                })
+            })
+            .unwrap();
+
+        let also_steven = person_iter.next().unwrap().unwrap();
+        println!("Read {:#?}", also_steven);
+        assert!(also_steven == steven);
+        assert!(person_iter.next() == None)
+    }
+}
