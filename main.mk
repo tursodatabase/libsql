361fb98b5d1ad2d9787e2b42cc849ec594a0fd51
dan <Dan Kennedy>
2022-10-19 18:03:39 +0000

    Move file /ext/misc/dbdata.c to the /ext/recover/ directory.
    FossilOrigin-Name: f6d5ac807efcf6140016a8b8f0c74566c0bfc98ad92bd8fca03c5cc80ebf3dec

--- main.mk
+++ main.mk
@@ -447,7 +447,7 @@ TESTSRC2 = \
   $(TOP)/ext/session/sqlite3session.c \
   $(TOP)/ext/session/test_session.c \
   $(TOP)/ext/recover/sqlite3recover.c \
-  $(TOP)/ext/misc/dbdata.c \
+  $(TOP)/ext/recover/dbdata.c \
   $(TOP)/ext/recover/test_recover.c \
   fts5.c
 
@@ -764,7 +764,7 @@ SHELL_SRC = \
 	$(TOP)/ext/expert/sqlite3expert.h \
 	$(TOP)/ext/misc/zipfile.c \
 	$(TOP)/ext/misc/memtrace.c \
-	$(TOP)/ext/misc/dbdata.c \
+	$(TOP)/ext/recover/dbdata.c \
 	$(TOP)/ext/recover/sqlite3recover.c \
 	$(TOP)/ext/recover/sqlite3recover.h \
         $(TOP)/src/test_windirent.c
