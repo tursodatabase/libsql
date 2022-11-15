618fdbdf30f69cbf8311e0c8237a585e808337a9
Pekka Enberg <penberg@chiselstrike.com>
2022-11-15 07:55:18 +0200

    Add simple Ruby example app

---
+++ libsql-server/examples/ruby/ruby-sqlite.rb
@@ -0,0 +1,17 @@
+require 'sqlite3'
+
+db = SQLite3::Database.open 'accounts.db'
+
+db.execute "CREATE TABLE IF NOT EXISTS bank_account (owner TEXT, balance DECIMAL)"
+db.execute "INSERT INTO bank_account (owner, balance) VALUES ('alice', 150)"
+db.execute "INSERT INTO bank_account (owner, balance) VALUES ('bob', 0)"
+
+db.execute "BEGIN"
+db.execute "UPDATE bank_account SET balance = balance - 100 WHERE owner = 'alice'"
+db.execute "UPDATE bank_account SET balance = balance + 100 WHERE owner = 'bob'"
+db.execute "COMMIT"
+
+balances = db.query "SELECT owner, balance FROM bank_account"
+while (balance = balances.next) do
+    puts balance.join "\s"
+end
