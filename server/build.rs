9f18d8eb26476b606262515e0339960baa31a66e
Piotr Jastrzebski <piotr@chiselstrike.com>
2022-11-29 12:10:57 +0100

    Support release mode in build.rs
    Signed-off-by: Piotr Jastrzebski <piotr@chiselstrike.com>

--- server/build.rs
+++ server/build.rs
@@ -6,11 +6,13 @@ fn main() {
     let mut pwd = env::current_dir().unwrap();
     pwd.push("../libsql");
     let libsql_dir = fs::canonicalize(pwd.as_path()).unwrap();
-    Command::new("./configure")
-        .current_dir(libsql_dir.as_path())
-        .arg("--with-pic")
-        .status()
-        .unwrap();
+    let mut bindings = Command::new("./configure");
+    let configure = bindings.current_dir(libsql_dir.as_path()).arg("--with-pic");
+    let profile = std::env::var("PROFILE").unwrap();
+    if profile.as_str() == "release" {
+        configure.arg("--enable-releasemode");
+    }
+    configure.status().unwrap();
     Command::new("make")
         .current_dir(libsql_dir.as_path())
         .status()
