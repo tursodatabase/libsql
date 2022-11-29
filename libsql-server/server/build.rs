a43bae4cee006031469789a71aad8643b51b3482
Piotr Jastrzebski <piotr@chiselstrike.com>
2022-11-29 12:42:06 +0100

    Rebuild libsql when it changes
    Signed-off-by: Piotr Jastrzebski <piotr@chiselstrike.com>

--- libsql-server/server/build.rs
+++ libsql-server/server/build.rs
@@ -19,4 +19,5 @@ fn main() {
         .unwrap();
     println!("cargo:rustc-link-search=native=libsql/.libs");
     println!("cargo:rustc-link-lib=static=sqlite3");
+    println!("cargo:rerun-if-changed=libsql/*");
 }
