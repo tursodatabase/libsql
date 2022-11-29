9d1f931136c80a79c187b0f9d30426235a3e4d66
Piotr Jastrzebski <piotr@chiselstrike.com>
2022-11-29 12:00:57 +0100

    Build libsql in cargo build
    Signed-off-by: Piotr Jastrzebski <piotr@chiselstrike.com>

--- libsql-server/server/build.rs
+++ libsql-server/server/build.rs
@@ -1,4 +1,20 @@
+use std::env;
+use std::fs;
+use std::process::Command;
+
 fn main() {
+    let mut pwd = env::current_dir().unwrap();
+    pwd.push("../libsql");
+    let libsql_dir = fs::canonicalize(pwd.as_path()).unwrap();
+    Command::new("./configure")
+        .current_dir(libsql_dir.as_path())
+        .arg("--with-pic")
+        .status()
+        .unwrap();
+    Command::new("make")
+        .current_dir(libsql_dir.as_path())
+        .status()
+        .unwrap();
     println!("cargo:rustc-link-search=native=libsql/.libs");
     println!("cargo:rustc-link-lib=static=sqlite3");
 }
