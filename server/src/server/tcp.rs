34ba80fbce7904e6da1b283722267cd8301c6df5
Pekka Enberg <penberg@chiselstrike.com>
2022-12-01 14:49:40 +0200

    server: Disable Nagle's algorithm
    The PostgreSQL wire protocol sends lots of messages back and forth
    between the client and the server. Let's disable Nagle's algorithm to
    avoid adding artificial wait time for the client, which improves
    throughput:
    
    Before:
    
      $ pgbench -h 127.0.0.1 -p 5000 -f pg_bench_script.sql -c 10 -t 100
    
      transaction type: pg_bench_script.sql
      scaling factor: 1
      query mode: simple
      number of clients: 10
      number of threads: 1
      number of transactions per client: 100
      number of transactions actually processed: 1000/1000
      latency average = 41.115 ms
      initial connection time = 37.032 ms
      tps = 243.221182 (without initial connection time)
    
    After:
    
      $ pgbench -h 127.0.0.1 -p 5000 -f pg_bench_script.sql -c 10 -t 100
    
      transaction type: pg_bench_script.sql
      scaling factor: 1
      query mode: simple
      number of clients: 10
      number of threads: 1
      number of transactions per client: 1000
      number of transactions actually processed: 10000/10000
      latency average = 0.376 ms
      initial connection time = 32.961 ms
      tps = 26564.376109 (without initial connection time)

--- server/src/server/tcp.rs
+++ server/src/server/tcp.rs
@@ -24,7 +24,10 @@ impl Stream for TcpAdapter {
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let conn = ready!(self.as_mut().listener.poll_accept(cx));
         match conn {
-            Ok((stream, addr)) => Poll::Ready(Some(Ok((NetStream::Tcp { stream }, addr)))),
+            Ok((stream, addr)) => {
+                stream.set_nodelay(true).unwrap();
+                Poll::Ready(Some(Ok((NetStream::Tcp { stream }, addr))))
+            }
             Err(e) => Poll::Ready(Some(Err(e))),
         }
     }
