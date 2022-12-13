2aaaf416860983bb7025757d31d5d8a71606e7d7
Pekka Enberg <penberg@chiselstrike.com>
2022-12-13 13:49:20 +0200

    examples: Improve Ruby example app
    Use a fresh database for every invocation and add more logging to make
    the example more understandable.

--- examples/ruby/ruby-sqlite.rb
+++ examples/ruby/ruby-sqlite.rb
@@ -6,18 +6,30 @@ if db_uri.nil?
     raise "Please configure database via the `DB_URI` environment variable."
 end
 
-puts db_uri
-
 db = SQLite3::Database.open db_uri
 
+puts "Connected to `#{db_uri}` database"
+puts ""
+
 db.execute "CREATE TABLE IF NOT EXISTS bank_account (owner TEXT, balance DECIMAL)"
+db.execute "DELETE FROM bank_account"
 db.execute "INSERT INTO bank_account (owner, balance) VALUES ('alice', 150)"
 db.execute "INSERT INTO bank_account (owner, balance) VALUES ('bob', 0)"
+
+puts "Initial account balances:"
+
+balances = db.query "SELECT owner, balance FROM bank_account"
+while (balance = balances.next) do
+    puts balance.join "\s"
+end
 
 db.execute "BEGIN"
 db.execute "UPDATE bank_account SET balance = balance - 100 WHERE owner = 'alice'"
 db.execute "UPDATE bank_account SET balance = balance + 100 WHERE owner = 'bob'"
 db.execute "COMMIT"
+
+puts ""
+puts "Account balances after transaction:"
 
 balances = db.query "SELECT owner, balance FROM bank_account"
 while (balance = balances.next) do
