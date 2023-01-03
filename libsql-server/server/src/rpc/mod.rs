22518b10887e193795b2024fa6a202abeccb0675
ad hoc <postma.marin@protonmail.com>
2023-01-03 14:03:33 +0100

    hook wal logger

--- libsql-server/server/src/rpc/mod.rs
+++ libsql-server/server/src/rpc/mod.rs
@@ -1,4 +1,5 @@
 use std::net::SocketAddr;
+use std::sync::Arc;
 
 use crate::database::service::DbFactory;
 use crate::rpc::proxy::proxy_rpc::proxy_server::ProxyServer;
@@ -10,7 +11,11 @@ use crate::wal_logger::WalLogger;
 pub mod proxy;
 pub mod wal_log;
 
-pub async fn run_rpc_server<F>(addr: SocketAddr, factory: F) -> anyhow::Result<()>
+pub async fn run_rpc_server<F>(
+    addr: SocketAddr,
+    factory: F,
+    logger: Arc<WalLogger>,
+) -> anyhow::Result<()>
 where
     F: DbFactory + 'static,
     F::Db: Sync + Send + Clone,
