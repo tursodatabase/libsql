367688f1b6fc0f56837d4adabefaef02108719d7
ad hoc <postma.marin@protonmail.com>
2023-01-03 14:01:48 +0100

    introduce wal_log rpc implementation

--- server/src/rpc/mod.rs
+++ server/src/rpc/mod.rs
@@ -3,8 +3,12 @@ use std::net::SocketAddr;
 use crate::database::service::DbFactory;
 use crate::rpc::proxy::proxy_rpc::proxy_server::ProxyServer;
 use crate::rpc::proxy::ProxyService;
+use crate::rpc::wal_log::wal_log_rpc::wal_log_server::WalLogServer;
+use crate::rpc::wal_log::WalLogService;
+use crate::wal_logger::WalLogger;
 
 pub mod proxy;
+pub mod wal_log;
 
 pub async fn run_rpc_server<F>(addr: SocketAddr, factory: F) -> anyhow::Result<()>
 where
@@ -12,10 +16,12 @@ where
     F::Db: Sync + Send + Clone,
 {
     let proxy_service = ProxyService::new(factory);
+    let logger_service = WalLogService::new(logger);
 
     tracing::info!("serving write proxy server at {addr}");
     tonic::transport::Server::builder()
         .add_service(ProxyServer::new(proxy_service))
+        .add_service(WalLogServer::new(logger_service))
         .serve(addr)
         .await?;
 
