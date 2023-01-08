d7d70bcaadeca63d093c4f91600bfc56107b7c9a
Piotr Sarna <sarna@chiselstrike.com>
2023-01-08 19:36:46 +0100

    wal: include relevant virtual WAL headers in sqlite3.h
    It was reported on Discord that the amalgamation file only includes
    libsql_wal_methods as an opaque type, which is not easily consumable,
    especially for projects which automatically generate bindings
    to other languages.
    This issue was an oversight, rooting from the fact that our Rust bindings
    were simply written from scratch. And while we still intend to add
    official Rust bindings, virtual WAL methods should be included in the
    amalgamation sqlite3.h file, so that they can be easily consumed
    by other languages.

--- src/pcache.h
+++ src/pcache.h
@@ -15,37 +15,7 @@
 
 #ifndef _PCACHE_H_
 
-typedef struct PgHdr PgHdr;
-typedef struct PCache PCache;
-
-/*
-** Every page in the cache is controlled by an instance of the following
-** structure.
-*/
-struct PgHdr {
-  sqlite3_pcache_page *pPage;    /* Pcache object page handle */
-  void *pData;                   /* Page data */
-  void *pExtra;                  /* Extra content */
-  PCache *pCache;                /* PRIVATE: Cache that owns this page */
-  PgHdr *pDirty;                 /* Transient list of dirty sorted by pgno */
-  Pager *pPager;                 /* The pager this page is part of */
-  Pgno pgno;                     /* Page number for this page */
-#ifdef SQLITE_CHECK_PAGES
-  u32 pageHash;                  /* Hash of page content */
-#endif
-  u16 flags;                     /* PGHDR flags defined below */
-
-  /**********************************************************************
-  ** Elements above, except pCache, are public.  All that follow are 
-  ** private to pcache.c and should not be accessed by other modules.
-  ** pCache is grouped with the public elements for efficiency.
-  */
-  i16 nRef;                      /* Number of users of this page */
-  PgHdr *pDirtyNext;             /* Next element in list of dirty pages */
-  PgHdr *pDirtyPrev;             /* Previous element in list of dirty pages */
-                          /* NB: pDirtyNext and pDirtyPrev are undefined if the
-                          ** PgHdr object is not dirty */
-};
+#include "page_header.h"
 
 /* Bit values for PgHdr.flags */
 #define PGHDR_CLEAN           0x001  /* Page not on the PCache.pDirty list */
