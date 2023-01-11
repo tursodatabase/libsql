8518eaccd79ae83a55fad3c3fa545beb16fb127c
drh <>
2023-01-11 20:52:15 +0000

    Fix duplicate semicolon in btreeInt.h. [forum:/forumpost/8db1711ca2|Forum post 8db1711ca2]
    FossilOrigin-Name: 7526c46632578a2b602622b9debc406b52af4a42cc880970c4307d13853d59d3

--- src/btreeInt.h
+++ src/btreeInt.h
@@ -698,7 +698,7 @@ struct IntegrityCk {
   Pgno nPage;       /* Number of pages in the database */
   int mxErr;        /* Stop accumulating errors when this reaches zero */
   int nErr;         /* Number of messages written to zErrMsg so far */
-  int rc;;          /* SQLITE_OK, SQLITE_NOMEM, or SQLITE_INTERRUPT */
+  int rc;           /* SQLITE_OK, SQLITE_NOMEM, or SQLITE_INTERRUPT */
   u32 nStep;        /* Number of steps into the integrity_check process */
   const char *zPfx; /* Error message prefix */
   Pgno v1;          /* Value for first %u substitution in zPfx */
