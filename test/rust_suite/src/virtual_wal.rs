8bc54b6ea2b39612dbfb401192d762ca55821560
Piotr Sarna <sarna@chiselstrike.com>
2023-01-12 11:06:45 +0100

    tests: add journal_mode check to virtual WAL test
    The test now ensures that the database is already open
    in WAL mode by default.

--- test/rust_suite/src/virtual_wal.rs
+++ test/rust_suite/src/virtual_wal.rs
@@ -121,8 +121,7 @@ mod tests {
         db: extern "C" fn(wal: *mut Wal, db: *const c_void),
         pathname_len: extern "C" fn(orig_len: i32) -> i32,
         get_pathname: extern "C" fn(buf: *mut u8, orig: *const u8, orig_len: i32),
-        pre_main_db_open:
-            extern "C" fn(methods: *mut libsql_wal_methods, name: *const i8) -> i32,
+        pre_main_db_open: extern "C" fn(methods: *mut libsql_wal_methods, name: *const i8) -> i32,
         b_uses_shm: i32,
         name: *const u8,
         p_next: *const c_void,
@@ -257,8 +256,10 @@ mod tests {
         println!("\t\tread {} bytes", data.len());
         0
     }
-    extern "C" fn db_size(_wal: *mut Wal) -> i32 {
-        ERR_MISUSE
+    extern "C" fn db_size(wal: *mut Wal) -> i32 {
+        println!("Db size called");
+        let methods = unsafe { &*(*wal).wal_methods };
+        methods.pages.len() as i32
     }
     extern "C" fn begin_write(_wal: *mut Wal) -> i32 {
         println!("Write started");
@@ -334,12 +335,14 @@ mod tests {
         panic!("Should never be called")
     }
     extern "C" fn db(_wal: *mut Wal, _db: *const c_void) {}
-    extern "C" fn pathname_len(_orig_len: i32) -> i32 {
-        println!("Returning length 0");
-        0
+    extern "C" fn pathname_len(orig_len: i32) -> i32 {
+        orig_len + 4
     }
-    extern "C" fn get_pathname(_buf: *mut u8, _orig: *const u8, _orig_len: i32) {
-        panic!("Should never be called")
+    extern "C" fn get_pathname(buf: *mut u8, orig: *const u8, orig_len: i32) {
+        unsafe {
+            std::ptr::copy_nonoverlapping(orig, buf, orig_len as usize);
+            std::ptr::copy_nonoverlapping(".wal".as_ptr(), buf.offset(orig_len as isize), 4);
+        }
     }
     extern "C" fn pre_main_db_open(_methods: *mut libsql_wal_methods, _name: *const i8) -> i32 {
         0
@@ -402,7 +405,11 @@ mod tests {
             Box::leak(vwal);
             Connection::from_handle(pdb).unwrap()
         };
-        conn.pragma_update(None, "journal_mode", "wal").unwrap();
+        let journal_mode: String = conn
+            .query_row("PRAGMA journal_mode", [], |r| r.get(0))
+            .unwrap();
+        println!("Journaling mode: {}", journal_mode);
+        assert_eq!(journal_mode, "wal".to_string());
         conn.execute("CREATE TABLE t(id)", ()).unwrap();
         conn.execute("INSERT INTO t(id) VALUES (42)", ()).unwrap();
         conn.execute("INSERT INTO t(id) VALUES (zeroblob(8193))", ())
