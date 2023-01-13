974ef3683f7b8001373f398cb68908b2379d8465
Pekka Enberg <penberg@chiselstrike.com>
2023-01-13 08:29:53 +0200

    libsql-client: Fix tests to use new API

--- packages/libsql-client/src/__tests__/libsql-js.test.ts
+++ packages/libsql-client/src/__tests__/libsql-js.test.ts
@@ -1,7 +1,8 @@
-import { Database } from "../";
+import { connect } from "../";
 
 test("execute", async () => {
-  const db = new Database();
+  const config = {};
+  const db = connect(config);
   await db.execute("CREATE TABLE users (email TEXT)");
   const stmt = db.execute("SELECT * FROM users");
   // TODO: check results
