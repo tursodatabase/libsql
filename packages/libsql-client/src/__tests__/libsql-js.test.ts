88d6e0194db0ac2056b030fe281b0560d49a7295
Pekka Enberg <penberg@chiselstrike.com>
2023-01-13 08:49:38 +0200

    libsql-client: Support local database files
    Fixes #30

--- packages/libsql-client/src/__tests__/libsql-js.test.ts
+++ packages/libsql-client/src/__tests__/libsql-js.test.ts
@@ -1,7 +1,7 @@
 import { connect } from "../";
 
 test("execute", async () => {
-  const config = {};
+  const config = { url: "file::memory:" };
   const db = connect(config);
   await db.execute("CREATE TABLE users (email TEXT)");
   const stmt = db.execute("SELECT * FROM users");
