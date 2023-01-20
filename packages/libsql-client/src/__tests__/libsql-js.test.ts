2ac32b3b0047b0d66e3b2ba86a9bd34844924dce
Pekka Enberg <penberg@chiselstrike.com>
2023-01-20 12:37:26 +0200

    libsql-client: Improve test case

--- packages/libsql-client/src/__tests__/libsql-js.test.ts
+++ packages/libsql-client/src/__tests__/libsql-js.test.ts
@@ -1,9 +1,14 @@
 import { connect } from "../";
 
 test("execute", async () => {
-  const config = { url: "file::memory:" };
+  const url = process.env.DB_URL ?? "file::memory:";
+  const config = { url };
   const db = connect(config);
-  await db.execute("CREATE TABLE users (email TEXT)");
-  const stmt = db.execute("SELECT * FROM users");
-  // TODO: check results
+  await db.execute("CREATE TABLE IF NOT EXISTS users (email TEXT)");
+  await db.execute("DELETE FROM users");
+  await db.execute("INSERT INTO users (email) VALUES ('alice@example.com')");
+  await db.execute("INSERT INTO users (email) VALUES ('bob@example.com')");
+  const rs = await db.execute("SELECT * FROM users");
+  expect(rs.columns).toEqual(['email']);
+  expect(rs.rows).toEqual([['alice@example.com'], ['bob@example.com']]);
 });
