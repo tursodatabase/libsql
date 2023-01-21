e837090ca80da4c8ae7fc702ffd88a431860fc97
Piotr Sarna <sarna@chiselstrike.com>
2023-01-21 19:45:49 +0100

    build: statically link Wasm user-defined functions
    This commit changes the dependency from official rusqlite
    to a fork which comes with bundled libsql.
    
    That allows us to simplify the integration code, drop explicit
    unsafe blocks, and statically link with WebAssembly bindings.

--- libsql-server/sqld/src/libsql/mwal/mod.rs
+++ libsql-server/sqld/src/libsql/mwal/mod.rs
@@ -6,30 +6,23 @@ pub(crate) fn open_with_virtual_wal(
     path: impl AsRef<std::path::Path>,
     flags: rusqlite::OpenFlags,
     vwal_methods: Arc<Mutex<mwal::ffi::libsql_wal_methods>>,
-) -> anyhow::Result<super::WalConnection> {
-    use std::os::unix::ffi::OsStrExt;
+) -> anyhow::Result<rusqlite::Connection> {
     let mut vwal_methods = vwal_methods.lock().map_err(|e| anyhow::anyhow!("{}", e))?;
     unsafe {
-        let mut pdb: *mut rusqlite::ffi::sqlite3 = std::ptr::null_mut();
-        let ppdb: *mut *mut rusqlite::ffi::sqlite3 = &mut pdb;
         let register_err = super::ffi::libsql_wal_methods_register(
             &mut *vwal_methods as *const mwal::ffi::libsql_wal_methods as _,
         );
         assert_eq!(register_err, 0);
-        let open_err = super::libsql_open(
-            path.as_ref().as_os_str().as_bytes().as_ptr(),
-            ppdb,
-            flags.bits(),
-            std::ptr::null(),
-            vwal_methods.name,
-        );
-        assert_eq!(open_err, 0);
-        let conn = super::Connection::from_handle(pdb)?;
-        conn.pragma_update(None, "journal_mode", "wal")?;
-        tracing::trace!(
-            "Opening a connection with virtual WAL at {}",
-            path.as_ref().display()
-        );
-        Ok(super::WalConnection { inner: conn })
     }
+    tracing::trace!(
+        "Opening a connection with virtual WAL at {}",
+        path.as_ref().display()
+    );
+    let conn = rusqlite::Connection::open_with_flags_and_wal(path, flags, unsafe {
+        std::ffi::CStr::from_ptr(vwal_methods.name as *const _)
+            .to_str()
+            .unwrap()
+    })?;
+    conn.pragma_update(None, "journal_mode", "wal")?;
+    Ok(conn)
 }
