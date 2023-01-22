c1d4c68082569d5b327c958205022d534120a429
Piotr Sarna <sarna@chiselstrike.com>
2023-01-22 13:02:27 +0100

    build: drop libsql/ submodule
    It's no longer needed, once we push the dependency management
    to a fork of libsqlite3-sys crate.

--- libsql-server/sqld/build.rs
+++ libsql-server/sqld/build.rs
@@ -1,34 +1,6 @@
-use std::env;
-use std::fs;
-use std::process::Command;
-
 use prost_build::Config;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut pwd = env::current_dir().unwrap();
-    pwd.push("../libsql");
-    let libsql_dir = fs::canonicalize(pwd.as_path()).unwrap();
-    let mut bindings = Command::new("./configure");
-    let configure = bindings.current_dir(libsql_dir.as_path()).arg("--with-pic");
-    let profile = std::env::var("PROFILE").unwrap();
-    if profile.as_str() == "release" {
-        configure.arg("--enable-releasemode");
-    }
-    let output = configure.output().unwrap();
-    if !output.status.success() {
-        println!("{}", std::str::from_utf8(&output.stderr).unwrap());
-        panic!("failed to configure");
-    }
-
-    if !Command::new("make")
-        .current_dir(libsql_dir.as_path())
-        .status()
-        .unwrap()
-        .success()
-    {
-        panic!("failed to compile");
-    }
-
     let mut config = Config::new();
     config.bytes([".wal_log"]);
     tonic_build::configure()
