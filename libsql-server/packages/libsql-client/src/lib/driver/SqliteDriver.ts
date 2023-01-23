c88d15ba15a13d0be1d92060cadd894c8ed4ca27
Doug Stevenson <doug@chiselstrike.com>
2023-01-23 08:55:43 -0500

    Changed client interface to support parameter binding

--- libsql-server/packages/libsql-client/src/lib/driver/SqliteDriver.ts
+++ libsql-server/packages/libsql-client/src/lib/driver/SqliteDriver.ts
@@ -1,55 +1,77 @@
 import DatabaseConstructor, {Database, SqliteError, Statement} from "better-sqlite3";
-import { ResultSet } from "../libsql-js";
+import { BoundStatement, Params, ResultSet, SqlValue } from "../libsql-js";
 import { Driver } from "./Driver";
 
 export class SqliteDriver implements Driver {
-    db: Database;
+    private db: Database;
+
     constructor(url: string) {
         this.db = new DatabaseConstructor(url.substring(5))
     }
-    async transaction(sqls: string[]): Promise<ResultSet[]> {
-        const result = [];
-        for (let sql of sqls) {
-            const rs = await this.execute(sql);
-            result.push(rs);
-        }
-        return result;
-    }
-    async execute(sql: string): Promise<ResultSet> {
+
+    async execute(sql: string, params?: Params): Promise<ResultSet> {
         return await new Promise(resolve => {
-            let stmt: Statement
+            let columns: string[];
+            let rows: any[];
+
             try {
-                stmt = this.db.prepare(sql);
+                const stmt = this.db.prepare(sql);
+                if (stmt.reader) {
+                    columns = stmt.columns().map(c => c.name);
+                    if (params === undefined) {
+                        rows = stmt.all()
+                    } else {
+                        rows = stmt.all(params)
+                    }
+                    rows = rows.map(row => {
+                        return columns.map(column => row[column]);
+                    });
+                } else {
+                    columns = [];
+                    rows = [];
+                    if (params === undefined) {
+                        stmt.run();
+                    } else {
+                        stmt.run(params);
+                    }
+                }
             } catch (e: any) {
                 resolve({
                     success: false,
                     error: { message: e.message },
                     meta: { duration: 0 },
-                })
-                return
-            }
-            var columns: string[];
-            var rows: any[];
-            if (stmt.reader) {
-                columns = stmt.columns().map(c => c.name);
-                rows = stmt.all().map(row => {
-                    return columns.map(column => row[column]);
                 });
-            } else {
-                columns = [];
-                rows = [];
-                stmt.run();
+                return;
             }
-            // FIXME: error handling
-            const rs = {
+
+            resolve({
+                success: true,
                 columns,
                 rows,
-                success: true,
-                meta: {
-                    duration: 0,
-                },
-            };
-            resolve(rs);
+                meta: { duration: 0 },
+            });
         });
+    }
+
+    async transaction(stmts: (string | BoundStatement)[]): Promise<ResultSet[]> {
+        // TODO this is not really a "transaction", however, the better-sqlite3
+        // transaction API blocks the event loop and does not work with async
+        // functions. We need to investigate working the transaction manually
+        // with begin/commit, however, that likely does not support concurrent
+        // overlapping invocations from multiple procedures in the same process.
+        //
+        // https://github.com/WiseLibs/better-sqlite3/blob/HEAD/docs/api.md#transactionfunction---function
+
+        const result = [];
+        for (const stmt of stmts) {
+            let rs;
+            if (typeof stmt === "string") {
+                rs = await this.execute(stmt);
+            } else {
+                rs = await this.execute(stmt.sql, stmt.params);
+            }
+            result.push(rs);
+        }
+        return result;
     }
 }
