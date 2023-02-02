a154bfd243fc92463b5eab46cb7959d6fb6c1519
Jan Špaček <patek.mail@gmail.com>
2023-02-02 16:09:02 +0100

    Support custom executor

--- pypackages/libsql-client/libsql_client/sqlite_driver.py
+++ pypackages/libsql-client/libsql_client/sqlite_driver.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional, TYPE_CHECKING
 import asyncio
 import sqlite3
 
@@ -6,18 +6,26 @@ from .driver import _Driver, _RawStmt
 from .errors import ClientResponseError
 from .result import ResultSet, Row
 
+if TYPE_CHECKING:
+    import concurrent.futures
+
 class _SqliteDriver(_Driver):
     _conn: sqlite3.Connection
     _conn_lock: asyncio.Lock
+    _executor: Optional["concurrent.futures.ThreadPoolExecutor"]
 
-    def __init__(self, path: str) -> None:
+    def __init__(
+        self, path: str,
+        executor: Optional["concurrent.futures.ThreadPoolExecutor"] = None,
+    ) -> None:
         self._conn = sqlite3.connect(path, check_same_thread=False, isolation_level=None)
         self._conn_lock = asyncio.Lock()
+        self._executor = executor
 
     async def batch(self, stmts: List[_RawStmt]) -> List[ResultSet]:
         async with self._conn_lock:
             loop = asyncio.get_running_loop()
-            return await loop.run_in_executor(None, lambda: _batch(self._conn, stmts))
+            return await loop.run_in_executor(self._executor, lambda: _batch(self._conn, stmts))
 
     async def close(self) -> None:
         self._conn.close()
