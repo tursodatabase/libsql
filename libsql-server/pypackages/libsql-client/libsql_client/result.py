a3539d3f2aacc567ac4d5c3203ec9300bdf32d08
Jan Špaček <patek.mail@gmail.com>
2023-02-02 11:35:02 +0100

    Configure mypy and fix type errors

--- libsql-server/pypackages/libsql-client/libsql_client/result.py
+++ libsql-server/pypackages/libsql-client/libsql_client/result.py
@@ -3,12 +3,15 @@ from typing import Dict, List, Tuple, Union
 Value = Union[str, float, int, bytes, None]
 
 class ResultSet:
-    def __init__(self, columns: Tuple[str], rows: List["Row"]) -> None:
+    _columns: Tuple[str, ...]
+    _rows: List["Row"]
+
+    def __init__(self, columns: Tuple[str, ...], rows: List["Row"]) -> None:
         self._columns = columns
         self._rows = rows
 
     @property
-    def columns(self) -> Tuple[str]:
+    def columns(self) -> Tuple[str, ...]:
         return self._columns
 
     @property
@@ -17,9 +20,9 @@ class ResultSet:
 
 class Row:
     _column_idxs: Dict[str, int]
-    _values: Tuple[Value]
+    _values: Tuple[Value, ...]
 
-    def __init__(self, column_idxs: Dict[str, int], values: Tuple[Value]) -> None:
+    def __init__(self, column_idxs: Dict[str, int], values: Tuple[Value, ...]) -> None:
         self._column_idxs = column_idxs
         self._values = values
 
