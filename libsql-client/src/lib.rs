bb344ed0aa4e6615ff384984bf9247997c66d410
Piotr Sarna <sarna@chiselstrike.com>
2023-02-07 17:39:21 +0100

    libsql-client: move cloudflare workers to a separate feature
    This commit introduces a Connection trait, and a workers::Connection
    class implementing it.
    The trait abstracts away common code, and the Cloudflare Workers backend
    is now guarded by an opt-in feature flag.
    
    Creating connections can also be abstracted away in order to deduplicate
    the code even further, but it's left for a follow-up.

--- libsql-client/src/lib.rs
+++ libsql-client/src/lib.rs
@@ -12,14 +12,19 @@
 use std::collections::HashMap;
 use std::iter::IntoIterator;
 
-use base64::Engine;
-use worker::*;
+use anyhow::{anyhow, Result};
 
 pub mod statement;
 pub use statement::Statement;
 
-pub mod cell_value;
-pub use cell_value::CellValue;
+pub mod value;
+pub use value::Value;
+
+pub mod connection;
+pub use connection::Connection;
+
+#[cfg(feature = "workers_backend")]
+pub mod workers;
 
 /// Metadata of a database request
 #[derive(Clone, Debug, Default)]
@@ -30,7 +35,7 @@ pub struct Meta {
 /// A database row
 #[derive(Clone, Debug)]
 pub struct Row {
-    pub cells: HashMap<String, CellValue>,
+    pub cells: HashMap<String, Value>,
 }
 
 /// Structure holding a set of rows returned from a query
@@ -48,21 +53,13 @@ pub enum QueryResult {
     Success((ResultSet, Meta)),
 }
 
-/// Database connection. This is the main structure used to
-/// communicate with the database.
-#[derive(Clone, Debug)]
-pub struct Connection {
-    url: String,
-    auth: String,
-}
-
-fn parse_columns(columns: Vec<serde_json::Value>, result_idx: usize) -> Result<Vec<String>> {
+pub fn parse_columns(columns: Vec<serde_json::Value>, result_idx: usize) -> Result<Vec<String>> {
     let mut result = Vec::with_capacity(columns.len());
     for (idx, column) in columns.into_iter().enumerate() {
         match column {
             serde_json::Value::String(column) => result.push(column),
             _ => {
-                return Err(worker::Error::from(format!(
+                return Err(anyhow!(format!(
                     "Result {result_idx} column name {idx} not a string",
                 )))
             }
@@ -71,32 +68,32 @@ fn parse_columns(columns: Vec<serde_json::Value>, result_idx: usize) -> Result<V
     Ok(result)
 }
 
-fn parse_value(
+pub fn parse_value(
     cell: serde_json::Value,
     result_idx: usize,
     row_idx: usize,
     cell_idx: usize,
-) -> Result<CellValue> {
+) -> Result<Value> {
     match cell {
-        serde_json::Value::Null => Ok(CellValue::Null),
-        serde_json::Value::Bool(v) => Ok(CellValue::Bool(v)),
+        serde_json::Value::Null => Ok(Value::Null),
+        serde_json::Value::Bool(v) => Ok(Value::Bool(v)),
         serde_json::Value::Number(v) => match v.as_i64() {
-            Some(v) => Ok(CellValue::Number(v)),
+            Some(v) => Ok(Value::Number(v)),
             None => match v.as_f64() {
-                Some(v) => Ok(CellValue::Float(v)),
-                None => Err(worker::Error::from(format!(
+                Some(v) => Ok(Value::Float(v)),
+                None => Err(anyhow!(
                     "Result {result_idx} row {row_idx} cell {cell_idx} had unknown number value: {v}",
-                ))),
+                )),
             },
         },
-        serde_json::Value::String(v) => Ok(CellValue::Text(v)),
-        _ => Err(worker::Error::from(format!(
+        serde_json::Value::String(v) => Ok(Value::Text(v)),
+        _ => Err(anyhow!(
             "Result {result_idx} row {row_idx} cell {cell_idx} had unknown type",
-        ))),
+        )),
     }
 }
 
-fn parse_rows(
+pub fn parse_rows(
     rows: Vec<serde_json::Value>,
     columns: &Vec<String>,
     result_idx: usize,
@@ -106,9 +103,9 @@ fn parse_rows(
         match row {
             serde_json::Value::Array(row) => {
                 if row.len() != columns.len() {
-                    return Err(worker::Error::from(format!(
+                    return Err(anyhow!(
                         "Result {result_idx} row {idx} had wrong number of cells",
-                    )));
+                    ));
                 }
                 let mut cells = HashMap::with_capacity(columns.len());
                 for (cell_idx, value) in row.into_iter().enumerate() {
@@ -119,17 +116,13 @@ fn parse_rows(
                 }
                 result.push(Row { cells })
             }
-            _ => {
-                return Err(worker::Error::from(format!(
-                    "Result {result_idx} row {idx} was not an array",
-                )))
-            }
+            _ => return Err(anyhow!("Result {result_idx} row {idx} was not an array",)),
         }
     }
     Ok(result)
 }
 
-fn parse_query_result(result: serde_json::Value, idx: usize) -> Result<QueryResult> {
+pub fn parse_query_result(result: serde_json::Value, idx: usize) -> Result<QueryResult> {
     match result {
         serde_json::Value::Object(obj) => {
             if let Some(err) = obj.get("error") {
@@ -138,25 +131,21 @@ fn parse_query_result(result: serde_json::Value, idx: usize) -> Result<QueryResu
                         Some(serde_json::Value::String(msg)) => {
                             Ok(QueryResult::Error((msg.clone(), Meta::default())))
                         }
-                        _ => Err(worker::Error::from(format!(
-                            "Result {idx} error message was not a string",
-                        ))),
+                        _ => Err(anyhow!("Result {idx} error message was not a string",)),
                     },
-                    _ => Err(worker::Error::from(format!(
-                        "Result {idx} results was not an object",
-                    ))),
+                    _ => Err(anyhow!("Result {idx} results was not an object",)),
                 };
             }
 
             let results = obj.get("results");
             match results {
                 Some(serde_json::Value::Object(obj)) => {
-                    let columns = obj.get("columns").ok_or_else(|| {
-                        worker::Error::from(format!("Result {idx} had no columns"))
-                    })?;
+                    let columns = obj
+                        .get("columns")
+                        .ok_or_else(|| anyhow!(format!("Result {idx} had no columns")))?;
                     let rows = obj
                         .get("rows")
-                        .ok_or_else(|| worker::Error::from(format!("Result {idx} had no rows")))?;
+                        .ok_or_else(|| anyhow!(format!("Result {idx} had no rows")))?;
                     match (rows, columns) {
                         (serde_json::Value::Array(rows), serde_json::Value::Array(columns)) => {
                             let columns = parse_columns(columns.to_vec(), idx)?;
@@ -166,230 +155,15 @@ fn parse_query_result(result: serde_json::Value, idx: usize) -> Result<QueryResu
                                 Meta::default(),
                             )))
                         }
-                        _ => Err(worker::Error::from(format!(
+                        _ => Err(anyhow!(
                             "Result {idx} had rows or columns that were not an array",
-                        ))),
+                        )),
                     }
                 }
-                Some(_) => Err(worker::Error::from(format!(
-                    "Result {idx} was not an object",
-                ))),
-                None => Err(worker::Error::from(format!(
-                    "Result {idx} did not contain results or error",
-                ))),
+                Some(_) => Err(anyhow!("Result {idx} was not an object",)),
+                None => Err(anyhow!("Result {idx} did not contain results or error",)),
             }
         }
-        _ => Err(worker::Error::from(format!(
-            "Result {idx} was not an object",
-        ))),
-    }
-}
-
-impl Connection {
-    /// Establishes a database connection, given a [`Url`]
-    ///
-    /// # Arguments
-    /// * `url` - [`Url`] object of the database endpoint. This cannot be a relative URL;
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use libsql_client::Connection;
-    /// use url::Url;
-    ///
-    /// let url  = Url::parse("https://foo:bar@localhost:8080").unwrap();
-    /// let db = Connection::connect_from_url(&url).unwrap();
-    /// ```
-    pub fn connect_from_url(url: &url::Url) -> Result<Self> {
-        let username = url.username();
-        let password = url.password().unwrap_or_default();
-        let mut url = url.clone();
-        url.set_username("").map_err(|_| {
-            worker::Error::from("Could not extract username from URL. Invalid URL?".to_string())
-        })?;
-        url.set_password(None).map_err(|_| {
-            worker::Error::from("Could not extract password from URL. Invalid URL?".to_string())
-        })?;
-        Ok(Self::connect(url.as_str(), username, password))
-    }
-
-    /// Establishes a database connection.
-    ///
-    /// # Arguments
-    /// * `url` - URL of the database endpoint, without its username or password
-    /// * `username` - database username
-    /// * `pass` - user's password
-    ///
-    /// # Examples
-    /// ```
-    /// # use libsql_client::Connection;
-    ///
-    /// let db = Connection::connect("https://localhost:8080", "user", "pass");
-    /// ```
-    pub fn connect(
-        url: impl Into<String>,
-        username: impl Into<String>,
-        pass: impl Into<String>,
-    ) -> Self {
-        let username = username.into();
-        let pass = pass.into();
-        let url = url.into();
-        // Auto-update the URL to start with https:// if no protocol was specified
-        let url = if !url.contains("://") {
-            "https://".to_owned() + &url
-        } else {
-            url
-        };
-        Self {
-            url,
-            auth: format!(
-                "Basic {}",
-                base64::engine::general_purpose::STANDARD.encode(format!("{username}:{pass}"))
-            ),
-        }
-    }
-
-    /// Establishes a database connection from Cloudflare Workers context.
-    /// Expects the context to contain the following secrets defined:
-    /// * `LIBSQL_CLIENT_URL`
-    /// * `LIBSQL_CLIENT_USER`
-    /// * `LIBSQL_CLIENT_PASS`
-    /// # Arguments
-    /// * `ctx` - Cloudflare Workers route context
-    pub fn connect_from_ctx<D>(ctx: &worker::RouteContext<D>) -> Result<Self> {
-        Ok(Self::connect(
-            ctx.secret("LIBSQL_CLIENT_URL")?.to_string(),
-            ctx.secret("LIBSQL_CLIENT_USER")?.to_string(),
-            ctx.secret("LIBSQL_CLIENT_PASS")?.to_string(),
-        ))
-    }
-
-    /// Executes a single SQL statement
-    ///
-    /// # Arguments
-    /// * `stmt` - the SQL statement
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # async fn f() {
-    /// let db = libsql_client::Connection::connect("https://example.com", "admin", "s3cr3tp4ss");
-    /// let result = db.execute("SELECT * FROM sqlite_master").await;
-    /// let result_params = db
-    ///     .execute(libsql_client::Statement::with_params(
-    ///         "UPDATE t SET v = ? WHERE key = ?",
-    ///         &[libsql_client::CellValue::Number(5), libsql_client::CellValue::Text("five".to_string())],
-    ///     ))
-    ///     .await;
-    /// # }
-    /// ```
-    pub async fn execute(&self, stmt: impl Into<Statement>) -> Result<QueryResult> {
-        let mut results = self.batch(std::iter::once(stmt)).await?;
-        Ok(results.remove(0))
-    }
-
-    /// Executes a batch of SQL statements.
-    /// Each statement is going to run in its own transaction,
-    /// unless they're wrapped in BEGIN and END
-    ///
-    /// # Arguments
-    /// * `stmts` - SQL statements
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # async fn f() {
-    /// let db = libsql_client::Connection::connect("https://example.com", "admin", "s3cr3tp4ss");
-    /// let result = db
-    ///     .batch(["CREATE TABLE t(id)", "INSERT INTO t VALUES (42)"])
-    ///     .await;
-    /// # }
-    /// ```
-    pub async fn batch(
-        &self,
-        stmts: impl IntoIterator<Item = impl Into<Statement>>,
-    ) -> Result<Vec<QueryResult>> {
-        let mut headers = Headers::new();
-        headers.append("Authorization", &self.auth).ok();
-        // FIXME: serialize and deserialize with existing routines from sqld
-        let mut body = "{\"statements\": [".to_string();
-        let mut stmts_count = 0;
-        for stmt in stmts {
-            body += &format!("{},", stmt.into());
-            stmts_count += 1;
-        }
-        if stmts_count > 0 {
-            body.pop();
-        }
-        body += "]}";
-        let request_init = RequestInit {
-            body: Some(wasm_bindgen::JsValue::from_str(&body)),
-            headers,
-            cf: CfProperties::new(),
-            method: Method::Post,
-            redirect: RequestRedirect::Follow,
-        };
-        let req = Request::new_with_init(&self.url, &request_init)?;
-        let response = Fetch::Request(req).send().await;
-        let resp: String = response?.text().await?;
-        let response_json: serde_json::Value = serde_json::from_str(&resp)?;
-        match response_json {
-            serde_json::Value::Array(results) => {
-                if results.len() != stmts_count {
-                    Err(worker::Error::from(format!(
-                        "Response array did not contain expected {stmts_count} results"
-                    )))
-                } else {
-                    let mut query_results: Vec<QueryResult> = Vec::with_capacity(stmts_count);
-                    for (idx, result) in results.into_iter().enumerate() {
-                        query_results.push(parse_query_result(result, idx)?);
-                    }
-
-                    Ok(query_results)
-                }
-            }
-            e => Err(worker::Error::from(format!(
-                "Error: {} ({:?})",
-                e, request_init.body
-            ))),
-        }
-    }
-
-    /// Executes an SQL transaction.
-    /// Does not support nested transactions - do not use BEGIN or END
-    /// inside a transaction.
-    ///
-    /// # Arguments
-    /// * `stmts` - SQL statements
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # async fn f() {
-    /// let db = libsql_client::Connection::connect("https://example.com", "admin", "s3cr3tp4ss");
-    /// let result = db
-    ///     .transaction(["CREATE TABLE t(id)", "INSERT INTO t VALUES (42)"])
-    ///     .await;
-    /// # }
-    /// ```
-    pub async fn transaction(
-        &self,
-        stmts: impl IntoIterator<Item = impl Into<Statement>>,
-    ) -> Result<Vec<QueryResult>> {
-        // TODO: Vec is not a good fit for popping the first element,
-        // let's return a templated collection instead and let the user
-        // decide where to store the result.
-        let mut ret: Vec<QueryResult> = self
-            .batch(
-                std::iter::once(Statement::new("BEGIN"))
-                    .chain(stmts.into_iter().map(|s| s.into()))
-                    .chain(std::iter::once(Statement::new("END"))),
-            )
-            .await?
-            .into_iter()
-            .skip(1)
-            .collect();
-        ret.pop();
-        Ok(ret)
+        _ => Err(anyhow!("Result {idx} was not an object",)),
     }
 }
