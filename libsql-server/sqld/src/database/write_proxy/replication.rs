77c69f05b242069873d95bc0f17bccbb0af1db40
ad hoc <postma.marin@protonmail.com>
2023-02-13 15:22:38 +0100

    enable client handshake

--- libsql-server/sqld/src/database/write_proxy/replication.rs
+++ libsql-server/sqld/src/database/write_proxy/replication.rs
@@ -21,24 +21,33 @@ use std::io::ErrorKind;
 use std::mem::size_of;
 use std::os::unix::prelude::FileExt;
 use std::path::Path;
+use std::str::FromStr;
 use std::time::Duration;
 
-use anyhow::ensure;
+use anyhow::Context;
+use bytemuck::{bytes_of, try_from_bytes, Pod, Zeroable};
+use crossbeam::channel::TryRecvError;
 use futures::StreamExt;
 use rusqlite::ffi::SQLITE_ERROR;
 use rusqlite::OpenFlags;
 use tokio::runtime::Handle;
 use tonic::transport::Channel;
+use tonic::Code;
+use uuid::Uuid;
 
 use crate::libsql::ffi::{types::XWalFrameFn, PgHdr, Wal};
 use crate::libsql::open_with_regular_wal;
 use crate::libsql::wal_hook::WalHook;
 use crate::rpc::wal_log::wal_log_rpc::{wal_log_client::WalLogClient, LogOffset};
+use crate::rpc::wal_log::wal_log_rpc::{HelloRequest, HelloResponse};
+use crate::rpc::wal_log::NO_HELLO_ERROR_MSG;
 use crate::wal_logger::{WalFrame, WAL_PAGE_SIZE};
+use crate::HARD_RESET;
 
 pub struct PeriodicDbUpdater {
     interval: Duration,
     db: rusqlite::Connection,
+    abort_receiver: crossbeam::channel::Receiver<ReplicationError>,
 }
 
 /// The `PeriodicUpdater` role is to periodically trigger a dummy write that will be intercepted by
@@ -49,7 +58,7 @@ impl PeriodicDbUpdater {
         logger: WalLogClient<Channel>,
         interval: Duration,
     ) -> anyhow::Result<Self> {
-        let hook = ReadReplicationHook::new(logger, path).await?;
+        let (hook, abort_receiver) = ReadReplicationHook::new(logger, path).await?;
         let path = path.to_owned();
         let db = tokio::task::spawn_blocking(move || {
             open_with_regular_wal(
@@ -64,23 +73,42 @@ impl PeriodicDbUpdater {
         })
         .await??;
 
-        Ok(Self { interval, db })
+        Ok(Self {
+            interval,
+            db,
+            abort_receiver,
+        })
     }
 
     /// blocking!
-    pub fn step(&mut self) {
-        // dummy write that triggers a call to xFrame
-        let _ = self.db.execute(
-            "create table if not exists __dummy__ (dummy); insert into __dummy__ values (1);",
-            (),
-        );
-        std::thread::sleep(self.interval);
+    /// return whether to continue
+    pub fn step(&mut self) -> anyhow::Result<bool> {
+        match self.abort_receiver.try_recv() {
+            Ok(e) => {
+                // received an error from loop
+                Err(e.into())
+            }
+            Err(TryRecvError::Empty) => {
+                // dummy write that triggers a call to xFrame
+                let _ = self.db.execute(
+                    "create table if not exists __dummy__ (dummy); insert into __dummy__ values (1);",
+                    (),
+                );
+                std::thread::sleep(self.interval);
+
+                Ok(true)
+            }
+
+            Err(TryRecvError::Disconnected) => {
+                // graceful exit
+                Ok(false)
+            }
+        }
     }
 }
 
 struct ReadReplicationHook {
     logger: WalLogClient<Channel>,
-    fetch_frame_index: u64,
     /// Persistent last committed index used for restarts.
     /// The File should contain two little-endian u64:
     /// - The first one is the attempted commit index before the call xFrame
@@ -88,11 +116,16 @@ struct ReadReplicationHook {
     /// After a flight of pages has been successfully written, the two numbers should be the same.
     /// On startup the two number are checked for consistency. If they differ, the database is
     /// considered corrupted, since it is impossible to know what the actually replicated index is.
-    last_applied_index_file: File,
-    last_applied_index: Option<u64>,
+    wal_index_meta_file: File,
+    wal_index_meta: WalIndexMeta,
     /// Buffer for incoming frames
     buffer: VecDeque<WalFrame>,
     rt: Handle,
+    /// A channel to send error back to the polling loop.
+    /// When an error occurs that causes an abort, this handle should be replaced with None, and
+    /// the error sent. This means that if `abort_sender` is None, we should assume a previous
+    /// abort.
+    abort_sender: Option<crossbeam::channel::Sender<ReplicationError>>,
 }
 
 /// Debug assertion. Make sure that all the pages have been applied
@@ -122,6 +155,11 @@ unsafe impl WalHook for ReadReplicationHook {
         sync_flags: c_int,
         orig: XWalFrameFn,
     ) -> c_int {
+        // A fatal error has occured before, don't do anyhting
+        if self.abort_sender.is_none() {
+            return SQLITE_ERROR;
+        }
+
         let rt = self.rt.clone();
         if let Err(e) = rt.block_on(self.fetch_log_entries()) {
             tracing::error!("error fetching log entries: {e}");
@@ -133,24 +171,14 @@ unsafe impl WalHook for ReadReplicationHook {
             assert_ne!(size_after, 0, "commit index points to non commit frame");
 
             tracing::trace!(commit = ?frame_count, size_after = size_after);
-            let attempted_commit_index = self
-                .last_applied_index
-                .map(|x| x + frame_count as u64)
-                .unwrap_or_default();
-            // pre-write index
-            self.last_applied_index_file
-                .write_all_at(&attempted_commit_index.to_le_bytes(), 0)
-                .unwrap();
-
+            self.inc_pre_commit(frame_count as u64)
+                .expect("failed to write pre-commit index");
             let ret = unsafe { orig(wal, WAL_PAGE_SIZE, page_headers, size_after, 1, sync_flags) };
 
             if ret == 0 {
                 debug_assert!(all_applied(page_headers));
-                // persist new commited index
-                self.last_applied_index_file
-                    .write_all_at(&attempted_commit_index.to_le_bytes(), size_of::<u64>() as _)
-                    .unwrap();
-                self.last_applied_index.replace(attempted_commit_index);
+                self.sync_post_commit()
+                    .expect("failed to write post-commit index");
                 // remove commited entries.
                 self.buffer.drain(..frame_count);
                 tracing::trace!("applied frame batch");
@@ -206,39 +234,170 @@ fn free_page_header(h: *const PgHdr) {
     }
 }
 
+#[repr(C)]
+#[derive(Debug, Pod, Zeroable, Clone, Copy)]
+struct WalIndexMeta {
+    /// This is the anticipated next frame index to request
+    pre_commit_index: u64,
+    /// After we have written the frames back to the wal, we set this value to the same value as
+    /// pre_commit_index
+    /// On startup we check this value against the pre-commit value to check for consistency
+    post_commit_index: u64,
+    /// Generation Uuid
+    generation_id: u128,
+    /// Uuid of the database this instance is a replica of
+    database_id: u128,
+}
+
+#[derive(Debug, thiserror::Error)]
+enum ReplicationError {
+    #[error("Replica is ahead of primary")]
+    Lagging,
+    #[error("Trying to replicate incompatible databases")]
+    DbIncompatible,
+    #[error("{0}")]
+    Other(#[from] anyhow::Error),
+    #[error("Replication loop exited")]
+    Exit,
+}
+
+impl WalIndexMeta {
+    fn read(meta_file: &File) -> anyhow::Result<Option<Self>> {
+        let mut buf = [0; size_of::<WalIndexMeta>()];
+        let meta = match meta_file.read_exact_at(&mut buf, 0) {
+            Ok(()) => {
+                meta_file.read_exact_at(&mut buf, 0)?;
+                let meta = *try_from_bytes(&buf)
+                    .map_err(|_| anyhow::anyhow!("invalid index meta file"))?;
+                Some(meta)
+            }
+            Err(e) if e.kind() == ErrorKind::UnexpectedEof => None,
+            Err(e) => Err(e)?,
+        };
+
+        Ok(meta)
+    }
+
+    /// attempts to merge two meta files.
+    fn merge_from_hello(mut self, hello: HelloResponse) -> Result<Self, ReplicationError> {
+        let hello_db_id = Uuid::from_str(&hello.database_id)
+            .context("invalid database id from primary")?
+            .as_u128();
+        let hello_gen_id = Uuid::from_str(&hello.generation_id)
+            .context("invalid generation id from primary")?
+            .as_u128();
+
+        if hello_db_id != self.database_id {
+            return Err(ReplicationError::DbIncompatible);
+        }
+
+        if self.generation_id == hello_gen_id {
+            Ok(self)
+        } else if self.pre_commit_index <= hello.generation_start_index {
+            // Ok: generation changed, but we aren't ahead of primary
+            self.generation_id = hello_gen_id;
+            Ok(self)
+        } else {
+            Err(ReplicationError::Lagging)
+        }
+    }
+
+    fn new_from_hello(hello: HelloResponse) -> anyhow::Result<WalIndexMeta> {
+        let database_id = Uuid::from_str(&hello.database_id)
+            .context("invalid database id from primary")?
+            .as_u128();
+        let generation_id = Uuid::from_str(&hello.generation_id)
+            .context("invalid generation id from primary")?
+            .as_u128();
+
+        Ok(Self {
+            pre_commit_index: 0,
+            post_commit_index: 0,
+            generation_id,
+            database_id,
+        })
+    }
+}
+
 impl ReadReplicationHook {
-    async fn new(logger: WalLogClient<Channel>, db_path: &Path) -> anyhow::Result<Self> {
+    async fn new(
+        logger: WalLogClient<Channel>,
+        db_path: &Path,
+    ) -> anyhow::Result<(Self, crossbeam::channel::Receiver<ReplicationError>)> {
         let path = db_path.join("client_wal_index");
-        let last_applied_index_file = OpenOptions::new()
+        let index_meta_file = OpenOptions::new()
             .create(true)
             .read(true)
             .write(true)
             .open(path)?;
 
-        let mut buf = [0; 2 * size_of::<u64>()];
-        let last_applied_index = match last_applied_index_file.read_exact_at(&mut buf, 0) {
-            Ok(()) => {
-                let pre = u64::from_le_bytes(buf[..size_of::<u64>()].try_into()?);
-                let post = u64::from_le_bytes(buf[size_of::<u64>()..].try_into()?);
-                ensure!(pre == post, "database corrupted");
-                Some(pre)
-            }
-            Err(e) if e.kind() == ErrorKind::UnexpectedEof => {
-                // OK: the file was empty, we don't have a log yet.
-                None
-            }
-            Err(e) => Err(e)?,
+        let (sender, receiver) = crossbeam::channel::bounded(1);
+        let mut abort_sender = Some(sender);
+        let meta = WalIndexMeta::read(&index_meta_file)?;
+        let wal_index_meta =
+            Self::perform_handshake(meta, logger.clone(), &mut abort_sender).await?;
+        let this = Self {
+            logger,
+            wal_index_meta_file: index_meta_file,
+            wal_index_meta,
+            buffer: Default::default(),
+            abort_sender,
+            rt: Handle::current(),
         };
 
-        Ok(Self {
-            logger,
-            // ask for the frame right after the one we last applied
-            fetch_frame_index: last_applied_index.map(|x| x + 1).unwrap_or_default(),
-            last_applied_index_file,
-            last_applied_index,
-            buffer: Default::default(),
-            rt: Handle::current(),
-        })
+        Ok((this, receiver))
+    }
+
+    async fn perform_handshake(
+        meta: Option<WalIndexMeta>,
+        mut logger: WalLogClient<Channel>,
+        abort_sender: &mut Option<crossbeam::channel::Sender<ReplicationError>>,
+    ) -> anyhow::Result<WalIndexMeta> {
+        let hello = logger.hello(HelloRequest {}).await?.into_inner();
+        tracing::debug!(?hello);
+        match meta {
+            Some(meta) => match meta.merge_from_hello(hello) {
+                Ok(meta) => Ok(meta),
+                Err(e @ ReplicationError::Lagging) => {
+                    tracing::error!("Replica ahead of primary: hard-reseting replica");
+                    HARD_RESET.notify_waiters();
+
+                    // We don't send an error here because we don't want the program to exit: the
+                    // hard reset flow will take care of cleaning behind us and restart fresh.
+                    abort_sender.take();
+
+                    Err(e.into())
+                }
+                Err(e @ ReplicationError::DbIncompatible) => {
+                    if let Some(sender) = abort_sender.take() {
+                        let _ = sender.send(e);
+                    }
+
+                    Err(ReplicationError::Exit.into())
+                }
+                // non-fatal error
+                Err(e) => Err(e.into()),
+            },
+            None => Ok(WalIndexMeta::new_from_hello(hello)?),
+        }
+    }
+
+    fn flush_meta(&self) -> anyhow::Result<()> {
+        self.wal_index_meta_file
+            .write_all_at(bytes_of(&self.wal_index_meta), 0)?;
+
+        Ok(())
+    }
+
+    /// Increment the pre-commit index by n, and flush the meta file
+    fn inc_pre_commit(&mut self, n: u64) -> anyhow::Result<()> {
+        self.wal_index_meta.pre_commit_index += n;
+        self.flush_meta()
+    }
+
+    fn sync_post_commit(&mut self) -> anyhow::Result<()> {
+        self.wal_index_meta.post_commit_index = self.wal_index_meta.pre_commit_index;
+        self.flush_meta()
     }
 
     /// Returns the next page headers list the log truncate count, and the commit frame for the
@@ -270,29 +429,55 @@ impl ReadReplicationHook {
 
     /// Asks the writer for new log frames to apply.
     async fn fetch_log_entries(&mut self) -> anyhow::Result<()> {
-        // try to fetch next page.
-        let start_offset = self.fetch_frame_index;
-        let req = LogOffset { start_offset };
+        let current_offset = self.current_frame_index();
+        let req = LogOffset { current_offset };
 
-        let mut stream = self.logger.log_entries(req).await?.into_inner();
-        while let Some(raw_frame) = stream.next().await {
-            let raw_frame = raw_frame?;
-            let frame = WalFrame::decode(raw_frame.data)?;
-            debug_assert_eq!(
-                frame.header.frame_id, self.fetch_frame_index,
-                "out of order log frame"
-            );
-            self.fetch_frame_index = frame.header.frame_id + 1;
-            self.buffer.push_back(frame);
+        match self.logger.log_entries(req).await {
+            Ok(stream) => {
+                let mut stream = stream.into_inner();
+                let mut frame_count = 0;
+                while let Some(raw_frame) = stream.next().await {
+                    let raw_frame = raw_frame?;
+                    let frame = WalFrame::decode(raw_frame.data)?;
+                    debug_assert_eq!(
+                        frame.header.frame_id, self.wal_index_meta.pre_commit_index,
+                        "out of order log frame"
+                    );
+                    frame_count += 1;
+                    self.buffer.push_back(frame);
+                }
+
+                tracing::debug!(current_offset, frame_count,);
+
+                Ok(())
+            }
+            Err(s) if s.code() == Code::FailedPrecondition && s.message() == NO_HELLO_ERROR_MSG => {
+                tracing::info!("Primary restarted, perfoming hanshake again");
+                self.wal_index_meta = Self::perform_handshake(
+                    Some(self.wal_index_meta),
+                    self.logger.clone(),
+                    &mut self.abort_sender,
+                )
+                .await?;
+
+                Ok(())
+            }
+            Err(e) => Err(e)?,
         }
+    }
 
-        tracing::trace!(
-            start_offset,
-            self.fetch_frame_index,
-            "received {} frames",
-            self.fetch_frame_index - start_offset,
-        );
-
-        Ok(())
+    /// Return the current frame index. None if we haven't received any frame yet
+    fn current_frame_index(&self) -> Option<u64> {
+        // This is how we detect a fresh start.
+        // While we could _potentially_ already have requested for index 0, and commited *only*
+        // frame 0, it's ok to reapply it.
+        if self.buffer.is_empty() && self.wal_index_meta.pre_commit_index == 0 {
+            None
+        } else {
+            // the next frame we want to fetch is the one that after the last we have commiter +
+            // the ones we have buffered
+            let index = self.wal_index_meta.pre_commit_index + self.buffer.len() as u64;
+            Some(index)
+        }
     }
 }
