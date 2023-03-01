9673adf4cca58b728db734896a7b26e8e28f9162
Jan Špaček <patek.mail@gmail.com>
2023-03-01 13:05:50 +0100

    Remove hrana-client from this repo
    It has been moved to libsql/hrana-client-ts

--- libsql-server/packages/js/hrana-client/examples/jwt_auth.mjs
+++
@@ -1,6 +0,0 @@
-import * as hrana from "@libsql/hrana-client";
-
-const client = hrana.open("ws://localhost:2023", process.env.JWT);
-const stream = client.openStream();
-console.log(await stream.queryValue("SELECT 1"));
-client.close();
