69e92d963fe6dfde3ccf361dfef97a20dab892a8
csh <458761603@qq.com>
2023-03-16 19:14:31 +0800

    fix: pass `TEXT` & `BLOB` by `malloc`

--- ext/udf/wasmedge_bindings.c
+++ ext/udf/wasmedge_bindings.c
@@ -23,6 +23,7 @@ void libsql_run_wasm(libsql_wasm_udf_api *api, sqlite3_context *context, libsql_
 
   WasmEdge_Value params[argc];
   WasmEdge_Value results[1];
+  WasmEdge_Value malloc_param[1];
 
   int mem_size = WasmEdge_MemoryInstanceGetPageSize(mem_ctx) * 65536;
   int mem_offset = mem_size;
@@ -39,50 +40,60 @@ void libsql_run_wasm(libsql_wasm_udf_api *api, sqlite3_context *context, libsql_
     case SQLITE_TEXT: {
       int text_len = sqlite3_value_bytes(argv[i]);
       const char *text = sqlite3_value_text(argv[i]);
-      if (mem_offset + text_len + 2 > mem_size) {
-        int delta = (text_len + 2 + 65535) / 65536;
-        WasmEdge_Result res = WasmEdge_MemoryInstanceGrowPage(mem_ctx, delta);
-        if (!WasmEdge_ResultOK(res)) {
-          sqlite3_result_error(context, WasmEdge_ResultGetMessage(res), -1);
-        }
+
+      malloc_param[0] = WasmEdge_ValueGenI32(text_len + 2);
+      WasmEdge_String wasmedge_func_name = WasmEdge_StringCreateByCString("malloc");
+      res = WasmEdge_VMExecute(ctx, wasmedge_func_name, malloc_param, 1, results, 1);
+      WasmEdge_StringDelete(wasmedge_func_name);
+      if (!WasmEdge_ResultOK(res)) {
+        sqlite3_result_error(context, "Execution failed", -1);
+        return;
       }
-      u8 *data = WasmEdge_MemoryInstanceGetPointer(mem_ctx, mem_offset, text_len);
+      mem_offset = WasmEdge_ValueGetI32(results[0]);
+
+      u8 *data = WasmEdge_MemoryInstanceGetPointer(mem_ctx, mem_offset, text_len + 2);
       data[0] = type;
       memcpy(data + 1, text, text_len);
       data[1 + text_len] = '\0';
       params[i] = WasmEdge_ValueGenI32(mem_offset);
-      mem_offset += text_len + 2;
       break;
     }
     case SQLITE_BLOB: {
       int blob_len = sqlite3_value_bytes(argv[i]);
       const void *blob = sqlite3_value_blob(argv[i]);
-      if (mem_offset + 4 + blob_len > mem_size) {
-        int delta = (blob_len + 1 + 65535) / 65536;
-        WasmEdge_Result res = WasmEdge_MemoryInstanceGrowPage(mem_ctx, delta);
-        if (!WasmEdge_ResultOK(res)) {
-          sqlite3_result_error(context, WasmEdge_ResultGetMessage(res), -1);
-        }
+      
+      malloc_param[0] = WasmEdge_ValueGenI32(blob_len + 5);
+      WasmEdge_String wasmedge_func_name = WasmEdge_StringCreateByCString("malloc");
+      res = WasmEdge_VMExecute(ctx, wasmedge_func_name, malloc_param, 1, results, 1);
+      WasmEdge_StringDelete(wasmedge_func_name);
+      if (!WasmEdge_ResultOK(res)) {
+        sqlite3_result_error(context, "Execution failed", -1);
+        return;
       }
-      u8 *data = WasmEdge_MemoryInstanceGetPointer(mem_ctx, mem_offset, blob_len);
+      mem_offset = WasmEdge_ValueGetI32(results[0]);
+
+      u8 *data = WasmEdge_MemoryInstanceGetPointer(mem_ctx, mem_offset, blob_len + 5);
       data[0] = type;
       sqlite3Put4byte(data + 1, blob_len);
       memcpy(data + 1 + 4, blob, blob_len);
       params[i] = WasmEdge_ValueGenI32(mem_offset);
-      mem_offset += blob_len + 1;
       break;
     }
     case SQLITE_NULL:
-      if (mem_offset + 1 > mem_size) {
-        WasmEdge_Result res = WasmEdge_MemoryInstanceGrowPage(mem_ctx, 1);
-        if (!WasmEdge_ResultOK(res)) {
-          sqlite3_result_error(context, WasmEdge_ResultGetMessage(res), -1);
-        }
+
+      malloc_param[0] = WasmEdge_ValueGenI32(1);
+      WasmEdge_String wasmedge_func_name = WasmEdge_StringCreateByCString("malloc");
+      res = WasmEdge_VMExecute(ctx, wasmedge_func_name, malloc_param, 1, results, 1);
+      WasmEdge_StringDelete(wasmedge_func_name);
+      if (!WasmEdge_ResultOK(res)) {
+        sqlite3_result_error(context, "Execution failed", -1);
+        return;
       }
+      mem_offset = WasmEdge_ValueGetI32(results[0]);
+
       u8 *data = WasmEdge_MemoryInstanceGetPointer(mem_ctx, mem_offset, 1);
       data[0] = type;
       params[i] = WasmEdge_ValueGenI32(mem_offset);
-      mem_offset++;
       break;
     }
   }
