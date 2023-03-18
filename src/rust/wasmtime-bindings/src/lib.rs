4c50a6fa1c67afc72f6edae79df6082e76076bcd
csh <458761603@qq.com>
2023-03-18 18:07:06 +0800

    perf(wasmtime): support `WASI` and pass `TEXT` & `BLOB` by `libsql_malloc`

--- src/rust/wasmtime-bindings/src/lib.rs
+++ src/rust/wasmtime-bindings/src/lib.rs
@@ -1,5 +1,6 @@
 use std::ffi::{c_char, c_void, CStr};
-use wasmtime::{Config, Engine, Instance, Module, Store, Val};
+use wasmtime::{Config, Engine, Linker, Module, Store, Val};
+use wasmtime_wasi::WasiCtxBuilder;
 
 const LIBSQL_INTEGER: i8 = 1;
 const LIBSQL_FLOAT: i8 = 2;
@@ -83,6 +84,7 @@ pub fn libsql_wasm_engine_new() -> *const c_void {
         Ok(eng) => eng,
         Err(_) => return std::ptr::null() as *const c_void,
     };
+
     let engine = Box::new(engine);
     let engine_ptr = &*engine as *const Engine as *const c_void;
     std::mem::forget(engine);
@@ -125,262 +127,186 @@ pub fn libsql_run_wasm(
     argc: i32,
     argv: *mut *mut c_void,
 ) {
-    let mut store = Store::new(unsafe { &*engine }, ());
-    let instance = match Instance::new(&mut store, unsafe { &*module }, &[]) {
-        Ok(inst) => inst,
-        Err(e) => {
-            let err = format!("Creating instance failed: {}", e);
-            unsafe {
-                ((*api).libsql_result_error)(
-                    libsql_ctx,
-                    err.as_ptr() as *const u8,
-                    err.len() as i32,
-                );
-            }
-            return;
-        }
-    };
-    let func_name: &str = match unsafe { CStr::from_ptr(func_name as *const c_char) }.to_str() {
-        Ok(s) => s,
-        Err(e) => {
-            let err = format!("Function name is not valid utf-8: {}", e);
-            unsafe {
-                ((*api).libsql_result_error)(
-                    libsql_ctx,
-                    err.as_ptr() as *const u8,
-                    err.len() as i32,
-                );
-            }
-            return;
-        }
-    };
-    let func = match instance.get_func(&mut store, func_name) {
-        Some(f) => f,
-        None => {
-            let err = format!("Function {} not found in Wasm module", func_name);
-            unsafe {
-                ((*api).libsql_result_error)(
-                    libsql_ctx,
-                    err.as_ptr() as *const u8,
-                    err.len() as i32,
-                );
-            }
-            return;
-        }
-    };
-    let memory = match instance.get_memory(&mut store, "memory") {
-        Some(mem) => mem,
-        None => {
-            unsafe {
-                ((*api).libsql_result_error)(
-                    libsql_ctx,
-                    "Memory \"memory\" not found in wasm module".as_ptr() as *const u8,
-                    -1,
-                );
-            }
-            return;
-        }
-    };
+    let engine = unsafe { &*engine };
+    let module = unsafe { &*module };
 
-    let mut mem_size = memory.size(&mut store) as usize;
-    let mut mem_offset = mem_size;
+    let run_wasm = |engine: &Engine, module: &Module| -> Result<(), String> {
+        let mut linker = Linker::new(&engine);
+        wasmtime_wasi::add_to_linker(&mut linker, |s| s)
+            .map_err(|e| format!("Add WASI failed: {}", e))?;
+        let wasi = WasiCtxBuilder::new()
+            .inherit_stdio()
+            .args(&[])
+            .map_err(|e| format!("Creating WasiCtx failed: {}", e))?
+            .build();
+        let mut store = Store::new(&engine, wasi);
 
-    let mut vals: Vec<Val> = Vec::new();
-    for i in 0..argc {
-        let arg = unsafe { *argv.offset(i as isize) };
-        match unsafe { ((*api).libsql_value_type)(arg) } as i8 {
-            LIBSQL_INTEGER => vals.push(Val::I64(unsafe { ((*api).libsql_value_int)(arg) } as i64)),
-            LIBSQL_FLOAT => vals.push(Val::F64(
-                unsafe { ((*api).libsql_value_double)(arg) }.to_bits(),
-            )),
-            LIBSQL_TEXT => {
-                let text: &str = unsafe {
-                    match CStr::from_ptr(((*api).libsql_value_text)(arg) as *const c_char).to_str()
-                    {
-                        Ok(s) => s,
-                        Err(e) => {
-                            let err = format!("Function name is not valid utf-8: {}", e);
-                            ((*api).libsql_result_error)(
-                                libsql_ctx,
-                                err.as_ptr() as *const u8,
-                                err.len() as i32,
-                            );
-                            return;
-                        }
-                    }
-                };
-                let text_len = unsafe { ((*api).libsql_value_bytes)(arg) } as usize;
+        let instance = linker
+            .instantiate(&mut store, module)
+            .map_err(|e| format!("Creating instance failed: {}", e))?;
 
-                if mem_offset + text_len + 2 > mem_size {
-                    let delta = (text_len + 2 + 65535) / 65536;
-                    match memory.grow(&mut store, delta as u64) {
-                        Ok(_) => (),
-                        Err(e) => {
-                            let err = format!("Failed to grow memory: {}", e);
-                            unsafe {
-                                ((*api).libsql_result_error)(
-                                    libsql_ctx,
-                                    err.as_ptr() as *const u8,
-                                    err.len() as i32,
-                                );
-                            }
-                            return;
-                        }
-                    };
-                    mem_size += delta * 65536;
+        let func_name: &str = unsafe { CStr::from_ptr(func_name as *const c_char) }
+            .to_str()
+            .map_err(|e| format!("Function name is not valid utf-8: {}", e))?;
+
+        let func = instance
+            .get_func(&mut store, func_name)
+            .ok_or_else(|| format!("Function {} not found in Wasm module", func_name))?;
+
+        let memory = instance
+            .get_memory(&mut store, "memory")
+            .ok_or_else(|| format!("Memory \"memory\" not found in wasm module"))?;
+
+        let mem_size = memory.size(&mut store) as usize;
+
+        let mut vals: Vec<Val> = Vec::new();
+        for i in 0..argc {
+            let arg = unsafe { *argv.offset(i as isize) };
+            match unsafe { ((*api).libsql_value_type)(arg) } as i8 {
+                LIBSQL_INTEGER => {
+                    vals.push(Val::I64(unsafe { ((*api).libsql_value_int)(arg) } as i64))
                 }
-                let data = memory.data_mut(&mut store);
-                data[mem_offset] = LIBSQL_TEXT as u8;
-                data[mem_offset + 1..mem_offset + 1 + text_len].copy_from_slice(text.as_bytes());
-                data[mem_offset + 1 + text_len] = 0;
-
-                vals.push(Val::I32(mem_offset as i32));
-                mem_offset += text_len + 2;
-            }
-            LIBSQL_BLOB => {
-                let blob_len = unsafe { ((*api).libsql_value_bytes)(arg) } as usize;
-                let blob: &[u8] = unsafe {
-                    std::slice::from_raw_parts(
-                        ((*api).libsql_value_blob)(arg) as *const u8,
-                        blob_len,
-                    )
-                };
-                let blob_len_i32 = blob_len as i32;
-
-                if mem_offset + blob_len + 1 + 4 > mem_size {
-                    let delta = (blob_len + 1 + 4 + 65535) / 65536;
-                    match memory.grow(&mut store, delta as u64) {
-                        Ok(_) => (),
-                        Err(e) => {
-                            let err = format!("Failed to grow memory: {}", e);
-                            unsafe {
-                                ((*api).libsql_result_error)(
-                                    libsql_ctx,
-                                    err.as_ptr() as *const u8,
-                                    err.len() as i32,
-                                );
-                            }
-                            return;
-                        }
-                    };
-                    mem_size += delta * 65536;
-                }
-                let data = memory.data_mut(&mut store);
-                data[mem_offset] = LIBSQL_BLOB as u8;
-                data[mem_offset + 1..mem_offset + 1 + 4]
-                    .copy_from_slice(&blob_len_i32.to_be_bytes());
-                data[mem_offset + 1 + 4..mem_offset + 1 + 4 + blob_len].copy_from_slice(blob);
-
-                vals.push(Val::I32(mem_offset as i32));
-                mem_offset += blob_len + 4 + 1;
-            }
-            LIBSQL_NULL => {
-                if mem_offset + 1 > mem_size {
-                    match memory.grow(&mut store, 1_u64) {
-                        Ok(_) => (),
-                        Err(e) => {
-                            let err = format!("Failed to grow memory: {}", e);
-                            unsafe {
-                                ((*api).libsql_result_error)(
-                                    libsql_ctx,
-                                    err.as_ptr() as *const u8,
-                                    err.len() as i32,
-                                );
-                            }
-                            return;
-                        }
-                    };
-                    mem_size += 65536;
-                }
-                memory.data_mut(&mut store)[mem_offset] = LIBSQL_NULL as u8;
-
-                vals.push(Val::I32(mem_offset as i32));
-                mem_offset += 1;
-            }
-            _ => {
-                unsafe {
-                    ((*api).libsql_result_error)(
-                        libsql_ctx,
-                        "Unknown libSQL type".as_ptr() as *const u8,
-                        19,
-                    )
-                }
-                return;
-            }
-        }
-    }
-
-    let mut result = Val::null();
-    match func.call(&mut store, &vals, std::slice::from_mut(&mut result)) {
-        Ok(_) => (),
-        Err(e) => {
-            let err = format!("Calling function {} failed: {}", func_name, e);
-            unsafe {
-                ((*api).libsql_result_error)(
-                    libsql_ctx,
-                    err.as_ptr() as *const u8,
-                    err.len() as i32,
-                );
-            }
-            return;
-        }
-    };
-
-    match result {
-        Val::I64(v) => unsafe { ((*api).libsql_result_int)(libsql_ctx, v as i32) },
-        Val::F64(v) => unsafe { ((*api).libsql_result_double)(libsql_ctx, f64::from_bits(v)) },
-        Val::I32(v) => {
-            let v = v as usize;
-            match memory.data(&store)[v] as i8 {
+                LIBSQL_FLOAT => vals.push(Val::F64(
+                    unsafe { ((*api).libsql_value_double)(arg) }.to_bits(),
+                )),
                 LIBSQL_TEXT => {
-                    let result_str = unsafe {
-                        CStr::from_ptr(
-                            (memory.data(&store).as_ptr() as *const c_char).offset(v as isize + 1),
-                        )
+                    let text_len = unsafe { ((*api).libsql_value_bytes)(arg) } as usize;
+                    let text: &[u8] = unsafe {
+                        std::slice::from_raw_parts(((*api).libsql_value_text)(arg), text_len)
                     };
-                    let result_ptr = alloc_slice(api, result_str.to_bytes_with_nul());
-                    unsafe {
-                        ((*api).libsql_result_text)(
-                            libsql_ctx,
-                            result_ptr as *const u8,
-                            result_str.to_str().unwrap().len() as i32, // safe to unwrap, created in alloc_slice
-                            (*api).libsql_free as *const c_void,
-                        )
-                    }
+
+                    let func_name = "libsql_malloc";
+                    let func_malloc =
+                        instance.get_func(&mut store, func_name).ok_or_else(|| {
+                            format!("Function {} not found in Wasm module", func_name)
+                        })?;
+                    let params = [Val::I32((text_len + 2) as i32)];
+                    let mut result = Val::null();
+                    func_malloc
+                        .call(&mut store, &params, std::slice::from_mut(&mut result))
+                        .map_err(|e| format!("Calling function {} failed: {}", func_name, e))?;
+
+                    let mem_offset = result.i32().unwrap_or(mem_size as i32) as usize;
+
+                    let data = memory.data_mut(&mut store);
+                    data[mem_offset] = LIBSQL_TEXT as u8;
+                    data[mem_offset + 1..mem_offset + 1 + text_len].copy_from_slice(text);
+                    data[mem_offset + 1 + text_len] = 0;
+
+                    vals.push(Val::I32(mem_offset as i32));
                 }
                 LIBSQL_BLOB => {
-                    let blob_len = i32::from_be_bytes(
-                        memory.data(&store)[v + 1..v + 1 + 4].try_into().unwrap(), // safe to unwrap, slice size == 4
-                    );
-                    let result_ptr = alloc_slice(
-                        api,
-                        &memory.data(&store)[v + 1 + 4..v + 1 + 4 + blob_len as usize],
-                    );
-                    unsafe {
-                        ((*api).libsql_result_blob)(
-                            libsql_ctx,
-                            result_ptr as *const c_void,
+                    let blob_len = unsafe { ((*api).libsql_value_bytes)(arg) } as usize;
+                    let blob: &[u8] = unsafe {
+                        std::slice::from_raw_parts(
+                            ((*api).libsql_value_blob)(arg) as *const u8,
                             blob_len,
-                            (*api).libsql_free as *const c_void,
                         )
-                    }
+                    };
+                    let blob_len_i32 = blob_len as i32;
+
+                    let func_name = "libsql_malloc";
+                    let func_malloc =
+                        instance.get_func(&mut store, func_name).ok_or_else(|| {
+                            format!("Function {} not found in Wasm module", func_name)
+                        })?;
+                    let params = [Val::I32((blob_len_i32 + 5) as i32)];
+                    let mut result = Val::null();
+                    func_malloc
+                        .call(&mut store, &params, std::slice::from_mut(&mut result))
+                        .map_err(|e| format!("Calling function {} failed: {}", func_name, e))?;
+
+                    let mem_offset = result.i32().unwrap_or(mem_size as i32) as usize;
+
+                    let data = memory.data_mut(&mut store);
+                    data[mem_offset] = LIBSQL_BLOB as u8;
+                    data[mem_offset + 1..mem_offset + 1 + 4]
+                        .copy_from_slice(&blob_len_i32.to_be_bytes());
+                    data[mem_offset + 1 + 4..mem_offset + 1 + 4 + blob_len].copy_from_slice(blob);
+
+                    vals.push(Val::I32(mem_offset as i32));
                 }
-                LIBSQL_NULL => unsafe { ((*api).libsql_result_null)(libsql_ctx) },
-                _ => unsafe {
-                    ((*api).libsql_result_error)(
-                        libsql_ctx,
-                        "Malformed result type byte".as_ptr() as *const u8,
-                        26,
-                    )
-                },
+                LIBSQL_NULL => {
+                    let func_name = "libsql_malloc";
+                    let func_malloc =
+                        instance.get_func(&mut store, func_name).ok_or_else(|| {
+                            format!("Function {} not found in Wasm module", func_name)
+                        })?;
+                    let params = [Val::I32(1)];
+                    let mut result = Val::null();
+                    func_malloc
+                        .call(&mut store, &params, std::slice::from_mut(&mut result))
+                        .map_err(|e| format!("Calling function {} failed: {}", func_name, e))?;
+
+                    let mem_offset = result.i32().unwrap_or(mem_size as i32) as usize;
+
+                    memory.data_mut(&mut store)[mem_offset] = LIBSQL_NULL as u8;
+
+                    vals.push(Val::I32(mem_offset as i32));
+                }
+                _ => {
+                    return Err(format!("Unknown libSQL type"));
+                }
             }
         }
-        _ => unsafe {
-            ((*api).libsql_result_error)(
-                libsql_ctx,
-                "Malformed result type".as_ptr() as *const u8,
-                21,
-            )
+
+        let mut result = Val::null();
+        func.call(&mut store, &vals, std::slice::from_mut(&mut result))
+            .map_err(|e| format!("Calling function {} failed: {}", func_name, e))?;
+
+        match result {
+            Val::I64(v) => unsafe { ((*api).libsql_result_int)(libsql_ctx, v as i32) },
+            Val::F64(v) => unsafe { ((*api).libsql_result_double)(libsql_ctx, f64::from_bits(v)) },
+            Val::I32(v) => {
+                let v = v as usize;
+                match memory.data(&store)[v] as i8 {
+                    LIBSQL_TEXT => {
+                        let result_str = unsafe {
+                            CStr::from_ptr(
+                                (memory.data(&store).as_ptr() as *const c_char)
+                                    .offset(v as isize + 1),
+                            )
+                        };
+                        let result_ptr = alloc_slice(api, result_str.to_bytes_with_nul());
+                        unsafe {
+                            ((*api).libsql_result_text)(
+                                libsql_ctx,
+                                result_ptr as *const u8,
+                                result_str.to_str().unwrap().len() as i32, // safe to unwrap, created in alloc_slice
+                                (*api).libsql_free as *const c_void,
+                            )
+                        }
+                    }
+                    LIBSQL_BLOB => {
+                        let blob_len = i32::from_be_bytes(
+                            memory.data(&store)[v + 1..v + 1 + 4].try_into().unwrap(), // safe to unwrap, slice size == 4
+                        );
+                        let result_ptr = alloc_slice(
+                            api,
+                            &memory.data(&store)[v + 1 + 4..v + 1 + 4 + blob_len as usize],
+                        );
+                        unsafe {
+                            ((*api).libsql_result_blob)(
+                                libsql_ctx,
+                                result_ptr as *const c_void,
+                                blob_len,
+                                (*api).libsql_free as *const c_void,
+                            )
+                        }
+                    }
+                    LIBSQL_NULL => unsafe { ((*api).libsql_result_null)(libsql_ctx) },
+                    _ => return Err(format!("Malformed result type byte")),
+                }
+            }
+            _ => return Err(format!("Malformed result type")),
+        }
+        Ok(())
+    };
+
+    match run_wasm(engine, module) {
+        Ok(_) => {}
+        Err(err) => unsafe {
+            ((*api).libsql_result_error)(libsql_ctx, err.as_ptr() as *const u8, err.len() as i32);
         },
     }
 }
