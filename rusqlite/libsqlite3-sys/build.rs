e62f497fce55f93b1e2b9d835786787549b91e62
gwenn <gtreguier@gmail.com>
2023-03-25 15:38:24 +0100

    Fix sqlite3_cancel_auto_extension

--- rusqlite/libsqlite3-sys/build.rs
+++ rusqlite/libsqlite3-sys/build.rs
@@ -527,13 +527,28 @@ mod bindings {
             .raw_line(
                 r#"extern "C" {
     pub fn sqlite3_auto_extension(
-        xEntryPoint: ::std::option::Option<unsafe extern "C" fn(db: *mut sqlite3,
-         pzErrMsg: *mut *const ::std::os::raw::c_char,
-         pThunk: *const sqlite3_api_routines
-         ) -> ::std::os::raw::c_int>,
+        xEntryPoint: ::std::option::Option<
+            unsafe extern "C" fn(
+                db: *mut sqlite3,
+                pzErrMsg: *mut *const ::std::os::raw::c_char,
+                pThunk: *const sqlite3_api_routines,
+            ) -> ::std::os::raw::c_int,
+        >,
     ) -> ::std::os::raw::c_int;
 }"#,
             )
+            .blocklist_function("sqlite3_cancel_auto_extension")
+            .raw_line(r#"extern "C" {
+    pub fn sqlite3_cancel_auto_extension(
+        xEntryPoint: ::std::option::Option<
+            unsafe extern "C" fn(
+                db: *mut sqlite3,
+                pzErrMsg: *mut *const ::std::os::raw::c_char,
+                pThunk: *const sqlite3_api_routines,
+            ) -> ::std::os::raw::c_int,
+        >,
+    ) -> ::std::os::raw::c_int;
+}"#)
             .rustfmt_bindings(true);
 
         if cfg!(any(feature = "sqlcipher", feature = "bundled-sqlcipher")) {
