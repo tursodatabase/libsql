69c53795080dcbd9e09c220cb1cb1dcb64ab70aa
Jan Špaček <patek.mail@gmail.com>
2023-03-28 17:29:18 +0200

    Fix a typo in hrana proto definition

--- libsql-server/sqld/src/hrana/proto.rs
+++ libsql-server/sqld/src/hrana/proto.rs
@@ -24,7 +24,7 @@ pub enum ServerMsg {
 #[serde(tag = "type", rename_all = "snake_case")]
 pub enum Request {
     OpenStream(OpenStreamReq),
-    CloseStream(OpenStreamReq),
+    CloseStream(CloseStreamReq),
     Execute(ExecuteReq),
     Batch(BatchReq),
 }
