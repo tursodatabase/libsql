2127a19d101541c3e891ef0a9ad235838085d010
ad hoc <postma.marin@protonmail.com>
2023-04-03 19:36:41 +0200

    handle fatal replication error correctly

--- sqld/src/replication/mod.rs
+++ sqld/src/replication/mod.rs
@@ -11,5 +11,5 @@ pub const WAL_PAGE_SIZE: i32 = 4096;
 pub const WAL_MAGIC: u64 = u64::from_le_bytes(*b"SQLDWAL\0");
 const CRC_64_GO_ISO: Crc<u64> = Crc::<u64>::new(&crc::CRC_64_GO_ISO);
 
-/// The frame uniquely identifing, monotonically increasing number
+/// The frame uniquely identifying, monotonically increasing number
 pub type FrameNo = u64;
