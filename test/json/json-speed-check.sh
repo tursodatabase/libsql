ddc8b9ee0e3bccd6e488503750b31de5c672250d
drh <>
2023-04-26 13:52:20 +0000

    Fix to the json-speed-check.sh script.
    FossilOrigin-Name: d839c9544d7f28c1abc779eb2d40f95c1a9386984656fbd29d19b1e7830171bc

--- test/json/json-speed-check.sh
+++ test/json/json-speed-check.sh
@@ -68,7 +68,8 @@ echo "CC_OPTS        = $CC_OPTS" | tee -a summary-$NAME.txt
 rm -f cachegrind.out.* jsonshell
 $CC -g -Os -Wall -I. $CC_OPTS ./shell.c ./sqlite3.c -o jsonshell -ldl -lpthread
 ls -l jsonshell | tee -a summary-$NAME.txt
-home=/home/drh/sqlite/json-perf
+home=`echo $0 | sed -e 's,/[^/]*$,,'`
+echo ./jsonshell $home/json100mb.db "<$home/json-q1.txt"
 valgrind --tool=cachegrind ./jsonshell $home/json100mb.db <$home/json-q1.txt \
       2>&1 | tee -a summary-$NAME.txt
 cg_anno.tcl cachegrind.out.* >jout-$NAME.txt
