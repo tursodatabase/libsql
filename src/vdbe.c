d5de1f39b51ba95aab5ce51387eef9dfe8dff093
drh <>
2023-04-27 23:40:03 +0000

    Remove an ALWAYS() macro that can in fact sometimes be false. [forum:/forumpost/f0e872fcee|Forum post f0e872fcee].
    FossilOrigin-Name: d85880e4ed89e2198beea642818051f7a91a71fea9929be0a7ed0603b493ce47

--- src/vdbe.c
+++ src/vdbe.c
@@ -2745,7 +2745,7 @@ case OP_IfNullRow: {         /* jump */
   VdbeCursor *pC;
   assert( pOp->p1>=0 && pOp->p1<p->nCursor );
   pC = p->apCsr[pOp->p1];
-  if( ALWAYS(pC) && pC->nullRow ){
+  if( pC && pC->nullRow ){
     sqlite3VdbeMemSetNull(aMem + pOp->p3);
     goto jump_to_p2;
   }
