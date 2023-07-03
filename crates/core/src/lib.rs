5c4c2d5d9237af9adfb678e3843f4e54087f1029
Pekka Enberg <penberg@iki.fi>
2023-07-03 09:22:27 +0300

    RustDoc updates

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,12 +1,37 @@
-//! libSQL API with batteries included.
+//! # LibSQL API for Rust
 //!
-//! Example usage:
+//! LibSQL is an embeddable SQL database engine based on SQLite.
+//! This Rust API is a batteries-included wrapper around the SQLite C API to support transparent replication while retaining compatibility with the SQLite ecosystem, such as the SQL dialect and extensions. If you are building an application in Rust, this is the crate you should use.
+//! There are also libSQL language bindings of this Rust crate to other languages such as [JavaScript](), Python, Go, and C.
+//!
+//! ## Getting Started
+//!
+//! To get started, you first need to create a [`Database`] object and then open a [`Connection`] to it, which you use to query:
 //!
 //! ```rust
 //! let db = libsql::Database::open(":memory:");
+//! let conn = db.connect();
 //! conn.execute("CREATE TABLE IF NOT EXISTS users (email TEXT)") .unwrap();
 //! conn.execute("INSERT INTO users (email) VALUES ('alice@example.org')").unwrap();
 //! ```
+//!
+//! ## Embedded Replicas
+//!
+//! Embedded replica is libSQL database that's running in your application process, which keeps a local copy of a remote database.
+//! They are useful if you want to move data in the memory space of your application for fast access.
+//!
+//! You can open an embedded replica by passing an URL to the [`Database::open()`] method and calling the [`Database::sync()`] method to synchronize the replica with the primary:
+//!
+//! ```rust
+//! let db = libsql::Database::open("libsql://database.example.org");
+//! db.sync();
+//! let conn = db.connect();
+//! conn.execute("SELECT * FROM users").unwrap();
+//! ```
+//!
+//! ## Examples
+//!
+//! You can find more examples in the [`examples`](https://github.com/penberg/libsql-experimental/tree/libsql-api/crates/core/examples) directory.
 
 pub mod connection;
 pub mod database;
