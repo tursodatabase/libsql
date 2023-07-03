6cac023c52490f6c20f3e90295f4730954d747a0
Pekka Enberg <penberg@iki.fi>
2023-07-03 19:55:32 +0300

    core: Add more microbenchmarks

--- crates/core/benches/benchmark.rs
+++ crates/core/benches/benchmark.rs
@@ -12,6 +12,7 @@ fn bench(c: &mut Criterion) {
 
     let db = bench_db();
     let conn = db.connect().unwrap();
+
     group.bench_function("select 1", |b| {
         b.iter(|| {
             let rows = conn.execute("SELECT 1").unwrap().unwrap();
@@ -22,6 +23,29 @@ fn bench(c: &mut Criterion) {
 
     let stmt = conn.prepare("SELECT 1").unwrap();
     group.bench_function("select 1 (prepared)", |b| {
+        b.iter(|| {
+            let rows = stmt.execute().unwrap();
+            let row = rows.next().unwrap().unwrap();
+            assert_eq!(row.get::<i32>(0).unwrap(), 1);
+        });
+    });
+
+    conn.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT)").unwrap();
+    for _ in 0..1000 {
+        conn.execute("INSERT INTO users (name) VALUES ('FOO')").unwrap();
+    }
+
+    let stmt = conn.prepare("SELECT * FROM users LIMIT 1").unwrap();
+    group.bench_function("SELECT * FROM users LIMIT 1", |b| {
+        b.iter(|| {
+            let rows = stmt.execute().unwrap();
+            let row = rows.next().unwrap().unwrap();
+            assert_eq!(row.get::<i32>(0).unwrap(), 1);
+        });
+    });
+
+    let stmt = conn.prepare("SELECT * FROM users LIMIT 100").unwrap();
+    group.bench_function("SELECT * FROM users LIMIT 100", |b| {
         b.iter(|| {
             let rows = stmt.execute().unwrap();
             let row = rows.next().unwrap().unwrap();
