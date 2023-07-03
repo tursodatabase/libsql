3d592b3a25c17be574fe1891ff61adea17dfff56
Pekka Enberg <penberg@iki.fi>
2023-07-03 13:26:13 +0300

    Document Connection API

--- crates/core/src/connection.rs
+++ crates/core/src/connection.rs
@@ -10,6 +10,7 @@ pub struct Connection {
 unsafe impl Send for Connection {} // TODO: is this safe?
 
 impl Connection {
+    /// Connect to the database.
     pub(crate) fn connect(db: &Database) -> Result<Connection> {
         let mut raw = std::ptr::null_mut();
         let url = db.url.clone();
@@ -31,21 +32,38 @@ impl Connection {
         Ok(Connection { raw })
     }
 
+    /// Disconnect from the database.
     pub fn disconnect(&self) {
         unsafe {
             libsql_sys::sqlite3_close_v2(self.raw);
         }
     }
 
+    /// Prepare the SQL statement.
     pub fn prepare<S: Into<String>>(&self, sql: S) -> Result<Statement> {
         Statement::prepare(self.raw, sql.into().as_str())
     }
 
+    /// Execute the SQL statement synchronously.
+    ///
+    /// If you execute a SQL query statement (e.g. `SELECT` statement) that
+    /// returns rows, then this method returns `Some(Rows)`on success; otherwise
+    /// this method returns `None`.
+    ///
+    /// This method blocks the thread until the SQL statement is executed.
+    /// However, for SQL query statements, the method blocks only until the
+    /// first row is available. To fetch all rows, you need to call `Rows::next()`
+    /// consecutively.
     pub fn execute<S: Into<String>>(&self, sql: S) -> Result<Option<Rows>> {
         let stmt = Statement::prepare(self.raw, sql.into().as_str())?;
         Ok(stmt.execute())
     }
 
+    /// Execute the SQL statement synchronously.
+    ///
+    /// This method never blocks the thread until, but instead returns a
+    /// `RowsFuture` object immediately that can be used to deferredly
+    /// execute the statement.
     pub fn execute_async<S: Into<String>>(&self, sql: S) -> RowsFuture {
         RowsFuture {
             raw: self.raw,
