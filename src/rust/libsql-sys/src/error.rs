a125b1933b1c0d8cedcc0ee2a78cd83871ce212e
Piotr Sarna <sarna@chiselstrike.com>
2023-07-05 13:12:22 +0200

    libsql-sys 0.2.5

--- src/rust/libsql-sys/src/error.rs
+++ src/rust/libsql-sys/src/error.rs
@@ -5,4 +5,13 @@ pub enum Error {
     Bug(&'static str),
 }
 
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::LibError(e) => write!(f, "LibError({})", e),
+            Self::Bug(e) => write!(f, "Bug({})", e),
+        }
+    }
+}
+
 pub type Result<T> = std::result::Result<T, Error>;
