555f65f4194d7ed37fb2d3ade08d55db5b5ab840
Piotr Sarna <sarna@turso.tech>
2023-07-27 14:46:06 +0200

    crates/core: add last_insert_rowid()

--- crates/core/src/connection.rs
+++ crates/core/src/connection.rs
@@ -101,6 +101,10 @@ impl Connection {
     pub fn changes(&self) -> u64 {
         unsafe { ffi::sqlite3_changes64(self.raw) as u64 }
     }
+
+    pub fn last_insert_rowid(&self) -> i64 {
+        unsafe { ffi::sqlite3_last_insert_rowid(self.raw) }
+    }
 }
 
 // Automatically drop all dangling statements when the connection is dropped.
