e809d84618287b24129d414e6c5878959cdb89d0
drh <>
2023-07-29 21:22:26 +0000

    Do not try to apply MEM_Term to a MEM_Ephem strings.
    FossilOrigin-Name: 2a378a66783ce4d749f7e1e7d08d8404a4aa78d47aeeefd6794b6753188438f6

--- src/vdbemem.c
+++ src/vdbemem.c
@@ -322,7 +322,7 @@ int sqlite3VdbeMemClearAndResize(Mem *pMem, int szNew){
 ** this routine is a no-op.
 */
 void sqlite3VdbeMemZeroTerminateIfAble(Mem *pMem){
-  if( (pMem->flags & (MEM_Str|MEM_Term))!=MEM_Str ) return;
+  if( (pMem->flags & (MEM_Str|MEM_Term|MEM_Ephem))!=MEM_Str ) return;
   if( pMem->enc!=SQLITE_UTF8 ) return;
   if( NEVER(pMem->z==0) ) return;
   if( pMem->flags & MEM_Dyn ){
