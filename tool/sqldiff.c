675342a5a85082fa681c0dfb8fef94e6b9c0e50e
drh <>
2023-08-22 16:44:33 +0000

    Fix a harmless memory leak in the sqldiff utility.
    FossilOrigin-Name: 724bc15701f3f647c741b614d4ac4fd98e1cdfe49a85f48c64fb1df5b83811b0

--- tool/sqldiff.c
+++ tool/sqldiff.c
@@ -596,7 +596,9 @@ static void diff_one_table(const char *zTab, FILE *out){
 
   /* Build the comparison query */
   for(n2=n; az2[n2]; n2++){
-    fprintf(out, "ALTER TABLE %s ADD COLUMN %s;\n", zId, safeId(az2[n2]));
+    char *zTab = safeId(az2[n2]);
+    fprintf(out, "ALTER TABLE %s ADD COLUMN %s;\n", zId, zTab);
+    sqlite3_free(zTab);
   }
   nQ = nPk2+1+2*(n2-nPk2);
   if( n2>nPk2 ){
