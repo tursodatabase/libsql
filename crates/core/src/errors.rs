c03c172414f50adc221339d7be09e39686436d37
Pekka Enberg <penberg@iki.fi>
2023-08-30 16:27:44 +0300

    libsql/core: Improve error message when sync is not supported

--- crates/core/src/errors.rs
+++ crates/core/src/errors.rs
@@ -24,8 +24,8 @@ pub enum Error {
     ToSqlConversionFailure(crate::BoxError),
     #[error("Hrana: `{0}`")]
     Hrana(#[from] crate::v2::HranaError),
-    #[error("Sync is not supported")]
-    SyncNotSupported,
+    #[error("Sync is not supported in databases opened in {0} mode.")]
+    SyncNotSupported(String),
     #[error("Column not found: {0}")]
     ColumnNotFound(i32),
 }
