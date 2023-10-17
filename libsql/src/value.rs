6350dd48814705ec1b313290a5508bc042ebf66c
Jonah Lund <jlunddev.64@proton.me>
2023-10-17 06:41:34 +0200

    Support Option<T> conversion for Value

--- libsql/src/value.rs
+++ libsql/src/value.rs
@@ -178,6 +178,15 @@ impl From<Vec<u8>> for Value {
     }
 }
 
+impl<T: Into<Value>> From<Option<T>> for Value {
+    fn from(value: Option<T>) -> Self {
+        match value {
+            Some(inner) => inner.into(),
+            None => Value::Null,
+        }
+    }
+}
+
 #[cfg(feature = "core")]
 impl From<libsql_sys::Value> for Value {
     fn from(value: libsql_sys::Value) -> Value {
