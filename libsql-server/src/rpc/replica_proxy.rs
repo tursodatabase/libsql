20bcca17e02018342cf6a2651700e0c84164091e
ad hoc <postma.marin@protonmail.com>
2023-10-31 15:04:00 +0100

    implement replica rpc streaming proxy

--- libsql-server/src/rpc/replica_proxy.rs
+++ libsql-server/src/rpc/replica_proxy.rs
@@ -1,14 +1,14 @@
 use std::sync::Arc;
 
 use hyper::Uri;
+use libsql_replication::rpc::proxy::{
+    proxy_client::ProxyClient, proxy_server::Proxy, Ack, DescribeRequest, DescribeResult,
+    DisconnectMessage, ExecReq, ExecResp, ExecuteResults, ProgramReq,
+};
+use tokio_stream::StreamExt;
 use tonic::{transport::Channel, Request, Status};
 
 use crate::auth::Auth;
-
-use super::proxy::rpc::{
-    self, proxy_client::ProxyClient, proxy_server::Proxy, Ack, DescribeRequest, DescribeResult,
-    DisconnectMessage, ExecuteResults,
-};
 
 pub struct ReplicaProxyService {
     client: ProxyClient<Channel>,
@@ -32,9 +32,34 @@ impl ReplicaProxyService {
 
 #[tonic::async_trait]
 impl Proxy for ReplicaProxyService {
+    type StreamExecStream = tonic::codec::Streaming<ExecResp>;
+
+    async fn stream_exec(
+        &self,
+        req: tonic::Request<tonic::Streaming<ExecReq>>,
+    ) -> Result<tonic::Response<Self::StreamExecStream>, tonic::Status> {
+        let (meta, ext, mut stream) = req.into_parts();
+        let stream = async_stream::stream! {
+            while let Some(it) = stream.next().await {
+                match it {
+                    Ok(it) => yield it,
+                    Err(e) => {
+                        // close the stream on error
+                        tracing::error!("error proxying stream request: {e}");
+                        break
+                    },
+                }
+            }
+        };
+        let mut req = tonic::Request::from_parts(meta, ext, stream);
+        self.do_auth(&mut req)?;
+        let mut client = self.client.clone();
+        client.stream_exec(req).await
+    }
+
     async fn execute(
         &self,
-        mut req: tonic::Request<rpc::ProgramReq>,
+        mut req: tonic::Request<ProgramReq>,
     ) -> Result<tonic::Response<ExecuteResults>, tonic::Status> {
         self.do_auth(&mut req)?;
 
