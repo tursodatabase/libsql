010fe90a54d148cf05f0113a2c20cf0beb16657b
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2023-11-06 14:48:58 +0900

    remove broken docs examples

--- libsql-remote/src/params.rs
+++ libsql-remote/src/params.rs
@@ -54,23 +54,6 @@ pub enum Params {
 }
 
 /// Convert an owned iterator into Params.
-///
-/// # Example
-///
-/// ```rust
-/// # use libsql_remote::params::params_from_iter;
-///  async fn run(conn: &) {
-///
-/// let iter = vec![1, 2, 3];
-///
-/// conn.query(
-///     "SELECT * FROM users WHERE id IN (?1, ?2, ?3)",
-///     params_from_iter(iter)
-/// )
-/// .await
-/// .unwrap();
-/// # }
-/// ```
 pub fn params_from_iter<I>(iter: I) -> impl IntoParams
 where
     I: IntoIterator,
