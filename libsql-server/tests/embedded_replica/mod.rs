9aad7c025d9326f49cab2ef14003cd470e6dadc4
ad hoc <postma.marin@protonmail.com>
2023-11-13 19:31:40 +0100

    test embedded replica reset.

--- libsql-server/tests/embedded_replica/mod.rs
+++ libsql-server/tests/embedded_replica/mod.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+use std::sync::Arc;
 
 use crate::common::http::Client;
 use crate::common::net::{init_tracing, SimServer, TestServer, TurmoilAcceptor, TurmoilConnector};
@@ -7,6 +8,7 @@ use libsql::Database;
 use serde_json::json;
 use sqld::config::{AdminApiConfig, RpcServerConfig, UserApiConfig};
 use tempfile::tempdir;
+use tokio::sync::Notify;
 use turmoil::{Builder, Sim};
 
 fn enable_libsql_logging() {
@@ -164,6 +166,129 @@ fn execute_batch() {
             INSERT INTO user (id) VALUES (2);", // COMMIT;",
         )
         .await?;
+
+        Ok(())
+    });
+
+    sim.run().unwrap();
+}
+
+#[test]
+fn replica_primary_reset() {
+    let mut sim = Builder::new().build();
+    let tmp = tempdir().unwrap();
+
+    let notify = Arc::new(Notify::new());
+    let notify_clone = notify.clone();
+
+    init_tracing();
+    sim.host("primary", move || {
+        let notify = notify_clone.clone();
+        let path = tmp.path().to_path_buf();
+        async move {
+            let make_server = || async {
+                TestServer {
+                    path: path.clone().into(),
+                    user_api_config: UserApiConfig {
+                        ..Default::default()
+                    },
+                    admin_api_config: Some(AdminApiConfig {
+                        acceptor: TurmoilAcceptor::bind(([0, 0, 0, 0], 9090)).await.unwrap(),
+                        connector: TurmoilConnector,
+                        disable_metrics: true,
+                    }),
+                    rpc_server_config: Some(RpcServerConfig {
+                        acceptor: TurmoilAcceptor::bind(([0, 0, 0, 0], 4567)).await.unwrap(),
+                        tls_config: None,
+                    }),
+                    ..Default::default()
+                }
+            };
+            let server = make_server().await;
+            let shutdown = server.shutdown.clone();
+
+            let fut = async move { server.start_sim(8080).await };
+
+            tokio::pin!(fut);
+
+            loop {
+                tokio::select! {
+                    res =  &mut fut => {
+                        res.unwrap();
+                        break
+                    }
+                    _ = notify.notified() => {
+                        shutdown.notify_waiters();
+                    },
+                }
+            }
+            // remove the wallog and start again
+            tokio::fs::remove_file(path.join("dbs/default/wallog"))
+                .await
+                .unwrap();
+            notify.notify_waiters();
+            let server = make_server().await;
+            server.start_sim(8080).await.unwrap();
+
+            Ok(())
+        }
+    });
+
+    sim.client("client", async move {
+        let primary = Database::open_remote_with_connector("http://primary:8080", "", TurmoilConnector)?;
+        let conn = primary.connect()?;
+
+        // insert a few valued into the primary
+        conn.execute("create table test (x)", ()).await.unwrap();
+        for _ in 0..50 {
+            conn.execute("insert into test values (42)", ())
+                .await
+                .unwrap();
+        }
+
+        let tmp = tempdir().unwrap();
+        let replica = Database::open_with_remote_sync_connector(tmp.path().join("data").display().to_string(), "http://primary:8080", "", TurmoilConnector).await.unwrap();
+        let replica_index = replica.sync().await.unwrap().unwrap();
+        let primary_index = Client::new()
+            .get("http://primary:9090/v1/namespaces/default/stats")
+            .await
+            .unwrap()
+            .json_value()
+            .await
+            .unwrap()["replication_index"]
+            .clone()
+            .as_u64()
+            .unwrap();
+
+        assert_eq!(replica_index, primary_index);
+
+        let replica_count = *replica.connect().unwrap().query("select count(*) from test", ()).await.unwrap().next().unwrap().unwrap().get_value(0).unwrap().as_integer().unwrap();
+        let primary_count = *primary.connect().unwrap().query("select count(*) from test", ()).await.unwrap().next().unwrap().unwrap().get_value(0).unwrap().as_integer().unwrap();
+        assert_eq!(primary_count, replica_count);
+
+        notify.notify_waiters();
+        notify.notified().await;
+        
+        // drop the replica here, to make sure not to reuse an open connection.
+        drop(replica);
+        let replica = Database::open_with_remote_sync_connector(tmp.path().join("data").display().to_string(), "http://primary:8080", "", TurmoilConnector).await.unwrap();
+        let replica_index = replica.sync().await.unwrap().unwrap();
+        let primary_index = Client::new()
+            .get("http://primary:9090/v1/namespaces/default/stats")
+            .await
+            .unwrap()
+            .json_value()
+            .await
+            .unwrap()["replication_index"]
+            .clone()
+            .as_u64()
+            .unwrap();
+
+        assert_eq!(replica_index, primary_index);
+
+        let replica_count = *replica.connect().unwrap().query("select count(*) from test", ()).await.unwrap().next().unwrap().unwrap().get_value(0).unwrap().as_integer().unwrap();
+        let primary_count = *primary.connect().unwrap().query("select count(*) from test", ()).await.unwrap().next().unwrap().unwrap().get_value(0).unwrap().as_integer().unwrap();
+        assert_eq!(primary_count, replica_count);
 
         Ok(())
     });
