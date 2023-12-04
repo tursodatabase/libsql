d3683cc3ba20f72b9c36da5eed4c7f895c59650b
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2023-12-05 07:21:13 +0900

    implemented new HttpSend trait over cloudflare http response

--- libsql/src/wasm/cloudflare.rs
+++ libsql/src/wasm/cloudflare.rs
@@ -1,8 +1,9 @@
-use crate::hrana::pipeline::ServerMsg;
-use crate::hrana::{HranaError, HttpSend, Result};
+use crate::hrana::{HranaError, HttpBody, HttpSend, Result};
 use bytes::Bytes;
+use futures::{ready, Stream};
 use std::future::Future;
 use std::pin::Pin;
+use std::task::{Context, Poll};
 use worker::wasm_bindgen::JsValue;
 
 #[derive(Debug, Copy, Clone)]
@@ -13,7 +14,7 @@ impl CloudflareSender {
         CloudflareSender(())
     }
 
-    async fn send(url: &str, auth: &str, body: String) -> Result<Bytes> {
+    async fn send(url: &str, auth: &str, body: String) -> Result<HttpBody> {
         use worker::*;
 
         let mut response = Fetch::Request(Request::new_with_init(
@@ -36,14 +37,18 @@ impl CloudflareSender {
             let body = response.text().await?;
             Err(HranaError::Api(body))
         } else {
-            let body = response.bytes().await?;
-            Ok(Bytes::from(body))
+            let body = match response.body() {
+                ResponseBody::Empty => HttpBody::Body(Bytes::new()),
+                ResponseBody::Body(body) => HttpBody::Body(Bytes::from(body.clone())),
+                _ => HttpBody::Stream(Box::new(HttpStream(response.stream()?))),
+            };
+            Ok(body)
         }
     }
 }
 
 impl<'a> HttpSend<'a> for CloudflareSender {
-    type Result = Pin<Box<dyn Future<Output = Result<Bytes>> + 'a>>;
+    type Result = Pin<Box<dyn Future<Output = Result<HttpBody>> + 'a>>;
 
     fn http_send(&'a self, url: &'a str, auth: &'a str, body: String) -> Self::Result {
         let fut = Self::send(url, auth, body);
@@ -56,5 +61,24 @@ impl From<worker::Error> for HranaError {
         // This converts it to a string due to the error type being !Send/!Sync which will break
         // a lot of stuff.
         HranaError::Http(value.to_string())
+    }
+}
+
+struct HttpStream(worker::ByteStream);
+
+impl Stream for HttpStream {
+    type Item = Result<Bytes>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let pin = Pin::new(&mut self.0);
+        let res = ready!(pin.poll_next(cx));
+        match res {
+            None => Poll::Ready(None),
+            Some(Ok(data)) => Poll::Ready(Some(Ok(Bytes::from(data)))),
+            Some(Err(e)) => Poll::Ready(Some(Err(HranaError::Http(format!(
+                "cloudflare HTTP stream error: {}",
+                e
+            ))))),
+        }
     }
 }
