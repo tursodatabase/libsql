a3569fb2ca818cfdb63296413949a6169c62b086
drh <>
2023-12-19 11:57:29 +0000

    Extra ALWAYS() macros to verify state in the sqlite3ExprCanBeNull() routine.
    FossilOrigin-Name: be19b84c9f3fe127165809908add148dbe9a827a55608b0490de7e69b7f7f191

--- src/expr.c
+++ src/expr.c
@@ -2743,6 +2743,8 @@ int sqlite3ExprCanBeNull(const Expr *p){
              p->y.pTab==0 ||  /* Reference to column of index on expression */
              (p->iColumn>=0
               && p->y.pTab->aCol!=0 /* Possible due to prior error */
+              && ALWAYS(p->iColumn>=0)
+              && ALWAYS(p->iColumn<p->y.pTab->nCol)
               && p->y.pTab->aCol[p->iColumn].notNull==0);
     default:
       return 1;
