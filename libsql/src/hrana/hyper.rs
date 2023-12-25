327e7e109337661c5e3dce002f575dde97f0c4a1
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2023-12-26 07:14:36 +0900

    Hrana HttpSend::oneshot - check if tokio runtime is active before calling

--- libsql/src/hrana/hyper.rs
+++ libsql/src/hrana/hyper.rs
@@ -84,7 +84,11 @@ impl HttpSend for HttpSender {
     }
 
     fn oneshot(self, url: Arc<str>, auth: Arc<str>, body: String) {
-        let _ = tokio::spawn(self.send(url, auth, body));
+        if let Ok(rt) = tokio::runtime::Handle::try_current() {
+            rt.spawn(self.send(url, auth, body));
+        } else {
+            tracing::warn!("couldn't send Hrana oneshot request: no tokio runtime active")
+        }
     }
 }
 
