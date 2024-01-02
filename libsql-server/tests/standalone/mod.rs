20323aa89847a0873d4364b0a268984dae23e9a0
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2024-01-02 11:54:22 +0900

    adjusted tests

--- libsql-server/tests/standalone/mod.rs
+++ libsql-server/tests/standalone/mod.rs
@@ -83,8 +83,7 @@ fn basic_metrics() {
 
         let snapshot = snapshot_metrics();
         snapshot.assert_counter("libsql_server_libsql_execute_program", 3);
-        // cursor-based execution produces two responses per execution request
-        snapshot.assert_counter("libsql_server_user_http_response", 6);
+        snapshot.assert_counter("libsql_server_user_http_response", 3);
 
         for (key, (_, _, val)) in snapshot.snapshot() {
             if key.kind() == metrics_util::MetricKind::Counter
@@ -231,10 +230,11 @@ fn basic_query_fail() {
         conn.execute("create unique index test_index on test(x)", ())
             .await?;
         conn.execute("insert into test values (12)", ()).await?;
-        assert_debug_snapshot!(conn
+        let e = conn
             .execute("insert into test values (12)", ())
             .await
-            .unwrap_err());
+            .unwrap_err();
+        assert_debug_snapshot!(e);
 
         Ok(())
     });
