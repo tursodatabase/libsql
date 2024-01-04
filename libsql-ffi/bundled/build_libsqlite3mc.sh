c504f8311b8007e8233a3e3f42880f3c16484c2e
Piotr Sarna <sarna@turso.tech>
2024-01-04 11:08:13 +0100

    libsql-ffi: move multiple-ciphers support to a feature
    Enabled with `cargo build -F multiple-ciphers`

--- libsql-ffi/bundled/build_libsqlite3mc.sh
+++ libsql-ffi/bundled/build_libsqlite3mc.sh
@@ -4,5 +4,5 @@ set -x
 
 git submodule update --init SQLite3MultipleCiphers
 cd SQLite3MultipleCiphers/build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DSQLITE3MC_STATIC=ON -DSQLITE3MC_BUILD_SHELL=OFF
 make -j12
