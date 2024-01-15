56323cb8993cda2a9e1bb249116b8dc31c35f5a4
Piotr Sarna <sarna@turso.tech>
2024-01-15 10:02:37 +0100

    libsql-server: drop stray serde(default) from allow_attach

--- libsql-server/src/query_analysis.rs
+++ libsql-server/src/query_analysis.rs
@@ -283,7 +283,6 @@ impl Statement {
                 }) => Some((expr.clone(), name.clone())),
                 _ => None,
             };
-            tracing::info!("attachiu: {:?}", attach_info);
             Ok(Statement {
                 stmt: c.to_string(),
                 kind,
