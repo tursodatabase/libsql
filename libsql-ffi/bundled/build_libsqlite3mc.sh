4824cb213fedf8f009eb744026dc87cfee03b2a6
Piotr Sarna <sarna@turso.tech>
2024-01-18 09:51:00 +0100

    libsql-ffi: add a few flags for SQLite3MultipleCiphers

--- libsql-ffi/bundled/build_libsqlite3mc.sh
+++ libsql-ffi/bundled/build_libsqlite3mc.sh
@@ -6,5 +6,7 @@ git submodule update --init SQLite3MultipleCiphers
 mkdir -p SQLite3MultipleCiphers/build
 cd SQLite3MultipleCiphers/build
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DSQLITE3MC_STATIC=ON \
-    -DCODEC_TYPE=AES256 -DSQLITE3MC_BUILD_SHELL=OFF
+    -DCODEC_TYPE=AES256 -DSQLITE3MC_BUILD_SHELL=OFF \
+    -DSQLITE_SHELL_IS_UTF8=OFF -DSQLITE_USER_AUTHENTICATION=OFF \
+    -DSQLITE_SECURE_DELETE=OFF
 make -j12
