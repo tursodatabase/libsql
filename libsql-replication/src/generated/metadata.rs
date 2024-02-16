398846926ca45570659bf957fdbc02ad36f84859
ad hoc <postma.marin@protonmail.com>
2024-02-16 15:47:14 +0100

    update proto

--- libsql-replication/src/generated/metadata.rs
+++ libsql-replication/src/generated/metadata.rs
@@ -23,4 +23,6 @@ pub struct DatabaseConfig {
     pub txn_timeout_s: ::core::option::Option<u64>,
     #[prost(bool, tag = "9")]
     pub allow_attach: bool,
+    #[prost(uint64, optional, tag = "10")]
+    pub max_row_size: ::core::option::Option<u64>,
 }
