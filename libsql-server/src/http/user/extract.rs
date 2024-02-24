86a2b6ddb67d8b63dda866bbbd15a74266a9d566
ad hoc <postma.marin@protonmail.com>
2024-02-24 11:05:23 +0100

    extract RequestContext

---
+++ libsql-server/src/http/user/extract.rs
@@ -0,0 +1,46 @@
+use axum::extract::FromRequestParts;
+
+use crate::{
+    auth::{Jwt, UserAuthContext, UserAuthStrategy},
+    connection::RequestContext,
+    namespace::MakeNamespace,
+};
+
+use super::{db_factory, AppState};
+
+#[async_trait::async_trait]
+impl<F> FromRequestParts<AppState<F>> for RequestContext
+where
+    F: MakeNamespace,
+{
+    type Rejection = crate::error::Error;
+
+    async fn from_request_parts(
+        parts: &mut axum::http::request::Parts,
+        state: &AppState<F>,
+    ) -> std::result::Result<Self, Self::Rejection> {
+        let namespace = db_factory::namespace_from_headers(
+            &parts.headers,
+            state.disable_default_namespace,
+            state.disable_namespaces,
+        )?;
+
+        let namespace_jwt_key = state
+            .namespaces
+            .with(namespace.clone(), |ns| ns.jwt_key())
+            .await??;
+
+        let auth_header = parts.headers.get(hyper::header::AUTHORIZATION);
+
+        let auth = match namespace_jwt_key {
+            Some(key) => Jwt::new(key).authenticate(UserAuthContext {
+                user_credential: auth_header.cloned(),
+            })?,
+            None => state.user_auth_strategy.authenticate(UserAuthContext {
+                user_credential: auth_header.cloned(),
+            })?,
+        };
+
+        Ok(Self::new(auth, namespace, state.namespaces.meta_store()))
+    }
+}
