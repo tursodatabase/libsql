4e0ce557a8fce0f9fe86b740c2f701b2d388e82d
Pekka Enberg <penberg@iki.fi>
2024-03-05 21:05:46 +0200

    libsql: Rename `periodic_sync` to `sync_interval` (#1125)
    Rename the configuration option for periodic sync to `sync_interval` to
    align with what we're doing with the rest of the SDKs.

--- libsql-server/tests/embedded_replica/mod.rs
+++ libsql-server/tests/embedded_replica/mod.rs
@@ -847,7 +847,7 @@ fn freeze() {
 }
 
 #[test]
-fn periodic_sync() {
+fn sync_interval() {
     let mut sim = Builder::new().build();
 
     let tmp_embedded = tempdir().unwrap();
@@ -870,7 +870,7 @@ fn periodic_sync() {
             "".to_string(),
         )
         .connector(TurmoilConnector)
-        .periodic_sync(Duration::from_millis(100))
+        .sync_interval(Duration::from_millis(100))
         .build()
         .await?;
 
