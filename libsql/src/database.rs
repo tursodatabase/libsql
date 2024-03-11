a50d1c328504cbe42d9c8b65c652dde11c7879bb
Lucio Franco <luciofranco14@gmail.com>
2024-03-11 07:48:03 -0700

    fix error import

--- libsql/src/database.rs
+++ libsql/src/database.rs
@@ -588,7 +588,7 @@ fn connector() -> Result<hyper_rustls::HttpsConnector<hyper::client::HttpConnect
 
     Ok(hyper_rustls::HttpsConnectorBuilder::new()
         .with_native_roots()
-        .map_err(Error::InvalidTlsConfiguration)?
+        .map_err(crate::Error::InvalidTlsConfiguration)?
         .https_or_http()
         .enable_http1()
         .wrap_connector(http))
