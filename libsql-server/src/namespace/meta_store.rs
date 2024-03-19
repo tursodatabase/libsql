608e616674672c992c0d8d4d93b3163c03c1a374
Lucio Franco <luciofranco14@gmail.com>
2024-03-19 08:43:57 -0400

    server: fix conflict on attach update

--- libsql-server/src/namespace/meta_store.rs
+++ libsql-server/src/namespace/meta_store.rs
@@ -349,7 +349,7 @@ fn try_process(
             }
         }
         tx.execute(
-            "INSERT OR REPLACE INTO namespace_configs (namespace, config) VALUES (?1, ?2)",
+            "INSERT OR REPLACE INTO namespace_configs (namespace, config) VALUES (?1, ?2) ON CONFLICT(namespace) DO UPDATE SET config=excluded.config",
             rusqlite::params![namespace.as_str(), config_encoded],
         )?;
         tx.execute(
