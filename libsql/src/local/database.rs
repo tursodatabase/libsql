65a8bfb6ab01f327fdbe09e7c8dadba691f40837
Piotr Jastrzębski <haaawk@gmail.com>
2024-04-17 11:02:34 +0200

    Prefetch frames during handshake (#1317)
    * Prefetch frames in handshake
    
    Signed-off-by: Piotr Jastrzebski <piotr@chiselstrike.com>
    
    * Remove unwraps from replication client creation
    
    Signed-off-by: Piotr Jastrzebski <piotr@chiselstrike.com>
    
    * remove secondary client
    
    ---------
    
    Signed-off-by: Piotr Jastrzebski <piotr@chiselstrike.com>
    Co-authored-by: ad hoc <postma.marin@protonmail.com>

--- libsql/src/local/database.rs
+++ libsql/src/local/database.rs
@@ -1,6 +1,7 @@
 use std::sync::Once;
 
 cfg_replication!(
+    use http::uri::InvalidUri;
     use crate::database::EncryptionConfig;
     use libsql_replication::frame::FrameNo;
 
@@ -90,13 +91,16 @@ impl Database {
 
         let endpoint = coerce_url_scheme(endpoint);
         let remote = crate::replication::client::Client::new(
-            connector,
-            endpoint.as_str().try_into().unwrap(),
-            auth_token,
+            connector.clone(),
+            endpoint
+                .as_str()
+                .try_into()
+                .map_err(|e: InvalidUri| crate::Error::Replication(e.into()))?,
+            auth_token.clone(),
             version.as_deref(),
-            http_request_callback,
+            http_request_callback.clone(),
         )
-        .unwrap();
+        .map_err(|e| crate::Error::Replication(e.into()))?;
         let path = PathBuf::from(db_path);
         let client = RemoteClient::new(remote.clone(), &path)
             .await
@@ -164,7 +168,10 @@ impl Database {
         let endpoint = coerce_url_scheme(endpoint);
         let remote = crate::replication::client::Client::new(
             connector,
-            endpoint.as_str().try_into().unwrap(),
+            endpoint
+                .as_str()
+                .try_into()
+                .map_err(|e: InvalidUri| crate::Error::Replication(e.into()))?,
             auth_token,
             version.as_deref(),
             http_request_callback,
