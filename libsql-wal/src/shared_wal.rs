951833ccae59387e71828cfa58e28b13ace7c490
ad hoc <postma.marin@protonmail.com>
2024-04-30 18:01:26 +0200

    fix read lock busy loop

--- libsql-wal/src/shared_wal.rs
+++ libsql-wal/src/shared_wal.rs
@@ -19,7 +19,7 @@ use crate::transaction::{ReadTransaction, Savepoint, Transaction, WriteTransacti
 
 #[derive(Default)]
 pub struct WalLock {
-    tx_id: Mutex<Option<u64>>,
+    pub(crate) tx_id: Mutex<Option<u64>>,
     /// When a writer is popped from the write queue, its write transaction may not be reading from the most recent
     /// snapshot. In this case, we return `SQLITE_BUSY_SNAPHSOT` to the caller. If no reads were performed
     /// with that transaction before upgrading, then the caller will call us back immediately after re-acquiring
@@ -27,17 +27,17 @@ pub struct WalLock {
     /// Without the reserved slot, the writer would be re-enqueued, a writer before it would be inserted,
     /// and we'd find ourselves in the initial situation. Instead, we use the reserved slot to bypass the queue when the 
     /// writer tried to re-acquire the write lock.
-    reserved: Mutex<Option<u64>>,
+    pub(crate) reserved: Mutex<Option<u64>>,
     next_tx_id: AtomicU64,
-    waiters: Injector<(Unparker, u64)>,
+    pub(crate) waiters: Injector<(Unparker, u64)>,
 }
 
 pub struct SharedWal<FS: FileSystem> {
-    current: ArcSwap<CurrentSegment<FS::File>>,
-    wal_lock: Arc<WalLock>,
-    db_file: FS::File,
-    namespace: NamespaceName,
-    registry: Arc<WalRegistry<FS>>,
+    pub(crate) current: ArcSwap<CurrentSegment<FS::File>>,
+    pub(crate) wal_lock: Arc<WalLock>,
+    pub(crate) db_file: FS::File,
+    pub(crate) namespace: NamespaceName,
+    pub(crate) registry: Arc<WalRegistry<FS>>,
 }
 
 impl<FS: FileSystem> SharedWal<FS> {
@@ -49,24 +49,19 @@ impl<FS: FileSystem> SharedWal<FS> {
     pub fn begin_read(&self, conn_id: u64) -> ReadTransaction<FS::File> {
         // FIXME: this is not enough to just increment the counter, we must make sure that the segment
         // is not sealed. If the segment is sealed, retry with the current segment
-        loop {
-            let current = self.current.load();
-            if current.is_sealed() {
-                continue;
-            }
-            current.inc_reader_count();
-            let (max_frame_no, db_size) =
-                current.with_header(|header| (header.last_committed(), header.db_size()));
-            let id = self.wal_lock.next_tx_id.fetch_add(1, Ordering::Relaxed);
-            return ReadTransaction {
-                id,
-                max_frame_no,
-                current: current.clone(),
-                db_size,
-                created_at: Instant::now(),
-                conn_id,
-                pages_read: 0,
-            };
+        let current = self.current.load();
+        current.inc_reader_count();
+        let (max_frame_no, db_size) =
+            current.with_header(|header| (header.last_committed(), header.db_size()));
+        let id = self.wal_lock.next_tx_id.fetch_add(1, Ordering::Relaxed);
+        ReadTransaction {
+            id,
+            max_frame_no,
+            current: current.clone(),
+            db_size,
+            created_at: Instant::now(),
+            conn_id,
+            pages_read: 0,
         }
     }
 
@@ -128,7 +123,7 @@ impl<FS: FileSystem> SharedWal<FS> {
         // 2) that transaction held the lock to tx_id (be in a transaction critical section)
         let current = self.current.load();
         let last_commited = current.last_committed();
-        if read_tx.max_frame_no != last_commited {
+        if read_tx.max_frame_no != last_commited  || current.is_sealed() {
             if read_tx.pages_read <= 1 {
                 // this transaction hasn't read anything yet, it will retry to
                 // acquire the lock, reserved the slot so that it can make
@@ -215,5 +210,9 @@ impl<FS: FileSystem> SharedWal<FS> {
     pub fn last_committed_frame_no(&self) -> u64 {
         let current = self.current.load();
         current.last_committed_frame_no()
+    }
+
+    pub fn namespace(&self) -> &NamespaceName {
+        &self.namespace
     }
 }
