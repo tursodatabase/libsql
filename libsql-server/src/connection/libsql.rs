3ffdfa0d0402142d2aaed91a3045d94dcd24313d
Lucio Franco <luciofranco14@gmail.com>
2024-04-30 10:24:35 -0400

    add txn_timeout config to conn manager

--- libsql-server/src/connection/libsql.rs
+++ libsql-server/src/connection/libsql.rs
@@ -30,7 +30,7 @@ use super::connection_manager::{
 use super::program::{
     check_describe_auth, check_program_auth, DescribeCol, DescribeParam, DescribeResponse, Vm,
 };
-use super::{MakeConnection, Program, RequestContext};
+use super::{MakeConnection, Program, RequestContext, TXN_TIMEOUT};
 
 pub struct MakeLibSqlConn<W> {
     db_path: PathBuf,
@@ -69,6 +69,8 @@ where
         block_writes: Arc<AtomicBool>,
         resolve_attach_path: ResolveNamespacePathFn,
     ) -> Result<Self> {
+        let txn_timeout = config_store.get().txn_timeout.unwrap_or(TXN_TIMEOUT);
+
         let mut this = Self {
             db_path,
             stats,
@@ -83,7 +85,7 @@ where
             encryption_config,
             block_writes,
             resolve_attach_path,
-            connection_manager: ConnectionManager::default(),
+            connection_manager: ConnectionManager::new(txn_timeout),
         };
 
         let db = this.try_create_db().await?;
@@ -172,7 +174,7 @@ impl LibSqlConnection<libsql_sys::wal::wrapper::PassthroughWalWrapper> {
             tokio::sync::watch::channel(None).1,
             Default::default(),
             Arc::new(|_| unreachable!()),
-            ConnectionManager::default(),
+            ConnectionManager::new(TXN_TIMEOUT),
         )
         .await
         .unwrap()
