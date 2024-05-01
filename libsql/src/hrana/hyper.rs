8f62cef622822ff514db114b8fe72b38f1546099
Piotr Jastrzebski <piotr@chiselstrike.com>
2024-05-01 21:26:14 +0200

    Implement execute_transactional_batch for HRANA connection
    Signed-off-by: Piotr Jastrzebski <piotr@chiselstrike.com>

--- libsql/src/hrana/hyper.rs
+++ libsql/src/hrana/hyper.rs
@@ -7,7 +7,7 @@ use crate::hrana::{bind_params, unwrap_err, HranaError, HttpSend, Result};
 use crate::params::Params;
 use crate::transaction::Tx;
 use crate::util::ConnectorService;
-use crate::{Rows, Statement};
+use crate::{Error, Rows, Statement};
 use bytes::Bytes;
 use futures::future::BoxFuture;
 use futures::{Stream, TryStreamExt};
@@ -278,7 +278,20 @@ impl Conn for HranaStream<HttpSender> {
     }
 
     async fn execute_transactional_batch(&self, sql: &str) -> crate::Result<()> {
-        todo!("haaawk")
+        let mut stmts = Vec::new();
+        let parse = crate::parser::Statement::parse(sql);
+        for s in parse {
+            let s = s?;
+            if s.kind == crate::parser::StmtKind::TxnBegin || s.kind == crate::parser::StmtKind::TxnBeginReadOnly || s.kind == crate::parser::StmtKind::TxnEnd {
+                return Err(Error::TransactionalBatchError("Transactions forbidden inside transactional batch".to_string()));
+            }
+            stmts.push(Stmt::new(s.stmt, false));
+        }
+        let res = self
+            .batch_inner(Batch::transactional(stmts), true)
+            .await
+            .map_err(|e| crate::Error::Hrana(e.into()))?;
+        unwrap_err(res)
     }
 
     async fn prepare(&self, sql: &str) -> crate::Result<Statement> {
