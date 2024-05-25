5e1ab6ff14aaa43b42f813399368afed63128571
Piotr Jastrzebski <piotr@chiselstrike.com>
2024-05-25 10:10:01 +0200

    Add libsql_reset_stmt
    Fixes #1416
    
    Signed-off-by: Piotr Jastrzebski <piotr@chiselstrike.com>

--- bindings/c/src/lib.rs
+++ bindings/c/src/lib.rs
@@ -450,6 +450,20 @@ pub unsafe extern "C" fn libsql_execute_stmt(
 }
 
 #[no_mangle]
+pub unsafe extern "C" fn libsql_reset_stmt(
+    stmt: libsql_stmt_t,
+    out_err_msg: *mut *const std::ffi::c_char,
+) -> std::ffi::c_int {
+    if stmt.is_null() {
+        set_err_msg("Null statement".to_string(), out_err_msg);
+        return 1;
+    }
+    let stmt = stmt.get_ref_mut();
+    stmt.params.clear();
+    0
+}
+
+#[no_mangle]
 pub unsafe extern "C" fn libsql_free_stmt(stmt: libsql_stmt_t) {
     if stmt.is_null() {
         return;
