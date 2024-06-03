facb8942e47a2bf3f7dd6b7adc98a3cb645b606a
Avinash Sajjanshetty <640792+avinassh@users.noreply.github.com>
2024-06-03 10:02:44 +0530

    Add storage server (#1431)
    * Add build and version files
    
    * Add `FrameStore` trait
    
    This trait provides the methods which can be implemented by different
    storage backends like Redis, Foundation DB etc.
    
    * Add a gRPC server which implements Storage Server proto
    
    * Add storage server runner with pluggable storage backends
    
    * Add an InMemory FrameStore
    
    * Address review comments
    
    - Remove unnecessary Arc/Mutex in the Service struct
    - Chnage all `&self mut` to `&self` in the `FrameStore` trait, handle
      locking internally
    
    * Set cargo publish to false

---
+++ libsql-storage-server/src/main.rs
@@ -0,0 +1,62 @@
+mod memory_store;
+mod service;
+mod store;
+
+use std::net::SocketAddr;
+
+use anyhow::Result;
+use clap::Parser;
+use libsql_storage::rpc::storage_server::StorageServer;
+use libsql_storage_server::version::Version;
+use service::Service;
+use tonic::transport::Server;
+use tracing::trace;
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+
+#[derive(clap::ValueEnum, Clone, Debug)]
+enum StorageType {
+    InMemory,
+    Redis,
+    FoundationDB,
+}
+
+#[derive(Debug, Parser)]
+#[command(name = "libsql-storage-server")]
+#[command(about = "libSQL Storage Server", version = Version::default(), long_about = None)]
+struct Cli {
+    /// The address and port the storage RPC protocol listens to. Example: `127.0.0.1:5002`.
+    #[clap(long, env = "LIBSQL_STORAGE_LISTEN_ADDR", default_value = "[::]:5002")]
+    listen_addr: SocketAddr,
+
+    /// The type of storage backend to use. Example: `redis`
+    #[clap(value_enum, long, default_value = "in-memory")]
+    storage_type: StorageType,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("libsql_storage_server=trace"));
+    tracing::subscriber::set_global_default(
+        FmtSubscriber::builder().with_env_filter(filter).finish(),
+    )
+    .expect("setting default subscriber failed");
+
+    let args = Cli::parse();
+    let service = match args.storage_type {
+        StorageType::Redis => Service::new(),
+        StorageType::FoundationDB => Service::new(),
+        _ => Service::new(),
+    };
+
+    trace!(
+        "Starting libSQL storage server (with type {:?}) on {}",
+        args.storage_type,
+        args.listen_addr
+    );
+    Server::builder()
+        .add_service(StorageServer::new(service))
+        .serve(args.listen_addr)
+        .await?;
+    Ok(())
+}
---
+++ libsql-storage-server/src/service.rs
@@ -0,0 +1,133 @@
+use std::sync::atomic::AtomicU32;
+
+use crate::memory_store::InMemFrameStore;
+use crate::store::FrameStore;
+use libsql_storage::rpc;
+use libsql_storage::rpc::storage_server::Storage;
+use tonic::{Request, Response, Status};
+use tracing::{error, trace};
+
+pub struct Service {
+    store: Box<dyn FrameStore + Send + Sync>,
+    db_size: AtomicU32,
+}
+
+impl Service {
+    pub fn new() -> Self {
+        Self {
+            store: Box::new(InMemFrameStore::new()),
+            db_size: AtomicU32::new(0),
+        }
+    }
+    #[allow(dead_code)]
+    pub fn with_store(store: Box<dyn FrameStore + Send + Sync>) -> Self {
+        Self {
+            store,
+            db_size: AtomicU32::new(0),
+        }
+    }
+}
+
+#[tonic::async_trait]
+impl Storage for Service {
+    async fn insert_frames(
+        &self,
+        request: Request<rpc::InsertFramesRequest>,
+    ) -> Result<Response<rpc::InsertFramesResponse>, Status> {
+        let mut num_frames = 0;
+        let request = request.into_inner();
+        let namespace = request.namespace;
+        for frame in request.frames.into_iter() {
+            trace!(
+                "inserted for page {} frame {}",
+                frame.page_no,
+                self.store
+                    .insert_frame(&namespace, frame.page_no, frame.data.into())
+                    .await
+            );
+            num_frames += 1;
+            self.db_size
+                .fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+        }
+        Ok(Response::new(rpc::InsertFramesResponse { num_frames }))
+    }
+
+    async fn find_frame(
+        &self,
+        request: Request<rpc::FindFrameRequest>,
+    ) -> Result<Response<rpc::FindFrameResponse>, Status> {
+        let request = request.into_inner();
+        let page_no = request.page_no;
+        let namespace = request.namespace;
+        trace!("find_frame(page_no={})", page_no);
+        if let Some(frame_no) = self.store.find_frame(&namespace, page_no).await {
+            Ok(Response::new(rpc::FindFrameResponse {
+                frame_no: Some(frame_no),
+            }))
+        } else {
+            error!("find_frame() failed for page_no={}", page_no);
+            Ok(Response::new(rpc::FindFrameResponse { frame_no: None }))
+        }
+    }
+
+    async fn read_frame(
+        &self,
+        request: Request<rpc::ReadFrameRequest>,
+    ) -> Result<Response<rpc::ReadFrameResponse>, Status> {
+        let request = request.into_inner();
+        let frame_no = request.frame_no;
+        let namespace = request.namespace;
+        trace!("read_frame(frame_no={})", frame_no);
+        if let Some(data) = self.store.read_frame(&namespace, frame_no).await {
+            Ok(Response::new(rpc::ReadFrameResponse {
+                frame: Some(data.clone().into()),
+            }))
+        } else {
+            error!("read_frame() failed for frame_no={}", frame_no);
+            Ok(Response::new(rpc::ReadFrameResponse { frame: None }))
+        }
+    }
+
+    async fn db_size(
+        &self,
+        _request: Request<rpc::DbSizeRequest>,
+    ) -> Result<Response<rpc::DbSizeResponse>, Status> {
+        let size = self.db_size.load(std::sync::atomic::Ordering::SeqCst) as u64;
+        Ok(Response::new(rpc::DbSizeResponse { size }))
+    }
+
+    async fn frames_in_wal(
+        &self,
+        request: Request<rpc::FramesInWalRequest>,
+    ) -> Result<Response<rpc::FramesInWalResponse>, Status> {
+        let namespace = request.into_inner().namespace;
+        Ok(Response::new(rpc::FramesInWalResponse {
+            count: self.store.frames_in_wal(&namespace).await,
+        }))
+    }
+
+    async fn frame_page_num(
+        &self,
+        request: Request<rpc::FramePageNumRequest>,
+    ) -> Result<Response<rpc::FramePageNumResponse>, Status> {
+        let request = request.into_inner();
+        let frame_no = request.frame_no;
+        let namespace = request.namespace;
+        if let Some(page_no) = self.store.frame_page_no(&namespace, frame_no).await {
+            Ok(Response::new(rpc::FramePageNumResponse { page_no }))
+        } else {
+            error!("frame_page_num() failed for frame_no={}", frame_no);
+            Ok(Response::new(rpc::FramePageNumResponse { page_no: 0 }))
+        }
+    }
+
+    async fn destroy(
+        &self,
+        request: Request<rpc::DestroyRequest>,
+    ) -> Result<Response<rpc::DestroyResponse>, Status> {
+        trace!("destroy()");
+        let namespace = request.into_inner().namespace;
+        self.store.destroy(&namespace).await;
+        Ok(Response::new(rpc::DestroyResponse {}))
+    }
+}
