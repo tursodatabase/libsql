32bf08cf46db0599f2339ba899ad0b53cbe7d265
Lucio Franco <luciofranco14@gmail.com>
2024-06-14 13:21:52 -0400

    wal: add basic s3 implementation

--- libsql-wal/src/bottomless/storage/fs.rs
+++ libsql-wal/src/bottomless/storage/fs.rs
@@ -2,40 +2,79 @@ use std::future::Future;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
+use tokio::io::AsyncBufRead;
+
 use crate::bottomless::job::CompactedSegmentDataHeader;
 use crate::bottomless::{Error, Result};
 use crate::io::{FileExt, Io};
 use libsql_sys::name::NamespaceName;
 
-use super::Storage;
+use super::{SegmentMeta, Storage};
 
-pub struct FsStorage<I> {
+pub struct FsStorage<I, S> {
     prefix: PathBuf,
     io: Arc<I>,
+    remote_storage: Arc<S>,
 }
 
-impl<I: Io> FsStorage<I> {
-    fn new(prefix: PathBuf, io: I) -> Result<Self> {
+impl<I: Io, S> FsStorage<I, S> {
+    fn new(prefix: PathBuf, io: I, remote_storage: S) -> Result<Self> {
         io.create_dir_all(&prefix.join("segments")).unwrap();
 
         Ok(FsStorage {
             prefix,
             io: Arc::new(io),
+            remote_storage: Arc::new(remote_storage),
         })
     }
 }
 
+pub(crate) trait RemoteStorage: Send + Sync + 'static {
+    type FetchStream: AsyncBufRead;
+
+    fn upload(
+        &self,
+        file_path: &Path,
+        key: &str,
+        meta: &SegmentMeta,
+    ) -> impl Future<Output = Result<()>> + Send;
+
+    fn fetch(
+        &self,
+        namespace: &NamespaceName,
+        frame_no: u64,
+        out_folder: &Path,
+    ) -> impl Future<Output = Result<Self::FetchStream>> + Send;
+}
+
+impl RemoteStorage for () {
+    type FetchStream = tokio::io::Empty;
+
+    async fn upload(&self, _file_path: &Path, _key: &str, _meta: &SegmentMeta) -> Result<()> {
+        Ok(())
+    }
+
+    async fn fetch(
+        &self,
+        _namespace: &NamespaceName,
+        frame_no: u64,
+        _out_folder: &Path,
+    ) -> Result<Self::FetchStream> {
+        Err(Error::FrameNotFound(frame_no))
+    }
+}
+
 // TODO(lucio): handle errors for fs module
-impl<I: Io> Storage for FsStorage<I> {
+impl<I: Io, S: RemoteStorage> Storage for FsStorage<I, S> {
     type Config = ();
 
-    fn store(
+    async fn store(
         &self,
         config: &Self::Config,
         meta: super::SegmentMeta,
         segment_data: impl crate::io::file::FileExt,
         segment_index: Vec<u8>,
-    ) -> impl Future<Output = Result<()>> + Send {
+    ) -> Result<()> {
         let key = format!(
             "{:019}-{:019}-{:019}.segment",
             meta.start_frame_no,
@@ -43,28 +82,29 @@ impl<I: Io> Storage for FsStorage<I> {
             meta.created_at.timestamp()
         );
 
-        let path = self.prefix.join("segments").join(key);
+        let path = self.prefix.join("segments").join(&key);
 
         let buf = Vec::with_capacity(segment_data.len().unwrap() as usize);
 
         let f = self.io.open(true, false, true, &path).unwrap();
-        async move {
-            let (buf, res) = segment_data.read_exact_at_async(buf, 0).await;
+        let (buf, res) = segment_data.read_exact_at_async(buf, 0).await;
 
-            let (_, res) = f.write_all_at_async(buf, 0).await;
-            res.unwrap();
+        let (_, res) = f.write_all_at_async(buf, 0).await;
+        res?;
 
-            Ok(())
-        }
+        self.remote_storage.upload(&path, &key, &meta).await?;
+
+        Ok(())
     }
 
     async fn fetch_segment(
         &self,
         _config: &Self::Config,
-        _namespace: NamespaceName,
+        namespace: NamespaceName,
         frame_no: u64,
         dest_path: &Path,
     ) -> Result<()> {
+        // TODO(lucio): prefix also via namespace
         let dir = self.prefix.join("segments");
 
         // TODO(lucio): optimization would be to cache this list, since we update the files in the
@@ -107,6 +147,17 @@ impl<I: Io> Storage for FsStorage<I> {
             }
         }
 
+        // TODO(lucio): fetch from remote storage
+        let out_folder = PathBuf::new();
+        let reader = self
+            .remote_storage
+            .fetch(&namespace, frame_no, &out_folder)
+            .await?;
+
+        // TODO(lucio): write buf reader content into the expected destination file then hard link
+
+        // self.io.hard_link(&path, dest_path)?;
+
         Err(Error::Store("".into()))
     }
 
@@ -121,6 +172,19 @@ impl<I: Io> Storage for FsStorage<I> {
     fn default_config(&self) -> std::sync::Arc<Self::Config> {
         todo!()
     }
+}
+
+pub(super) fn parse_segment_file_name(name: &str) -> Result<(u64, u64)> {
+    let key = name.split(".").next().unwrap();
+    let mut comp = key.split("-");
+
+    let start_frame = comp.next().unwrap();
+    let end_frame = comp.next().unwrap();
+
+    let start_frame: u64 = start_frame.parse().unwrap();
+    let end_frame: u64 = end_frame.parse().unwrap();
+
+    Ok((start_frame, end_frame))
 }
 
 #[cfg(test)]
@@ -138,7 +202,7 @@ mod tests {
     #[tokio::test]
     async fn read_write() {
         let dir = tempdir().unwrap();
-        let fs = FsStorage::new(dir.path().into(), StdIO::default()).unwrap();
+        let fs = FsStorage::new(dir.path().into(), StdIO::default(), ()).unwrap();
 
         let namespace = NamespaceName::from_string("".into());
         let segment = CompactedSegmentDataHeader {
