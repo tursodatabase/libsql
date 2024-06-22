37e5f7b405d750847092994493cf8b0e3338564d
Levy Albuquerque <levyddsa@gmail.com>
2024-06-21 21:26:22 -0300

    add nix flake, only devShell for now

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            nativeBuildInputs = with pkgs; [
+              tcl
+              gnumake
+              clang
+            ];
+          };
+      }
+    );
+}
