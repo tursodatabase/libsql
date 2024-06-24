8da0b63d490dd24832a8232cd48f9c27fa31b495
ad hoc <postma.marin@protonmail.com>
2024-06-24 15:11:25 +0200

    flag sqlite mimalloc allocator (#1497)
    * flag sqlite mimalloc allocator
    
    * fix potential segv when alloc return null

--- libsql-server/src/lib.rs
+++ libsql-server/src/lib.rs
@@ -391,7 +391,9 @@ where
         static INIT: std::sync::Once = std::sync::Once::new();
         let mut join_set = JoinSet::new();
 
-        setup_sqlite_alloc();
+        if std::env::var("LIBSQL_SQLITE_MIMALLOC").is_ok() {
+            setup_sqlite_alloc();
+        }
 
         INIT.call_once(|| {
             if let Ok(size) = std::env::var("LIBSQL_EXPERIMENTAL_PAGER") {
@@ -746,6 +748,11 @@ fn setup_sqlite_alloc() {
         let size_total = size as usize + size_of::<usize>();
         let layout = Layout::from_size_align(size_total, align_of::<usize>()).unwrap();
         let ptr = GLOBAL.alloc(layout);
+
+        if ptr.is_null() {
+            return std::ptr::null_mut();
+        }
+
         *(ptr as *mut usize) = size as usize;
         ptr.offset(size_of::<usize>() as _) as *mut _
     }
@@ -766,6 +773,11 @@ fn setup_sqlite_alloc() {
             layout,
             new_size as usize + size_of::<usize>(),
         );
+
+        if ptr.is_null() {
+            return std::ptr::null_mut();
+        }
+
         *(new_ptr as *mut usize) = new_size as usize;
         new_ptr.offset(size_of::<usize>() as _) as *mut _
     }
