2a038ea07842f3899468094fe15ceee9e0c813ae
ad hoc <postma.marin@protonmail.com>
2024-07-16 09:53:26 +0200

    add unhandled storage variant

--- libsql-wal/src/storage/error.rs
+++ libsql-wal/src/storage/error.rs
@@ -8,4 +8,21 @@ pub enum Error {
     Compact(#[from] crate::error::Error),
     #[error("frame not {0} found")]
     FrameNotFound(u64),
+    #[error("unhandled storage error: {error}, in {context}")]
+    UnhandledStorageError {
+        error: Box<dyn std::error::Error + Send + Sync + 'static>,
+        context: String,
+    },
+}
+
+impl Error {
+    pub(crate) fn unhandled(
+        e: impl std::error::Error + Send + Sync + 'static,
+        ctx: impl Into<String>,
+    ) -> Self {
+        Self::UnhandledStorageError {
+            error: Box::new(e),
+            context: ctx.into(),
+        }
+    }
 }
