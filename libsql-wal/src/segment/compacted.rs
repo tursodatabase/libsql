d04a43443209ab4a9be8192af4dbac0335048463
ad hoc <postma.marin@protonmail.com>
2024-07-24 15:25:21 +0200

    add CompactedSegment

--- libsql-wal/src/segment/compacted.rs
+++ libsql-wal/src/segment/compacted.rs
@@ -1,5 +1,13 @@
+use std::mem::size_of;
+use std::io;
+
 use zerocopy::little_endian::{U128 as lu128, U32 as lu32, U64 as lu64, U16 as lu16};
 use zerocopy::{AsBytes, FromBytes, FromZeroes};
+
+use crate::io::buf::{ZeroCopyBoxIoBuf, ZeroCopyBuf};
+use crate::io::FileExt;
+
+use super::{Frame, Result};
 
 #[derive(Debug, AsBytes, FromZeroes, FromBytes)]
 #[repr(C)]
@@ -17,4 +25,27 @@ pub struct CompactedSegmentDataHeader {
 #[repr(C)]
 pub struct CompactedSegmentDataFooter {
     pub(crate) checksum: lu32,
+}
+
+pub struct CompactedSegment<F> {
+    header: CompactedSegmentDataHeader,
+    file: F,
+}
+
+impl<F: FileExt> CompactedSegment<F> {
+    pub(crate) async fn open(file: F) -> Result<Self> {
+        let buf = ZeroCopyBuf::new_uninit();
+        let (buf, ret) = file.read_exact_at_async(buf, 0).await;
+        ret?;
+        let header = buf.into_inner();
+        Ok(Self { file, header })
+
+    }
+
+    pub(crate) async fn read_frame(&self, frame: Box<Frame>, offset: u32) -> (Box<Frame>, io::Result<()>) {
+        let offset = size_of::<CompactedSegmentDataHeader>() + size_of::<Frame>() * offset as usize;
+        let buf = ZeroCopyBoxIoBuf::new(frame);
+        let (buf, ret) = self.file.read_exact_at_async(buf, offset as u64).await;
+        (buf.into_inner(), ret)
+    }
 }
