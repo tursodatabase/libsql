3c88ee964f9d4798a35112774d49373e31592895
Levy A. <levyddsa@gmail.com>
2024-08-28 22:31:54 -0300

    chore: update bundle

--- libsql-ffi/bundled/src/sqlite3.h
+++ libsql-ffi/bundled/src/sqlite3.h
@@ -149,11 +149,11 @@ extern "C" {
 ** [sqlite3_libversion_number()], [sqlite3_sourceid()],
 ** [sqlite_version()] and [sqlite_source_id()].
 */
-#define SQLITE_VERSION        "3.45.1"
+#define SQLITE_VERSION "3.45.1"
 #define SQLITE_VERSION_NUMBER 3045001
-#define SQLITE_SOURCE_ID      "2024-01-30 16:01:20 e876e51a0ed5c5b3126f52e532044363a014bc594cfefa87ffb5b82257ccalt1"
+#define SQLITE_SOURCE_ID "2024-01-30 16:01:20 e876e51a0ed5c5b3126f52e532044363a014bc594cfefa87ffb5b82257ccalt1"
 
-#define LIBSQL_VERSION        "0.2.3"
+#define LIBSQL_VERSION "0.2.3"
 
 /*
 ** CAPI3REF: Run-Time Library Version Numbers
