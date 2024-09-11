d77cca249c4b7d2440734d9a509d21b398c84416
ad hoc <postma.marin@protonmail.com>
2024-09-11 09:31:09 +0200

    introduce or strategy combinator

--- libsql-wal/src/swap_strategy/mod.rs
+++ libsql-wal/src/swap_strategy/mod.rs
@@ -12,6 +12,14 @@ pub(crate) trait SwapStrategy: Sync + Send + 'static {
         And(self, other)
     }
 
+    fn or<O: SwapStrategy>(self, other: O) -> Or<Self, O>
+    where
+        Self: Sized,
+    {
+        Or(self, other)
+    }
+}
+
 pub struct And<A, B>(A, B);
 
 impl<A, B> SwapStrategy for And<A, B>
@@ -31,4 +39,21 @@ where
     }
 }
 
+pub struct Or<A, B>(A, B);
+
+impl<A, B> SwapStrategy for Or<A, B>
+where
+    A: SwapStrategy,
+    B: SwapStrategy,
+{
+    #[inline(always)]
+    fn should_swap(&self, frames_in_wal: usize) -> bool {
+        self.0.should_swap(frames_in_wal) || self.1.should_swap(frames_in_wal)
+    }
+
+    #[inline(always)]
+    fn swapped(&self) {
+        self.0.swapped();
+        self.1.swapped();
+    }
 }
