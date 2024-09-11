b7236baf4338c97339ed07420db6d3694fcc6781
ad hoc <postma.marin@protonmail.com>
2024-09-11 09:30:46 +0200

    introduce And swap_strategy combinator

--- libsql-wal/src/swap_strategy/mod.rs
+++ libsql-wal/src/swap_strategy/mod.rs
@@ -4,4 +4,31 @@ pub(crate) mod frame_count;
 pub(crate) trait SwapStrategy: Sync + Send + 'static {
     fn should_swap(&self, frames_in_wal: usize) -> bool;
     fn swapped(&self);
+
+    fn and<O: SwapStrategy>(self, other: O) -> And<Self, O>
+    where
+        Self: Sized,
+    {
+        And(self, other)
+    }
+
+pub struct And<A, B>(A, B);
+
+impl<A, B> SwapStrategy for And<A, B>
+where
+    A: SwapStrategy,
+    B: SwapStrategy,
+{
+    #[inline(always)]
+    fn should_swap(&self, frames_in_wal: usize) -> bool {
+        self.0.should_swap(frames_in_wal) && self.1.should_swap(frames_in_wal)
+    }
+
+    #[inline(always)]
+    fn swapped(&self) {
+        self.0.swapped();
+        self.1.swapped();
+    }
+}
+
 }
