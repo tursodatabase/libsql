2d27ad30e745c47a010dc77b4b494be41c373aab
Pekka Enberg <penberg@turso.tech>
2024-10-11 10:48:20 +0300

    libsql-sqlite3: Add libsql_wal_frame_count() API
    This patch adds a libsql_wal_frame_count() API that the caller can use
    to determine how many frames there are in the WAL. You use the
    libsql_wal_frame_count() API  together with the libsql_wal_get_frame()
    API, added in another patch, to read the raw WAL frames from a database
    and inject them into another WAL representing the copy of the database.

--- libsql-sqlite3/src/pager.h
+++ libsql-sqlite3/src/pager.h
@@ -133,6 +133,8 @@ int sqlite3PagerReadFileheader(Pager*, int, unsigned char*);
 void sqlite3PagerSetBusyHandler(Pager*, int(*)(void *), void *);
 int sqlite3PagerSetPagesize(Pager*, u32*, int);
 Pgno sqlite3PagerMaxPageCount(Pager*, Pgno);
+int sqlite3PagerWalFrameCount(Pager *, unsigned int *);
+
 void sqlite3PagerSetCachesize(Pager*, int);
 int sqlite3PagerSetSpillsize(Pager*, int);
 void sqlite3PagerSetMmapLimit(Pager *, sqlite3_int64);
