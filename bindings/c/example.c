fa9d76f819f0731642cb5ff106c0257b6ef8d742
Piotr Jastrzebski <piotr@chiselstrike.com>
2024-11-29 10:15:18 +0100

    C bindings: Make example show embedded replicas
    Signed-off-by: Piotr Jastrzebski <piotr@chiselstrike.com>

--- bindings/c/example.c
+++ bindings/c/example.c
@@ -1,6 +1,7 @@
 #include "libsql.h"
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
@@ -8,14 +9,38 @@ int main(int argc, char *argv[])
 	libsql_rows_t rows;
 	libsql_row_t row;
 	libsql_database_t db;
+	libsql_config config;
 	const char *err = NULL;
 	int retval = 0;
+	char db_path[1024];
 
-	retval = libsql_open_ext(":memory:", &db, &err);
-	if (retval != 0) {
-		fprintf(stderr, "%s\n", err);
-		goto quit;
-	}
+	if (argc > 1) {
+	    char* url = argv[1];
+	    char auth_token[1024];
+	    auth_token[0] = '\0';
+	    if (argc > 2) {
+	        strncpy(auth_token, argv[2], strlen(argv[2]));
+	    }
+	    strncpy(db_path, "test.db", strlen("test.db"));
+	    config.db_path = db_path;
+	    config.primary_url = url;
+	    config.auth_token = auth_token;
+	    config.read_your_writes = 0;
+	    config.encryption_key = NULL;
+	    config.sync_interval = 0;
+	    config.with_webpki = 0;
+	    retval = libsql_open_sync_with_config(config, &db, &err);
+        if (retval != 0) {
+            fprintf(stderr, "%s\n", err);
+            goto quit;
+        }
+    } else {
+        retval = libsql_open_ext(":memory:", &db, &err);
+        if (retval != 0) {
+            fprintf(stderr, "%s\n", err);
+            goto quit;
+        }
+    }
 
 	retval = libsql_connect(db, &conn, &err);
 	if (retval != 0) {
