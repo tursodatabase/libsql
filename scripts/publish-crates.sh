d08933fc24bb5d676b6c26784dac3b89c17ce42c
Pekka Enberg <penberg@turso.tech>
2025-03-21 11:47:01 +0200

    Update vendored rusqlite version

--- scripts/publish-crates.sh
+++ scripts/publish-crates.sh
@@ -2,6 +2,7 @@
 
 cargo publish -p libsql-ffi
 cargo publish -p libsql-sys
+cargo publish -p libsql-rusqlite
 cargo publish -p libsql-hrana
 cargo publish -p libsql_replication
 cargo publish -p libsql
