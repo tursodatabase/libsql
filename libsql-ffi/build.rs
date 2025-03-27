tursodatabase/libsql

commit da54c1bf97acc6920b361935ef121c40f9a2ae7b

fix: try fixing copy, again

libsql-ffi/build.rs
@@ -57,31 +57,41 @@ fn main() {
     build_bundled(&out_dir, &out_path);
 }
 
-#[cfg(target_os = "windows")]
-fn copy_with_cp(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
-    fs::copy(src, dst)?; // do a regular file copy on Windows
+fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
+    let dst = dst.as_ref();
+    fs::create_dir_all(dst)?;
+    for entry in fs::read_dir(src)? {
+        let entry = entry?;
+        let ty = entry.file_type()?;
+        if ty.is_dir() {
+            copy_dir_all(entry.path(), dst.join(entry.file_name()))?;
+        } else {
+            fs::copy(entry.path(), dst.join(entry.file_name()))?;
+        }
+    }
     Ok(())
 }
 
 /// This ensures that in sandboxed environments, such as Nix, permissions from other sources don't
 /// propagate into OUT_DIR. If not present, when trying to rewrite a file, a `Permission denied`
 /// error will occur.
-fn copy_with_cp(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
+fn copy_with_cp(from: impl AsRef<Path>, to: impl AsRef<Path>) -> io::Result<()> {
     let status = Command::new("cp")
         .arg("--no-preserve=mode,ownership")
         .arg("-R")
-        .arg(src.as_ref().to_str().unwrap())
-        .arg(dst.as_ref().to_str().unwrap())
+        .arg(from.as_ref().to_str().unwrap())
+        .arg(to.as_ref().to_str().unwrap())
         .status()?;
 
-    if !status.success() {
-        Err(io::Error::new(
-            io::ErrorKind::Other,
-            "Failed to copy using cp",
-        ))
-    } else {
-        Ok(())
+    if status.success() {
+        return Ok(());
     }
+
+    return match fs::copy(from.as_ref(), to.as_ref()) {
+        Err(err) if err.kind() == io::ErrorKind::InvalidInput => copy_dir_all(from, to),
+        Ok(_) => Ok(()),
+        Err(err) => Err(err),
+    };
 }
 
 fn make_amalgamation() {
@@ -98,6 +108,7 @@ fn make_amalgamation() {
         .env("CFLAGS", flags.join(" "))
         .output()
         .unwrap();
+
     Command::new("make")
         .current_dir(SQLITE_DIR)
         .output()
@@ -108,6 +119,7 @@ fn make_amalgamation() {
         (BUNDLED_DIR.as_ref() as &Path).join("src/sqlite3.c"),
     )
     .unwrap();
+
     copy_with_cp(
         (SQLITE_DIR.as_ref() as &Path).join("sqlite3.h"),
         (BUNDLED_DIR.as_ref() as &Path).join("src/sqlite3.h"),
@@ -410,32 +422,31 @@ fn build_multiple_ciphers(target: &str, out_path: &Path) {
     } else {
         "bundled/bindings/bindgen.rs"
     };
+
     if std::env::var("LIBSQL_DEV").is_ok() {
         let header = HeaderLocation::FromPath(format!("{BUNDLED_DIR}/src/sqlite3.h"));
         bindings::write_to_out_dir(header, bindgen_rs_path.as_ref());
     }
+
     let dir = env!("CARGO_MANIFEST_DIR");
     copy_with_cp(format!("{dir}/{bindgen_rs_path}"), out_path).unwrap();
 
+    let out_dir = env::var("OUT_DIR").unwrap();
+
     copy_with_cp(
-        (BUNDLED_DIR.as_ref() as &Path)
-            .join("src")
-            .join("sqlite3.c"),
-        (BUNDLED_DIR.as_ref() as &Path)
-            .join("SQLite3MultipleCiphers")
-            .join("src")
-            .join("sqlite3.c"),
+        dbg!(format!("{BUNDLED_DIR}/SQLite3MultipleCiphers")),
+        format!("{out_dir}/sqlite3mc"),
     )
     .unwrap();
 
-    let bundled_dir = env::current_dir()
-        .unwrap()
-        .join(BUNDLED_DIR)
-        .join("SQLite3MultipleCiphers");
-    let out_dir = env::var("OUT_DIR").unwrap();
+    copy_with_cp(
+        PathBuf::from(BUNDLED_DIR).join("src").join("sqlite3.c"),
+        format!("{out_dir}/sqlite3mc/src/sqlite3.c"),
+    )
+    .unwrap();
+
+    let bundled_dir = format!("{out_dir}/sqlite3mc");
     let sqlite3mc_build_dir = env::current_dir().unwrap().join(out_dir).join("sqlite3mc");
-    let _ = fs::remove_dir_all(sqlite3mc_build_dir.clone());
-    fs::create_dir_all(sqlite3mc_build_dir.clone()).unwrap();
 
     let mut cmake_opts: Vec<&str> = vec![];
 

commit 9364e90ea1ffcd91509607abc00756581d619bda

Fix libsql-ffi build on Windows
libsql-ffi build on Windows has been intermittently broken for a while due _build.rs_ calling `cp` which is not present on Windows.

Timeline:
- 2024-08-10: Issue #1657 was opened.
- 2024-11-14: #1791 was merged. First fix using `#[cfg(windows)]`.
- 2024-11-15: #1791 reverted in 9499c3c for breaking libsql-js on macOS.
- 2024-12-04: 9499c3c was reverted since #1791 was fine and did not cause the macOS problem. Windows build works again.
- 2025-03-17: da54c1b breaks Windows build.
- 2025-03-21: 0.9.1 is released with broken Windows build.
- 2025-03-27: This commit fixes the bug introduced in da54c1b.

This commit fixes a bug introduced by da54c1b. `Command::status()?` was propogating the error so the fallback, `fs::copy`, was never reached.
This is now fixed so libsql-ffi builds again on Windows.

Closes #1657

libsql-ffi/build.rs
@@ -76,22 +76,20 @@ fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> 
 /// propagate into OUT_DIR. If not present, when trying to rewrite a file, a `Permission denied`
 /// error will occur.
 fn copy_with_cp(from: impl AsRef<Path>, to: impl AsRef<Path>) -> io::Result<()> {
-    let status = Command::new("cp")
+    match Command::new("cp")
         .arg("--no-preserve=mode,ownership")
         .arg("-R")
         .arg(from.as_ref().to_str().unwrap())
         .arg(to.as_ref().to_str().unwrap())
-        .status()?;
-
-    if status.success() {
-        return Ok(());
+        .status()
+    {
+        Ok(status) if status.success() => Ok(()),
+        _ => match fs::copy(from.as_ref(), to.as_ref()) {
+            Err(err) if err.kind() == io::ErrorKind::InvalidInput => copy_dir_all(from, to),
+            Ok(_) => Ok(()),
+            Err(err) => Err(err),
+        },
     }
-
-    return match fs::copy(from.as_ref(), to.as_ref()) {
-        Err(err) if err.kind() == io::ErrorKind::InvalidInput => copy_dir_all(from, to),
-        Ok(_) => Ok(()),
-        Err(err) => Err(err),
-    };
 }
 
 fn make_amalgamation() {