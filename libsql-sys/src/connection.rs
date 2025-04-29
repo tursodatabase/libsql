tursodatabase/libsql

commit 8ea257afe9534e58cf90c5ee8ccad4fc7545d416

fix(libsql-sys): support non-Unix when rusqlite feature is disabled
Currently the codepath used when rusqlite is disabled, is
Unix-platform-dependent. This patch is adding a platform-independent
branch. The database path is expected to be UTF-8 by the libsql native
library. However, in the real world, all Unix paths are not necessarily
UTF-8. For this reason, I understand why the Unix codepath is attempting
a direct conversion from OsString to CString. However, it’s not possible
to do something similar on other platforms. For these platforms, we
are enforcing correct UTF-8 using to_str. At least, it should work
reasonably well in most cases.

libsql-sys/src/connection.rs
@@ -273,9 +273,27 @@ impl<W: Wal> Connection<W> {
 
         #[cfg(not(feature = "rusqlite"))]
         let conn = unsafe {
-            use std::os::unix::ffi::OsStrExt;
-            let path = std::ffi::CString::new(path.as_ref().as_os_str().as_bytes())
-                .map_err(|_| crate::error::Error::Bug("invalid database path"))?;
+            #[cfg(unix)]
+            let path = {
+                use std::os::unix::ffi::OsStrExt;
+                std::ffi::CString::new(path.as_ref().as_os_str().as_bytes()).map_err(|_| {
+                    crate::error::Error::Bug(
+                        "invalid database path containing an internal nul byte",
+                    )
+                })?
+            };
+            #[cfg(not(unix))]
+            let path = path
+                .to_str()
+                .ok_or_else(|| crate::error::Error::Bug("database path is not valid unicode"))
+                .and_then(|x| {
+                    std::ffi::CString::new(x).map_err(|_| {
+                        crate::error::Error::Bug(
+                            "invalid database path containing an internal nul byte",
+                        )
+                    })
+                })?;
+
             let mut conn: *mut crate::ffi::sqlite3 = std::ptr::null_mut();
             // We pass a pointer to the WAL methods data to the database connection. This means
             // that the reference must outlive the connection. This is guaranteed by the marker in

commit 64e2b055394cf442b37cdcf1a2811bf2a67fec31

fix(libsql-sys): fix non-Unix compilation when rusqlite feature is disabled
This commit fixes a compilation error introduced in commit 8ea257afe9534e58cf90c5ee8ccad4fc7545d416 on non-Unix systems.

libsql-sys/src/connection.rs
@@ -284,6 +284,7 @@ impl<W: Wal> Connection<W> {
             };
             #[cfg(not(unix))]
             let path = path
+                .as_ref()
                 .to_str()
                 .ok_or_else(|| crate::error::Error::Bug("database path is not valid unicode"))
                 .and_then(|x| {