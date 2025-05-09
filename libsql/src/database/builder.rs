3a84ed4d93b3ac0920d135cd9bbd6cad33b754f2
Levy A. <levyddsa@gmail.com>
2025-05-09 10:38:07 -0300

    feat: sycn_interval for offline writes

--- libsql/src/database/builder.rs
+++ libsql/src/database/builder.rs
@@ -2,7 +2,11 @@ cfg_core! {
     use crate::EncryptionConfig;
 }
 
-use crate::{Database, Result};
+use std::sync::Arc;
+
+use tracing::Instrument as _;
+
+use crate::{sync::DropAbort, Database, Result};
 
 use super::DbType;
 
@@ -108,6 +112,7 @@ impl Builder<()> {
                     read_your_writes: true,
                     remote_writes: false,
                     push_batch_size: 0,
+                    sync_interval: None,
                 },
             }
         }
@@ -401,12 +406,18 @@ cfg_replication! {
 
                         if res.status().is_success() {
                             tracing::trace!("Using sync protocol v2 for {}", url);
-                            return Builder::new_synced_database(path, url, auth_token)
+                            let builder = Builder::new_synced_database(path, url, auth_token)
                                 .connector(connector)
                                 .remote_writes(true)
-                                .read_your_writes(read_your_writes)
-                                .build()
-                                .await;
+                                .read_your_writes(read_your_writes);
+
+                            let builder = if let Some(sync_interval) = sync_interval {
+                                builder.sync_interval(sync_interval)
+                            } else {
+                                builder
+                            };
+
+                            return builder.build().await;
                         }
                         tracing::trace!("Using sync protocol v1 for {} based on probe results", url);
                     }
@@ -542,6 +553,7 @@ cfg_sync! {
         remote_writes: bool,
         read_your_writes: bool,
         push_batch_size: u32,
+        sync_interval: Option<std::time::Duration>,
     }
 
     impl Builder<SyncedDatabase> {
@@ -563,6 +575,14 @@ cfg_sync! {
 
         pub fn set_push_batch_size(mut self, v: u32) -> Builder<SyncedDatabase> {
             self.inner.push_batch_size = v;
+            self
+        }
+
+        /// Set the duration at which the replicator will automatically call `sync` in the
+        /// background. The sync will continue for the duration that the resulted `Database`
+        /// type is alive for, once it is dropped the background task will get dropped and stop.
+        pub fn sync_interval(mut self, duration: std::time::Duration) -> Builder<SyncedDatabase> {
+            self.inner.sync_interval = Some(duration);
             self
         }
 
@@ -594,6 +614,7 @@ cfg_sync! {
                 remote_writes,
                 read_your_writes,
                 push_batch_size,
+                sync_interval,
             } = self.inner;
 
             let path = path.to_str().ok_or(crate::Error::InvalidUTF8Path)?.to_owned();
@@ -624,6 +645,30 @@ cfg_sync! {
                 db.sync_ctx.as_ref().unwrap().lock().await.set_push_batch_size(push_batch_size);
             }
 
+            let mut bg_abort: Option<Arc<DropAbort>> = None;
+            let conn = db.connect()?;
+
+            let sync_ctx = db.sync_ctx.as_ref().unwrap().clone();
+
+            if let Some(sync_interval) = sync_interval {
+                let jh = tokio::spawn(
+                    async move {
+                        loop {
+                            tracing::trace!("trying to sync");
+                            let mut ctx = sync_ctx.lock().await;
+                            if let Err(e) = crate::sync::try_pull(&mut ctx, &conn).await {
+                                tracing::error!("sync error: {}", e);
+                            }
+
+                            tokio::time::sleep(sync_interval).await;
+                        }
+                    }
+                    .instrument(tracing::info_span!("sync_interval")),
+                );
+
+                bg_abort.replace(Arc::new(DropAbort(jh.abort_handle())));
+            }
+
             Ok(Database {
                 db_type: DbType::Offline {
                     db,
@@ -632,6 +677,7 @@ cfg_sync! {
                     url,
                     auth_token,
                     connector,
+                    _bg_abort: bg_abort,
                 },
                 max_write_replication_index: Default::default(),
             })
