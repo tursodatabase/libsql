891899bc30596486077ae2c23c181db34f6fbadd
Drew Robinson <drew.robinson@gaiaresources.com.au>
2026-01-09 14:42:32 +1100

    Update generated protobuf file with wincode comment

--- libsql-replication/src/generated/proxy.rs
+++ libsql-replication/src/generated/proxy.rs
@@ -180,7 +180,7 @@ pub struct Description {
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Value {
-    /// / bincode encoded Value
+    /// / wincode encoded Value (binary compatible with bincode)
     #[prost(bytes = "vec", tag = "1")]
     pub data: ::prost::alloc::vec::Vec<u8>,
 }
