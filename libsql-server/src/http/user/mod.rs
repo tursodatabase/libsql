ad804277e4836e3f7035ab6f94889f290e8a671b
Devon Shigaki <93375490+devonshigaki@users.noreply.github.com>
2026-03-28 16:29:23 -0700

    fix: Enable HTTP/2 support in hyper server

--- libsql-server/src/http/user/mod.rs
+++ libsql-server/src/http/user/mod.rs
@@ -469,7 +469,8 @@ where
 
             task_manager.spawn_with_shutdown_notify(|shutdown| async move {
                 let builder =
-                    hyper_util::server::conn::auto::Builder::new(hyper_util::rt::TokioExecutor::new());
+                    hyper_util::server::conn::auto::Builder::new(hyper_util::rt::TokioExecutor::new())
+                        .http2_only(false);
 
                 let mut acceptor = acceptor;
 
